e9e5905124d46c67fd8b03634e337292e8b69e26
bert Dvornik <advornik@simquest.com>
2013-02-11 15:04:29 -0500

    Add a few missing copyright notices.

--- SurgSim/Framework/UnitTests/MockObjects.h
+++ SurgSim/Framework/UnitTests/MockObjects.h
@@ -1,5 +1,20 @@
-#ifndef _SURGSIM_UNITTEST_MOCKOBJECTS_H_
-#define _SURGSIM_UNITTEST_MOCKOBJECTS_H_
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef MOCK_OBJECTS_H
+#define MOCK_OBJECTS_H
 
 #include <memory>
 
@@ -11,7 +26,7 @@
 #include "SurgSim/Framework/Assert.h"
 #include "SurgSim/Framework/Runtime.h"
 
-//! Class to catch the calls made to the scene element, does nothing
+/// Class to catch the calls made to the scene element, does nothing
 class MockSceneElement : public SurgSim::Framework::SceneElement
 {
 public:
@@ -175,4 +190,4 @@ public:
 // 	int updateCount;
 // };
 
-#endif
+#endif // MOCK_OBJECTS_H
