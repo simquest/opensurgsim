8bdc0fd04b27945f34172a15244575d01294a7b5
bert Dvornik <advornik@simquest.com>
2013-02-18 16:54:54 -0500

    Add interfaces for generic input devices and related API.
    Since these are just interfaces, there is no code to be unit tested.

---
+++ SurgSim/Input/DeviceClassManagerInterface.h
@@ -0,0 +1,45 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SURGSIM_INPUT_DEVICE_CLASS_MANAGER_INTERFACE_H
+#define SURGSIM_INPUT_DEVICE_CLASS_MANAGER_INTERFACE_H
+
+#include <memory>
+#include <string>
+
+#include <SurgSim/Input/InputDeviceInterface.h>
+
+namespace SurgSim
+{
+namespace Input
+{
+
+/// Interface for an object that creates and manages and particular class of devices (multi-axis human interface
+/// devices; Novint Technologies haptic devices; etc.).
+///
+// TODO(bert) This API really ought to include a method that creates a device, but right now we don't have any
+// uniform signature for that (each device type uses a different argument list).  Future configurability work
+// should try to resolve that...
+class DeviceClassManagerInterface
+{
+public:
+	/// Virtual destructor (empty).
+	virtual ~DeviceClassManagerInterface() {};
+};
+
+};  // namespace Input
+};  // namespace SurgSim
+
+#endif // SURGSIM_INPUT_DEVICE_CLASS_MANAGER_INTERFACE_H
