9b4de7f2752f6b533a014bcf674eeaaa42f2ef2d
Paul Novotny <pnovotny@simquest.com>
2013-02-28 11:18:42 -0500

    Install documentation and add 'd' to debug libs
    Added cmake install commands to install doxygen documentation to the
    documentation directory. The documentation subdirectory will be under
    the cmake install directory.
    
    Also, append 'd' to libraries when built as 'Debug' with Visual
    Studio.

--- CMake/SurgSimBuildFlags.cmake
+++ CMake/SurgSimBuildFlags.cmake
@@ -65,6 +65,7 @@ if(MSVC)
 	# Enable parallel builds:
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP")
 	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /MP")  # is this needed?
+	set(CMAKE_DEBUG_POSTFIX "d")
 endif(MSVC)
 
 # Windows-specific settings
