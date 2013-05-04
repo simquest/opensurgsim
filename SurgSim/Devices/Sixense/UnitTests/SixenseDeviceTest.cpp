47f9ae128e964f19a694dce344bd1c0ed9187f31
bert Dvornik <advornik@simquest.com>
2013-05-03 20:40:29 -0400

    Fix SixenseDeviceTest file comment.

--- SurgSim/Devices/Sixense/UnitTests/SixenseDeviceTest.cpp
+++ SurgSim/Devices/Sixense/UnitTests/SixenseDeviceTest.cpp
@@ -13,9 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/** @file
- * Tests for the SixenseDevice and SixenseManager classes.
- */
+/// \file
+/// Tests for the SixenseDevice and SixenseManager classes.
 
 #include <memory>
 #include <string>
