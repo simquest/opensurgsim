b550f8e43416c0e3bc7eec72f0c64dbac2339fd5
Daniel King <dking@simquest.com>
2013-05-15 17:25:43 -0400

    Add documentation to make the use of noexcept clear in AssertMessage's destructor.

--- SurgSim/Framework/AssertMessage.h
+++ SurgSim/Framework/AssertMessage.h
@@ -66,11 +66,11 @@ public:
 	{
 	}
 
-	/// Destructor
+	/// Destructor, which may throw an exception if the failure behavior does
 #ifdef _MSC_VER
-	~AssertMessage() throw(...)
+	~AssertMessage() throw(...)  // Visual Studio does not support noexcept. The throw(...) is optional.
 #else
-	~AssertMessage() noexcept(false)
+	~AssertMessage() noexcept(false)  /// C++11 introduced noexcept
 #endif
 	{
 		flush();
