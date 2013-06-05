e2f903653c5b5d5b23312414b57519c340127111
bert Dvornik <advornik@simquest.com>
2013-06-05 00:16:49 -0400

    Also check the header file include guards.
    The guard checking is not fully exhaustive.  For example, if a header
    file contains nested namespaces "Aaa", "Bbb" and "Ccc", but the
    include guard used is CCC_FILENAME_H, no warning will be reported.
    (Note that you can skip any number of namespaces, but not all of them;
    just FILENAME_H will produce a warning.)  This is needed to handle
    pre-declarations etc:
    
      namespace Aaa
      {
      namespace Bbb
      {
      class Predeclared;
      };
      };
      namespace Ccc
      { ...

--- Tools/run-lint.py
+++ Tools/run-lint.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Runs Google's cpplint.py on C++ files.
+"""Runs Google's cpplint.py and some other checks on C++ files.
 
 This script tries to flag violations of the OpenSurgSim coding
 standards in C++ code.
@@ -46,7 +46,7 @@ def emit_warning(fields):
   if 'category' in fields and fields['category'] is not None:
     fields['vscategory'] = fields['category']
   else:
-    fmt = re.sub(r'\s*\[\{category[^\}]*\}\w\]', '', fmt)
+    fmt = re.sub(r'\s*\[\{category[^\}]*\}\]', '', fmt)
     fmt = re.sub(r'\s*\{category[^\}]*\}', '', fmt)
     fields['category'] = '???'  # panic button
     fields['vscategory'] = 'RL'
@@ -67,6 +67,15 @@ def emit_error(fields):
   fields = dict(fields)
   fields['warning'] = 'error'
   emit_warning(fields)
+
+def slurp_lines(file):
+  try:
+    with open(file, 'r') as f:
+      return f.read().splitlines()
+  except IOError as e:
+    print >> sys.stderr, e
+    #emit_error({'file': file, 'text': str(e)})
+    return None
 
 def run_cpplint(script, filter, files):
   if not files:
@@ -111,6 +120,92 @@ def run_cpplint(script, filter, files):
     return False
   return cmd.returncode == 0
 
+def check_header_guard(flags, file):
+  lines = slurp_lines(file)
+  if not lines:
+    emit_warning({'file': file, 'text': "file is missing or empty."})
+    return False
+
+  ifndef = filter(lambda x: re.search(r'^\s*#\s*ifndef\b', x), lines)
+  if not ifndef:
+    emit_warning({'file': file, 'text': "no #ifndef lines!"})
+    return False
+#  elif len(ifndef) > 1:
+#    emit_warning({'file': file,
+#                  'text': "multiple #ifndef lines; using first."})
+
+  guard = ifndef[0]
+  guard = re.sub(r'//.*', '', guard)
+  guard = re.sub(r'/\*.*?\*/', '', guard)
+  guard_match = re.search(r'^\s*#\s*ifndef\s+(\w+)\s*$', guard)
+  if not guard_match:
+    emit_error({'file': file,
+                'text': "script error: failed to parse #ifndef line!"})
+    return False
+  guard = guard_match.group(1)
+
+  namespace = filter(lambda x: re.search(r'^\s*namespace\b', x), lines)
+  if not namespace:
+    emit_warning({'file': file, 'text': "no 'namespace' lines!"})
+  if filter(lambda x: re.search(r'^\s', x), namespace):
+    emit_warning({'file': file,
+                  'text': "one or more namespace declarations are indented!"})
+  namespace = map(lambda x: re.sub(r'^\s*namespace\b', '', x).strip(),
+                  namespace)
+  if filter(lambda x: re.search(r'{$', x), namespace):
+    emit_warning({'file': file,
+                  'text': "'{' on the same line as a namespace declaration!"})
+    namespace = map(lambda x: re.sub(r'\s*{$', '', x), namespace)
+
+  namespace_re = "".join(
+    map(lambda x: "(?:" + re.sub(r'[^\w\d]+', '', x.upper()) + "_)?",
+        namespace))
+  if len(namespace_re):
+    namespace_re += r'(?<=.)'
+
+  file_guard_re = re.sub(r'\W+', '_',
+                         re.sub(r'.*/', '', file)).upper()
+  guard_re = r'^' + namespace_re + file_guard_re + r'$'
+
+  if not re.search(guard_re, guard):
+    emit_warning({'file': file,
+                  'text': ("unexpected guard '{}'!  expected /{}/."
+                           .format(guard, guard_re)) })
+  define = filter(lambda x: re.search(r'^\s*#\s*define\b', x), lines)
+  if not define:
+    emit_warning({'file': file, 'text': "no #define lines!"})
+  else:
+    def_match = re.match(r'^\s*#\s*define\s+(\w+)\s*$',
+                         re.sub(r'/\*.*?\*/', '',
+                                re.sub(r'//.*', '', define[0])))
+    if not def_match:
+      emit_error({'file': file,
+                  'text': "script error: failed to parse #define line!"})
+    elif def_match.group(1) != guard:
+      emit_warning({'file': file, 'text': "#define doesn't match #ifndef!"})
+
+  endif = filter(lambda x: re.search(r'^\s*#\s*endif\b', x), lines)
+  if not endif:
+    emit_warning({'file': file, 'text': "no #endif lines!"})
+  elif re.search(r'^\s*#\s*endif\s*$', endif[-1]):
+    if flags.missing_endif_comments:
+      emit_warning({'file': file, 'text': "#endif with no comment."})
+  else:
+    if re.search(r'^\s*#\s*endif\s*///', endif[-1]):
+      emit_warning({'file': file,
+                    'text': "#endif with a Doxygen style /// comment."})
+    endif_match = re.search(r'^\s*#\s*endif\s*//+\s*(.*?)\s*$', endif[-1])
+    if not endif_match:
+      emit_error({'file': file,
+                  'text': "script error: failed to parse #endif comment."})
+    elif endif_match.group(1) != guard:
+      emit_warning({'file': file,
+                    'text': ("#endif comment doesn't match #ifndef! (" + \
+                               endif_match.group(1) + ")")})
+
+def check_guards(flags, files):
+  return all(map(lambda f: check_header_guard(flags, f), files))
+
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(
     description='Check source files for coding standard violations.')
@@ -118,9 +213,17 @@ if __name__ == '__main__':
                       help='The path to Google\'s cpplint.py script.',
                       default='cpplint.py')
   parser.add_argument('--cpplint-filter', '--filter', metavar='FILTER',
-                      help=('The --filter argument to pass to cpplint.'))
-  parser.add_argument('--no-cpplint', action='store_true',
+                      help='The --filter argument to pass to cpplint.')
+  parser.add_argument('--no-cpplint', action='store_false', dest='do_cpplint',
                       help='Do not run the cpplint.py script.')
+  parser.add_argument('--missing-endif-comments', action='store_true',
+                      help='Flag missing include guard #endif comments, too.')
+  parser.add_argument('--ignore-guards',
+                      action='store_false', dest='do_check_guards',
+                      help='Do not check the header file include guards.')
+  parser.add_argument('--ignore-ext',
+                      action='store_false', dest='do_check_extension',
+                      help='Do not check the file extensions.')
   parser.add_argument('--format',
                       help='The format string for warnings and errors.')
   parser.add_argument('--visual-studio', '--vs', action='store_true',
@@ -138,8 +241,22 @@ if __name__ == '__main__':
 
   ok = True
 
-  if not args.no_cpplint:
-    if not run_cpplint(args.cpplint_script, args.cpplint_filter, args.files):
+  if args.do_check_extension:
+    bad_ext = filter(lambda x: not re.search(r'\.(?:h|cpp)$', x), args.files)
+    for file in bad_ext:
+      emit_warning({'file': file,
+                    'text': "unknown extension (expected .h or .cpp)"})
+      ok = False
+
+  if args.do_cpplint:
+    if not run_cpplint(args.cpplint_script, args.cpplint_filter,
+                       filter(lambda x: re.search(r'\.(?:h|cpp)$', x),
+                              args.files)):
+      ok = False
+
+  if args.do_check_guards:
+    if not check_guards(args,
+                        filter(lambda x: re.search(r'\.h$', x), args.files)):
       ok = False
 
   if not ok:
