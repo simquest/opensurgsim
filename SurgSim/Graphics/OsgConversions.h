02b0e6df2fe1c8beb19228ff482b3465a2aef9ff
Daniel King <dking@simquest.com>
2013-06-17 12:49:05 -0400

    OsgConversions.h which includes the 4 different conversions headers for convenience.

---
+++ SurgSim/Graphics/OsgConversions.h
@@ -0,0 +1,27 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/// \file
+/// Conversions to and from OSG types
+
+#ifndef SURGSIM_GRAPHICS_OSGCONVERSIONS_H
+#define SURGSIM_GRAPHICS_OSGCONVERSIONS_H
+
+#include <SurgSim/Graphics/OsgMatrixConversions.h>
+#include <SurgSim/Graphics/OsgQuaternionConversions.h>
+#include <SurgSim/Graphics/OsgRigidTransformConversions.h>
+#include <SurgSim/Graphics/OsgVectorConversions.h>
+
+#endif  // SURGSIM_GRAPHICS_OSGQUATERNIONCONVERSIONS_H
