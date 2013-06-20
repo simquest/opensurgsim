0a7c7f5087b948d1580e1f52ee597176f436132c
Julien Lenoir <jlenoir@simquest.com>
2013-06-20 14:25:10 -0400

    Fix typos + docs

--- SurgSim/Physics/Computation.h
+++ SurgSim/Physics/Computation.h
@@ -41,8 +41,8 @@ public:
 	/// the last update call in seconds
 	std::shared_ptr<PhysicsManagerState> update(double dt, const std::shared_ptr<PhysicsManagerState>& state);;
 
-	/// Sets up wether the computation will copy the state of PhysicsManagerState before executing.
-	/// \param	val	Wether to create a copy of the PhysicsState before running the update fuction.
+	/// Sets up wheter the computation will copy the state of PhysicsManagerState before executing.
+	/// \param	val	Whether to create a copy of the PhysicsState before running the update fuction.
 	void setDoCopyState(bool val);
 
 	/// Query if this object is copying the PhysicsManagerState.
