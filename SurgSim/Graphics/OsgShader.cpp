0cc473cf3fe80c1355baaa50f0aa107e2ce831f7
Daniel King <dking@simquest.com>
2013-06-26 14:54:57 -0400

    Add methods to set/load/clear the vertex, geometry, and fragment source of Shaders.

--- SurgSim/Graphics/OsgShader.cpp
+++ SurgSim/Graphics/OsgShader.cpp
@@ -32,3 +32,147 @@ void OsgShader::removeFromStateSet(osg::StateSet* stateSet)
 {
 	stateSet->removeAttribute(m_program);
 }
+
+bool OsgShader:: hasVertexShader()
+{
+	return m_vertexShader.valid();
+}
+
+void OsgShader::clearVertexShader()
+{
+	if (m_vertexShader)
+	{
+		m_program->removeShader(m_vertexShader);
+		m_vertexShader = nullptr;
+	}
+}
+
+bool OsgShader::loadVertexShaderSource(const std::string& filePath)
+{
+	if (! m_vertexShader.valid())
+	{
+		m_vertexShader = new osg::Shader(osg::Shader::VERTEX);
+		m_program->addShader(m_vertexShader);
+	}
+	return m_vertexShader->loadShaderSourceFromFile(filePath);
+}
+
+void OsgShader::setVertexShaderSource(const std::string& source)
+{
+	if (! m_vertexShader.valid())
+	{
+		m_vertexShader = new osg::Shader(osg::Shader::VERTEX);
+		m_program->addShader(m_vertexShader);
+	}
+	m_vertexShader->setShaderSource(source);
+}
+
+bool OsgShader::getVertexShaderSource(std::string* source) const
+{
+	if (m_vertexShader)
+	{
+		*source = m_vertexShader->getShaderSource();
+		return true;
+	}
+	else
+	{
+		*source = "";
+		return false;
+	}
+}
+
+bool OsgShader::hasGeometryShader() const
+{
+	return m_geometryShader.valid();
+}
+
+void OsgShader::clearGeometryShader()
+{
+	if (m_geometryShader)
+	{
+		m_program->removeShader(m_geometryShader);
+		m_geometryShader = nullptr;
+	}
+}
+
+bool OsgShader::loadGeometryShaderSource(const std::string& filePath)
+{
+	if (! m_geometryShader.valid())
+	{
+		m_geometryShader = new osg::Shader(osg::Shader::GEOMETRY);
+		m_program->addShader(m_geometryShader);
+	}
+	return m_geometryShader->loadShaderSourceFromFile(filePath);
+}
+
+void OsgShader::setGeometryShaderSource(const std::string& source)
+{
+	if (! m_geometryShader.valid())
+	{
+		m_geometryShader = new osg::Shader(osg::Shader::GEOMETRY);
+		m_program->addShader(m_geometryShader);
+	}
+	m_geometryShader->setShaderSource(source);
+}
+
+bool OsgShader::getGeometryShaderSource(std::string* source) const
+{
+	if (m_geometryShader)
+	{
+		*source = m_geometryShader->getShaderSource();
+		return true;
+	}
+	else
+	{
+		*source = "";
+		return false;
+	}
+}
+
+bool OsgShader::hasFragmentShader() const
+{
+	return m_fragmentShader.valid();
+}
+
+void OsgShader::clearFragmentShader()
+{
+	if (m_fragmentShader)
+	{
+		m_program->removeShader(m_fragmentShader);
+		m_fragmentShader = nullptr;
+	}
+}
+
+bool OsgShader::loadFragmentShaderSource(const std::string& filePath)
+{
+	if (! m_fragmentShader.valid())
+	{
+		m_fragmentShader = new osg::Shader(osg::Shader::FRAGMENT);
+		m_program->addShader(m_fragmentShader);
+	}
+	return m_fragmentShader->loadShaderSourceFromFile(filePath);
+}
+
+void OsgShader::setFragmentShaderSource(const std::string& source)
+{
+	if (! m_fragmentShader.valid())
+	{
+		m_fragmentShader = new osg::Shader(osg::Shader::FRAGMENT);
+		m_program->addShader(m_fragmentShader);
+	}
+	m_fragmentShader->setShaderSource(source);
+}
+
+bool OsgShader::getFragmentShaderSource(std::string* source) const
+{
+	if (m_fragmentShader)
+	{
+		*source = m_fragmentShader->getShaderSource();
+		return true;
+	}
+	else
+	{
+		*source = "";
+		return false;
+	}
+}
