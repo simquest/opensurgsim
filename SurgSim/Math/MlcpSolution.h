1cc709ec4b7d243171520b88008962758b30b941
bert Dvornik <advornik@simquest.com>
2013-06-26 07:20:33 -0400

    Add header files related to solving mixed LCP problems.
    This includes MlcpProblem, MlcpSolution and MlcpSolver.
    There are no unit tests because there is no implementation code.

---
+++ SurgSim/Math/MlcpSolution.h
@@ -0,0 +1,58 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SURGSIM_MATH_MLCPSOLUTION_H
+#define SURGSIM_MATH_MLCPSOLUTION_H
+
+#include <Eigen/Core>
+
+namespace SurgSim
+{
+namespace Math
+{
+
+/// The description of a solution to an MLCP problem.
+///
+/// The solution consists of the vector \f$x\f$ and various diagnostic parameers.
+/// If \f$c\f$ is also needed, it can be computed by the caller.
+///
+/// \sa MlcpProblem, MlcpSolver
+
+struct MlcpSolution
+{
+	Eigen::VectorXd x;
+
+	int numIterations;
+	bool validConvergence;
+	bool validSignorini;
+	double convergenceCriteria;
+	double initialConvergenceCriteria;
+	double constraintConvergenceCriteria[MLCP_NUM_CONSTRAINT_TYPES];
+	double initialConstraintConvergenceCriteria[MLCP_NUM_CONSTRAINT_TYPES];
+
+	// NB: We let the compiler generate the default code for the constructor, copy constructor and copy assignment,
+	// because we currently sometimes need to copy the solution (although we ought to minimize this).
+	// The C++11-ish way to indicate that explicitly would be to write code like this:
+	//     MlcpProblem() = default;
+	//     MlcpProblem(const MlcpProblem& other) = default;
+	//     MlcpProblem& operator= (const MlcpProblem& other) = default;
+	// but I haven't yet tested that this works correctly on VS 2010, so I'm just putting in the comment.
+	// We may also want to add move construction and move assignment.  --advornik 2013-06-24
+};
+
+};  // namespace Math
+};  // namespace SurgSim
+
+#endif // SURGSIM_MATH_MLCPSOLUTION_H
