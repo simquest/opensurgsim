93b9067ba45cead63980f019de28d92c1a657083
Daniel King <dking@simquest.com>
2013-07-02 14:44:47 -0400

    Base graphics texture interfaces - for 1d, 2d, 3d, rectangle, and cube map textures

---
+++ SurgSim/Graphics/Texture.h
@@ -0,0 +1,44 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SURGSIM_GRAPHICS_TEXTURE_H
+#define SURGSIM_GRAPHICS_TEXTURE_H
+
+#include <string>
+
+namespace SurgSim
+{
+
+namespace Graphics
+{
+
+/// Base class defining the interface for a Graphics Texture.
+class Texture
+{
+public:
+	/// Loads an image into the texture from a file
+	/// \param	filePath	Path to the image file
+	/// \return	True if the image is successfully loaded, otherwise false
+	virtual bool loadImage(const std::string& filePath) = 0;
+
+	/// Removes the image from the texture
+	virtual void clearImage() = 0;
+};
+
+};  // namespace Graphics
+
+};  // namespace SurgSim
+
+#endif  // SURGSIM_GRAPHICS_TEXTURE_H
---
+++ SurgSim/Graphics/Texture2d.h
@@ -0,0 +1,49 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SURGSIM_GRAPHICS_TEXTURE2D_H
+#define SURGSIM_GRAPHICS_TEXTURE2D_H
+
+#include <SurgSim/Graphics/Texture.h>
+
+namespace SurgSim
+{
+
+namespace Graphics
+{
+
+/// Base class defining the interface for a 2D Graphics Texture.
+/// A 2D Texture has width and height.
+/// \note	Normalized texture coordinates are used to access this texture.
+class Texture2d : public virtual Texture
+{
+public:
+	/// Sets the size of the texture
+	/// \param	width	Width of the texture
+	/// \param	height	Height of the texture
+	/// \note	Use this to setup a texture as a render target rather than loading from file.
+	virtual void setSize(int width, int height) = 0;
+
+	/// Gets the size of the texture
+	/// \param[out]	width	Width of the texture
+	/// \param[out]	height	Height of the texture
+	virtual void getSize(int* width, int* height) const = 0;
+};
+
+};  // namespace Graphics
+
+};  // namespace SurgSim
+
+#endif  // SURGSIM_GRAPHICS_TEXTURE2D_H
