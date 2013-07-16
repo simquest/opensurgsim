f15c1475b91eb4b810d1dc402142aa3cb2fba8cc
Harald Scheirich <hscheirich@simquest.com>
2013-07-16 16:18:11 -0400

    Update Documentation

--- SurgSim/Testing/MathUtilities.h
+++ SurgSim/Testing/MathUtilities.h
@@ -25,27 +25,50 @@ namespace SurgSim
 {
 namespace Testing
 {
-
+	/// Does a linear interpolation between the start and the end dependent on t.
+	/// \tparam	T	The type of value to be interpolated.
+	/// \param	start	The start value.
+	/// \param	end  	The end value.
+	/// \param	t	 	The percentage for the interpolation.
+	/// \return	the interpolated value.
 	template <class T>
 	T interpolate(const T& start, const T& end,const double& t)
 	{
 		return (1-t)*start + t*end;
 	}
 
-	// Just to make sure delegate to the math version of interpolate for these two
+	/// Specialized template to call the correct function for Quaterniond, might be superfluous,
+	/// delegates to the eigen interpolation function for Quaterniond 
+	/// \param	start	The start quaternion.
+	/// \param	end  	The end quaternion.
+	/// \param	t	 	The percentage for the interpolation.
+	/// \return	the interpolated quaternion.
 	template <>
 	SurgSim::Math::Quaterniond interpolate<SurgSim::Math::Quaterniond>(
 		const SurgSim::Math::Quaterniond& start,
 		const SurgSim::Math::Quaterniond& end,
 		const double& t);
 
+	/// Specialized template to call the correct function for RigidTransform3d, might be superfluous,
+	/// delegates to the eigen interpolation function for RigidTransform3d
+	/// \param	start	The start quaternion.
+	/// \param	end  	The end quaternion.
+	/// \param	t	 	The percentage for the interpolation.
+	/// \return	the interpolated RigidTransform3d.
 	template <>
 	SurgSim::Math::RigidTransform3d interpolate<SurgSim::Math::RigidTransform3d>(
 		const SurgSim::Math::RigidTransform3d& start,
 		const SurgSim::Math::RigidTransform3d& end,
 		const double& t);
 
-
+	/// Does a linear interpolation on a pose, given Vector3d for angles and positions. The angles around the X, Y 
+	/// and Z axis are being passed in a Vector3d for a terser expression. 
+	/// \param	startAngles  	The start angles in the order X/Y/Z angle axis value in radians.
+	/// \param	endAngles	 	The end angles in the order X/Y/Z angle axis value in radians.
+	/// \param	startPosition	The start position.
+	/// \param	endPosition  	The end position.
+	/// \param	t			 	The percentage for the interpolation.
+	/// \return					The transform gained by interpolating and assembling the rotation and position values.
 	SurgSim::Math::RigidTransform3d interpolatePose(
 										const SurgSim::Math::Vector3d& startAngles,
 										const SurgSim::Math::Vector3d& endAngles,
