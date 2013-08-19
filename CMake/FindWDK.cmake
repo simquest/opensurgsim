b6bb472c6fac3505e3021398c495a4527cbf94b5
bert Dvornik <advornik@simquest.com>
2013-08-19 22:17:04 +0200

    Fix WDK detection code to work with both VS2012 and VS2010.
    For VS2012, the copy of the WDK included in Windows Kit 8 (provided
    with the compiler) will be detected and used.  For VS2010, the CMake
    code will attempt to find a copy of WDK 7.1.0. (It may be possible to
    make this work with VS2010 and WDK 8, but this has not been tested in
    any way, so the architecture directory support probably isn't right.)
    
    Note that the external WDK support relies on the fact that adding the
    quoted path ".;$(IncludePath)" to the include path, using CMake's
    include_directories command, works to add $(IncludePath) to the path.
    This relies on some accidents of CMake's implementation [*], but we
    don't know of a better way to do what we need with current CMake.
    
    [*] CMake seems to ALWAYS prefix all include_directories() entries
        with the current directory (unless the entry is an absolute path).
        So adding "$(IncludePath)" actually passes <curdir>/$(IncludePath)
        to Visual Studio, which does not work.
    
        However, including an explicit semicolon passes it to Visual
        Studio without any quoting, and Visual Studio treats it as a path
        separator.  So adding ".;$(IncludePath)" (with quotes) in CMake
        passes <curdir>/.;$(IncludePath) into the Visual Studio include
        list.  Visual Studio splits this at the ';', so we end up with two
        entries: <curdir>/. (which is benign) and $(IncludePath) (which is
        what we needed).
    
    fix #23
    fix #24

--- CMake/FindWDK.cmake
+++ CMake/FindWDK.cmake
@@ -9,64 +9,94 @@
 # Copyright 2013, SimQuest Solutions Inc.
 
 
-# Cache settings and WinDDK environment variables take precedence,
-# or we try to fall back to the default search.
+# We try finding the necessary files in the compiler's default include
+# and library paths.  If not found, then cache settings and WinDDK
+# environment variables take precedence, or we try to fall back to the
+# default search.
 
-find_path(WDK_INCLUDE_DIR
-	NAMES hidsdi.h
-	PATHS "$ENV{WINDDK_PATH}" "$ENV{WINDDK_ROOT}" "$ENV{WDK_PATH}"
-	PATH_SUFFIXES inc/api
-	NO_CMAKE_ENVIRONMENT_PATH NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH
-)
-find_path(WDK_INCLUDE_DIR
-	NAMES hidsdi.h
-	PATH_SUFFIXES inc/api
-)
+if(NOT DEFINED WDK_CAN_BUILD_DIRECTLY)
+	# Note: check_include_files will NOT find hidsdi.h, even if it's
+	#   present in the compiler's default search path!  Sigh.
+	try_compile(WDK_CAN_BUILD_DIRECTLY
+		${CMAKE_BINARY_DIR}/CMakeFiles/tryCompileWdk
+		${CMAKE_SOURCE_DIR}/CMake/tryCompileWdk  tryCompileWdk
+		OUTPUT_VARIABLE dbg_tryCompileWdk
+	)
+	message("OUT:${dbg_tryCompileWdk}")
+endif(NOT DEFINED WDK_CAN_BUILD_DIRECTLY)
+	
+if(WDK_CAN_BUILD_DIRECTLY)
+	# No include directory is needed.  Set it to . for convenience in other files.
+	set(WDK_INCLUDE_DIR . CACHE PATH "Include directory for the WDK.")
+	set(WDK_location provided_by_compiler)
+else()
+	# Find the include directory.
+	find_path(WDK_INCLUDE_DIR
+		NAMES hidsdi.h
+		PATHS "$ENV{WINDDK_PATH}" "$ENV{WINDDK_ROOT}" "$ENV{WDK_PATH}"
+		PATH_SUFFIXES inc/api
+		NO_CMAKE_ENVIRONMENT_PATH NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH
+	)
+	find_path(WDK_INCLUDE_DIR
+		NAMES hidsdi.h
+		PATH_SUFFIXES inc/api
+	)
+	set(WDK_location ${WDK_INCLUDE_DIR})
+	if(WDK_INCLUDE_DIR)
+		get_filename_component(WDK_DIR_UP1
+			${WDK_INCLUDE_DIR} PATH)
+		get_filename_component(WDK_ROOT_DIR
+			${WDK_DIR_UP1} PATH)
+	endif(WDK_INCLUDE_DIR)
+endif()
 mark_as_advanced(WDK_INCLUDE_DIR)
 
 # magical WinDDK system subdirectory names
-set(WDK_os_winXP "wxp")
-set(WDK_os_server2003 "wnet")
-set(WDK_os_winVista "wlh")
-set(WDK_os_server2008 "wlh")
-set(WDK_os_win7 "win7")
+set(WDK710_os_winXP "wxp")
+set(WDK710_os_server2003 "wnet")
+set(WDK710_os_winVista "wlh")
+set(WDK710_os_server2008 "wlh")
+set(WDK710_os_win7 "win7")
 
-set(LIB_ARCH "unknown")
+set(WDK710_LIB_ARCH "unknown")
 if(CMAKE_CL_64)
-	set(LIB_ARCH "amd64")
+	set(WDK710_LIB_ARCH "amd64")
 else()
-	set(LIB_ARCH "i386")
+	set(WDK710_LIB_ARCH "i386")
 endif()
 
-if(WDK_INCLUDE_DIR)
-	get_filename_component(WDK_DIR_UP1
-		${WDK_INCLUDE_DIR} PATH)
-	get_filename_component(WDK_ROOT_DIR
-		${WDK_DIR_UP1} PATH)
-endif(WDK_INCLUDE_DIR)
 
 
 macro(winddk_find_library LIB_NAME)
-	# find the library in the oldest available OS version directory
-	find_library(WDK_${LIB_NAME}_LIBRARY
-		NAMES ${LIB_NAME}
-		HINTS "${WDK_ROOT_DIR}"
-		PATH_SUFFIXES "lib/${WDK_os_winXP}/${LIB_ARCH}"
-			"lib/${WDK_os_server2003}/${LIB_ARCH}"
-			"lib/${WDK_os_winVista}/${LIB_ARCH}"
-			"lib/${WDK_os_server2008}/${LIB_ARCH}"
-			"lib/${WDK_os_win7}/${LIB_ARCH}"
-		NO_CMAKE_ENVIRONMENT_PATH NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH
-	)
-	find_library(WDK_${LIB_NAME}_LIBRARY
-		NAMES ${LIB_NAME}
-		HINTS "${WDK_ROOT_DIR}"
-		PATH_SUFFIXES "lib/${WDK_os_winXP}/${LIB_ARCH}"
-			"lib/${WDK_os_server2003}/${LIB_ARCH}"
-			"lib/${WDK_os_winVista}/${LIB_ARCH}"
-			"lib/${WDK_os_server2008}/${LIB_ARCH}"
-			"lib/${WDK_os_win7}/${LIB_ARCH}"
-	)
+	if(WDK_CAN_BUILD_DIRECTLY)
+		# just assume we can get at this library.
+		# Note: find_library and check_library_exists will NOT find the
+		#   WDK 8 libraries that are present in the compiler's default
+		#   search path!  Sigh.
+		set(WDK_${LIB_NAME}_LIBRARY ${LIB_NAME}
+			CACHE FILEPATH "Library for the WDK.")
+	else()
+		# find the library in the oldest available OS version directory
+		find_library(WDK_${LIB_NAME}_LIBRARY
+			NAMES ${LIB_NAME}
+			HINTS "${WDK_ROOT_DIR}"
+			PATH_SUFFIXES "lib/${WDK710_os_winXP}/${WDK710_LIB_ARCH}"
+				"lib/${WDK710_os_server2003}/${WDK710_LIB_ARCH}"
+				"lib/${WDK710_os_winVista}/${WDK710_LIB_ARCH}"
+				"lib/${WDK710_os_server2008}/${WDK710_LIB_ARCH}"
+				"lib/${WDK710_os_win7}/${WDK710_LIB_ARCH}"
+			NO_CMAKE_ENVIRONMENT_PATH NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH
+		)
+		find_library(WDK_${LIB_NAME}_LIBRARY
+			NAMES ${LIB_NAME}
+			HINTS "${WDK_ROOT_DIR}"
+			PATH_SUFFIXES "lib/${WDK710_os_winXP}/${WDK710_LIB_ARCH}"
+				"lib/${WDK710_os_server2003}/${WDK710_LIB_ARCH}"
+				"lib/${WDK710_os_winVista}/${WDK710_LIB_ARCH}"
+				"lib/${WDK710_os_server2008}/${WDK710_LIB_ARCH}"
+				"lib/${WDK710_os_win7}/${WDK710_LIB_ARCH}"
+		)
+	endif()
 	mark_as_advanced(WDK_${LIB_NAME}_LIBRARY)
 endmacro()
 
@@ -75,7 +105,8 @@ winddk_find_library(setupapi)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(WDK
-	DEFAULT_MSG WDK_INCLUDE_DIR WDK_hid_LIBRARY WDK_setupapi_LIBRARY)
+	DEFAULT_MSG WDK_location
+	WDK_INCLUDE_DIR WDK_hid_LIBRARY WDK_setupapi_LIBRARY)
 
 if(WDK_FOUND)
 	set(WDK_LIBRARIES
