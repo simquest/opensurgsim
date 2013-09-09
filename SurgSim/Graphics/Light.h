f1c0463bdb09225543b46330594fc777efe342f5
Harald Scheirich <hscheirich@simquest.com>
2013-09-09 17:20:01 -0400

    Implementation of Light Object as a Representation

---
+++ SurgSim/Graphics/Light.h
@@ -0,0 +1,120 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SURGSIM_GRAPHICS_LIGHT_H
+#define SURGSIM_GRAPHICS_LIGHT_H
+
+#include <SurgSim/Graphics/Representation.h>
+#include <SurgSim/Math/Vector.h>
+
+namespace SurgSim
+{
+namespace Graphics
+{
+
+class Group;
+
+/// Abstract interface for a light, a light needs to be assigned to a group to be active, only the members of this
+/// group will be considered to be lit by this light. Currently this light implements a pointlight. It will have to
+/// be extended for a directional and spot lights. The class should provide the following uniform values. The position
+/// is set by using the representations setPose() call
+/// \code
+/// uniform vec3 ossLightPosition
+/// uniform vec4 ossLightAmbientColor
+/// uniform vec4 ossLightDiffuseColor
+/// uniform vec4 ossLightSpecularColor
+/// uniform float ossLightConstantAttenuation
+/// uniform float ossLightLinearAttenuation
+/// uniform float ossLightQuadraticAttenuation
+/// \endcode
+/// 
+class Light : public virtual Representation
+{
+public:
+
+	/// Constructor
+	explicit Light(const std::string& name) : Representation(name) 
+	{
+	}
+
+	virtual ~Light()
+	{
+	}
+
+	/// Sets the group for this light, setting nullptr here will remove the light from its current group
+	/// \param	group	The group.
+	/// \return	true if it succeeds, false if the group is not an OsgGroup.
+	virtual bool setGroup(std::shared_ptr<SurgSim::Graphics::Group> group) = 0;
+
+	/// Gets the group that this light has been assigned to.
+	/// \return	The group or nullptr if no group has been set.
+	virtual std::shared_ptr<SurgSim::Graphics::Group> getGroup() = 0;
+
+	/// Sets ambient color of this light
+	/// \param	color	The color.
+	virtual void setAmbientColor(const SurgSim::Math::Vector4d& color) = 0;
+
+	/// Gets ambient color.
+	/// \return	The ambient color.
+	virtual SurgSim::Math::Vector4d getAmbientColor() = 0;
+
+	/// Sets diffuse color of this light.
+	/// \param	color	The color.
+	virtual void setDiffuseColor(const SurgSim::Math::Vector4d& color) = 0;
+
+	/// Gets diffuse color.
+	/// \return	The diffuse color.
+	virtual SurgSim::Math::Vector4d getDiffuseColor() = 0;
+
+	/// Sets specular color of this light.
+	/// \param	color	The color.
+	virtual void setSpecularColor(const SurgSim::Math::Vector4d& color) = 0;
+
+	/// Gets specular color.
+	/// \return	The specular color.
+	virtual SurgSim::Math::Vector4d getSpecularColor() = 0;
+
+	/// Sets constant attenuation.
+	/// \param	val	The value.
+	virtual void setConstantAttenuation(double val) = 0;
+
+	/// Gets constant attenuation.
+	/// \return	The constant attenuation.
+	virtual double getConstantAttenuation() = 0;
+
+	/// Sets linear attenuation.
+	/// \param	val	The value.
+	virtual void setLinearAttenuation(double val) = 0;
+
+	/// Gets linear attenuation.
+	/// \return	The linear attenuation.
+	virtual double getLinearAttenuation() = 0;
+
+	/// Sets quadratic attenuation.
+	/// \param	val	The value.
+	virtual void setQuadraticAttenuation(double val) = 0;
+
+	/// Gets quadratic attenuation.
+	/// \return	The quadratic attenuation.
+	virtual double getQuadraticAttenuation() = 0;
+	
+private:
+
+};
+
+}; // Graphics
+}; // SurgSim
+
+#endif
---
+++ SurgSim/Graphics/OsgLight.cpp
@@ -0,0 +1,214 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+
+#include <SurgSim/Graphics/OsgLight.h>
+
+#include <SurgSim/Framework/Log.h>
+#include <SurgSim/Graphics/OsgGroup.h>
+#include <SurgSim/Graphics/OsgConversions.h>
+
+#include <SurgSim/Math/Vector.h>
+#include <SurgSim/Math/Quaternion.h>
+#include <SurgSim/Math/RigidTransform.h>
+
+
+using SurgSim::Math::Vector3d;
+using SurgSim::Math::Quaterniond;
+using SurgSim::Math::RigidTransform3d;
+
+#include <osg/Uniform>
+
+using osg::Uniform;
+
+namespace SurgSim
+{
+namespace Graphics
+{
+
+/// \note HS-2013-sep-09 Right now we are implementing all the shader uniforms as floats, this
+/// 	  means that they all have to be downconverted from double, i don't know what the hit 
+/// 	  of going to double in the shaders would be
+OsgLight::OsgLight(const std::string& name) :
+	OsgRepresentation(name),
+	Representation(name),
+	Light(name),
+	m_ambientColor(0.0,0.0,0.0,1.0),
+	m_diffuseColor(1.0,1.0,1.0,1.0),
+	m_specularColor(1.0,1.0,1.0,1.0),
+	m_constantAttenuation(1.0),
+	m_linearAttenuation(0.0),
+	m_quadraticAttenuation(0.0)
+{
+	m_uniforms[POSITION] = new osg::Uniform(Uniform::FLOAT_VEC3,"ossLightPosition");
+	setPose(getPose());
+
+	m_uniforms[AMBIENT_COLOR] = new osg::Uniform(Uniform::FLOAT_VEC4, "ossLightAmbientColor");
+	setAmbientColor(m_ambientColor);
+
+	m_uniforms[DIFFUSE_COLOR] = new osg::Uniform(Uniform::FLOAT_VEC4, "ossLightDiffuseColor");
+	setDiffuseColor(m_diffuseColor);
+
+	m_uniforms[SPECULAR_COLOR] = new osg::Uniform(Uniform::FLOAT_VEC4, "ossLightSpecularColor");
+	setSpecularColor(m_specularColor);
+
+	m_uniforms[CONSTANT_ATTENUATION] = new osg::Uniform(Uniform::FLOAT, "ossLightConstantAttenuation");
+	setConstantAttenuation(m_constantAttenuation);
+
+	m_uniforms[LINEAR_ATTENUATION] = new osg::Uniform(Uniform::FLOAT, "ossLightLinearAttenuation");
+	setLinearAttenuation(m_linearAttenuation);
+
+	m_uniforms[QUADRATIC_ATTENUATION] = new osg::Uniform(Uniform::FLOAT, "ossLightQuadraticAttenuation");
+	setQuadraticAttenuation(m_quadraticAttenuation);
+}
+
+OsgLight::~OsgLight()
+{
+
+}
+
+bool OsgLight::setGroup(std::shared_ptr<SurgSim::Graphics::Group> group)
+{
+	std::shared_ptr<OsgGroup> newGroup = std::dynamic_pointer_cast<OsgGroup>(group);
+
+	if (group != nullptr && newGroup == nullptr)
+	{
+		SURGSIM_LOG_WARNING(SurgSim::Framework::Logger::getLogger("Graphics")) 
+			<< "OsgLight::setGroup() called with a group that is not an OsgGroupl.";
+	}
+
+	bool clearGroup = m_group != nullptr && (newGroup != nullptr || group == nullptr);
+	bool setGroup = (newGroup != nullptr);
+	bool success  = (group != nullptr && newGroup != nullptr) || group == nullptr;
+	
+	if (clearGroup)
+	{
+		osg::ref_ptr<osg::StateSet> stateSet = m_group->getOsgGroup()->getOrCreateStateSet();
+		remove(stateSet);
+		m_group = nullptr;
+	}
+
+	if (setGroup)
+	{
+		osg::ref_ptr<osg::StateSet> stateSet = newGroup->getOsgGroup()->getOrCreateStateSet();
+		apply(stateSet);
+		m_group = newGroup;
+	}
+
+	return success;
+}
+
+std::shared_ptr<SurgSim::Graphics::Group> OsgLight::getGroup()
+{
+	return m_group;
+}
+
+void OsgLight::setAmbientColor(const SurgSim::Math::Vector4d& color)
+{
+	m_ambientColor = color;
+	SurgSim::Math::Vector4f floatColor = color.cast<float>();
+	m_uniforms[AMBIENT_COLOR]->set(toOsg(floatColor));
+}
+
+SurgSim::Math::Vector4d OsgLight::getAmbientColor()
+{
+	return m_ambientColor;
+}
+
+void OsgLight::setDiffuseColor(const SurgSim::Math::Vector4d& color)
+{
+	m_diffuseColor = color;
+	SurgSim::Math::Vector4f floatColor = color.cast<float>();
+	m_uniforms[DIFFUSE_COLOR]->set(toOsg(floatColor));
+}
+
+SurgSim::Math::Vector4d OsgLight::getDiffuseColor()
+{
+	return m_diffuseColor;
+}
+
+void OsgLight::setSpecularColor(const SurgSim::Math::Vector4d& color)
+{
+	m_specularColor = color;
+	SurgSim::Math::Vector4f floatColor = color.cast<float>();
+	m_uniforms[SPECULAR_COLOR]->set(toOsg(floatColor));
+}
+
+SurgSim::Math::Vector4d OsgLight::getSpecularColor()
+{
+	return m_specularColor;
+}
+
+void OsgLight::setConstantAttenuation(double val)
+{
+	m_constantAttenuation = val;
+	m_uniforms[CONSTANT_ATTENUATION]->set(static_cast<float>(val));
+}
+
+double OsgLight::getConstantAttenuation()
+{
+	return m_constantAttenuation;
+}
+
+void OsgLight::setLinearAttenuation(double val)
+{
+	m_linearAttenuation = val;
+	m_uniforms[LINEAR_ATTENUATION]->set(static_cast<float>(val));
+}
+
+double OsgLight::getLinearAttenuation()
+{
+	return m_linearAttenuation;
+}
+
+void OsgLight::setQuadraticAttenuation(double val)
+{
+	m_quadraticAttenuation = val;
+	m_uniforms[QUADRATIC_ATTENUATION]->set(static_cast<float>(val));
+}
+
+double OsgLight::getQuadraticAttenuation()
+{
+	return m_quadraticAttenuation;
+}
+
+void OsgLight::apply(osg::ref_ptr<osg::StateSet> stateSet)
+{
+	for (auto it = std::begin(m_uniforms); it != std::end(m_uniforms); ++it)
+	{
+		stateSet->addUniform(it->second);
+	}
+}
+
+void OsgLight::remove(osg::ref_ptr<osg::StateSet> stateSet)
+{
+	for (auto it = std::begin(m_uniforms); it != std::end(m_uniforms); ++it)
+	{
+		stateSet->removeUniform(it->second);
+	}
+}
+
+void OsgLight::setPose(const SurgSim::Math::RigidTransform3d& pose)
+{
+	OsgRepresentation::setPose(pose);
+	SurgSim::Math::Vector3f position = pose.translation().cast<float>();
+	m_uniforms[POSITION]->set(toOsg(position));
+}
+
+
+
+}; // Graphics
+}; // SurgSim
