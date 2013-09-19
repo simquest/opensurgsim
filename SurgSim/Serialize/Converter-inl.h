0c3178325e0ff1822102245013250ce88ce0b41b
vientran <vtran@simquest.com>
2013-09-19 14:36:08 -0400

    Specialize YAML::convert<> template to serialize math components with unit tests.

---
+++ SurgSim/Serialize/Converter-inl.h
@@ -0,0 +1,140 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SURGSIM_SERIALIZE_CONVERTER_INL_H
+#define SURGSIM_SERIALIZE_CONVERTER_INL_H
+
+#include <yaml-cpp/yaml.h>
+#include <SurgSim/Math/Vector.h>
+#include <SurgSim/Math/Matrix.h>
+#include <SurgSim/Math/Quaternion.h>
+#include <SurgSim/Math/RigidTransform.h>
+
+namespace YAML
+{
+	/// Specialize of YAML::convert<> template vector3d class.
+	template <>
+	struct convert <SurgSim::Math::Vector3d> {
+		static Node encode(const SurgSim::Math::Vector3d& rhs) {
+			Node node;
+			node.push_back(rhs[0]);
+			node.push_back(rhs[1]);
+			node.push_back(rhs[2]);
+			return node;
+		}
+
+		static bool decode(const Node &node, SurgSim::Math::Vector3d& rhs) {
+			if (! node.IsSequence() || node.size() != 3)
+				return false;
+			rhs[0] = node[0].as<double>();
+			rhs[1] = node[1].as<double>();
+			rhs[2] = node[2].as<double>();
+			return true;
+		}
+	};
+
+
+	/// Specialize of YAML::convert<> template vector4d class.
+	template <>
+	struct convert <SurgSim::Math::Vector4d> {
+		static Node encode(const SurgSim::Math::Vector4d& rhs) {
+			Node node;
+			node.push_back(rhs[0]);
+			node.push_back(rhs[1]);
+			node.push_back(rhs[2]);
+			node.push_back(rhs[3]);
+			return node;
+		}
+
+		static bool decode(const Node& node, SurgSim::Math::Vector4d& rhs) {
+			if (! node.IsSequence() || node.size() != 4 )
+				return false;
+			rhs[0] = node[0].as<double>();
+			rhs[1] = node[1].as<double>();
+			rhs[2] = node[2].as<double>();
+			rhs[3] = node[3].as<double>();
+			return true;
+		}
+	};
+
+	/// Specialize of YAML::convert<> template quanterniond class.
+	template <>
+	struct convert <SurgSim::Math::Quaterniond> {
+		static Node encode(const SurgSim::Math::Quaterniond& rhs) {
+			
+			Node node;
+			node = convert<SurgSim::Math::Vector4d>::encode(rhs.coeffs());
+			return node;
+		}
+
+		static bool decode(const Node& node, SurgSim::Math::Quaterniond& rhs) {
+			
+			SurgSim::Math::Vector4d coeffs;
+			convert<SurgSim::Math::Vector4d>::decode(node, rhs.coeffs());
+			return true;
+		}
+	};
+
+	/// Specialize of YAML::convert<> template maxtrix44d class.
+	template <>
+	struct convert <SurgSim::Math::Matrix44d> {
+		static Node encode(const SurgSim::Math::Matrix44d& rhs) {
+
+			Node node;
+			/// A row-major encoding
+			for (auto row = 0; row < rhs.rows(); ++row)
+				node.push_back(convert<SurgSim::Math::Vector4d>::encode(rhs.row(row)));
+			return node;
+		}
+
+		static bool decode(const Node& node, SurgSim::Math::Matrix44d& rhs) {
+			if (! node.IsSequence() || rhs.rows() != 4 || rhs.cols() != 4)
+				return false;
+
+			/// A row-major decoding
+			for (auto row = 0; row < rhs.rows(); ++row)
+			{
+				SurgSim::Math::Vector4d vector4d;
+				convert<SurgSim::Math::Vector4d>::decode(node[row], vector4d);
+				rhs.row(row) = vector4d;
+			}
+			return true;
+		}
+	};
+
+	/// Specialize of YAML::convert<> template RigidTransform class.
+	template <>
+	struct convert <SurgSim::Math::RigidTransform3d> {
+		static Node encode(const SurgSim::Math::RigidTransform3d& rhs) {
+
+			Node node;
+			SurgSim::Math::Matrix44d mTransform = rhs.matrix();
+			node = convert<SurgSim::Math::Matrix44d>::encode(mTransform);
+			return node;
+		}
+
+		static bool decode(const Node& node, SurgSim::Math::RigidTransform3d& rhs) {
+
+			SurgSim::Math::Matrix44d mTransform;
+			convert<SurgSim::Math::Matrix44d>::decode(node, mTransform);
+			rhs.matrix() = mTransform;
+
+			return true;
+		}
+	};
+
+}
+
+#endif // SURGSIM_SERIALIZE_CONVERTER_INL_H
---
+++ SurgSim/Serialize/UnitTests/ConverterTest.cpp
@@ -0,0 +1,109 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include <fstream>
+#include <gtest/gtest.h>
+#include <SurgSim/Serialize/Converter.h>
+
+TEST(SerializeTest, ConverterVector3dTest)
+{
+	std::ofstream fout("config_vector3d.yaml");		
+	YAML::Node outnode;
+
+	SurgSim::Math::Vector3d vector3(1.000001, 2.000001, 3.000001);
+	outnode.push_back(vector3);
+	fout << outnode;
+	fout.close();
+
+	YAML::Node innode = YAML::LoadFile("config_vector3d.yaml");
+	SurgSim::Math::Vector3d expectedv3d = innode[0].as<SurgSim::Math::Vector3d>();
+	EXPECT_EQ(expectedv3d, vector3);
+}
+
+TEST(SerializeTest, ConverterVector4dTest)
+{
+	std::ofstream fout("config_vector4d.yaml");		
+	YAML::Node outnode;
+
+	SurgSim::Math::Vector4d vector4(1.000001, 2.000001, 3.000001, 4.000001);
+	outnode.push_back(vector4);
+	fout << outnode;
+	fout.close();
+
+	YAML::Node innode = YAML::LoadFile("config_vector4d.yaml");
+	SurgSim::Math::Vector4d expectedv4d = innode[0].as<SurgSim::Math::Vector4d>();
+	EXPECT_EQ(expectedv4d, vector4);
+}
+
+
+TEST(SerializeTest, ConverterQuaterniondTest)
+{
+	std::ofstream fout("config_quaterniond.yaml");		
+	YAML::Node outnode;
+
+	SurgSim::Math::Quaterniond quat(3, 2, 1, 1);
+	outnode.push_back(quat);
+	fout << outnode;
+	fout.close();
+
+	YAML::Node innode = YAML::LoadFile("config_quaterniond.yaml");
+	SurgSim::Math::Quaterniond expectedQuat = innode[0].as<SurgSim::Math::Quaterniond>();
+
+	EXPECT_EQ(expectedQuat.x(), quat.x());
+	EXPECT_EQ(expectedQuat.y(), quat.y());
+	EXPECT_EQ(expectedQuat.z(), quat.z());
+	EXPECT_EQ(expectedQuat.w(), quat.w());
+}
+
+
+TEST(SerializeTest, ConverterMatrix44dTest)
+{
+	std::ofstream fout("config_matrix44d.yaml");		
+	YAML::Node outnode;
+
+	SurgSim::Math::Matrix44d mat44d;
+	mat44d <<	1, 2, 3, 4,
+				4, 3, 2, 1,
+				1, 2, 3, 4,
+				4, 3, 2, 1;
+
+	outnode.push_back(mat44d);
+	fout << outnode;
+	fout.close();
+
+	YAML::Node innode = YAML::LoadFile("config_matrix44d.yaml");
+	SurgSim::Math::Matrix44d expectedm44d = innode[0].as<SurgSim::Math::Matrix44d>();
+	
+	EXPECT_EQ(expectedm44d, mat44d);
+}
+
+TEST(SerializeTest, ConverterRigidTransform3dTest)
+{
+	std::ofstream fout("config_rigidtransform3d.yaml");		
+	YAML::Node outnode;
+
+	SurgSim::Math::RigidTransform3d rigid = SurgSim::Math::makeRigidTransform(SurgSim::Math::Quaterniond::Identity(),
+		SurgSim::Math::Vector3d(1.0, 2.0, 3.0));
+
+	outnode.push_back(rigid);
+	fout << outnode;
+	fout.close();
+
+	YAML::Node innode = YAML::LoadFile("config_rigidtransform3d.yaml");
+	SurgSim::Math::RigidTransform3d expectedRigid = innode[0].as<SurgSim::Math::RigidTransform3d>();
+
+	EXPECT_EQ(expectedRigid.matrix(), rigid.matrix());
+}
