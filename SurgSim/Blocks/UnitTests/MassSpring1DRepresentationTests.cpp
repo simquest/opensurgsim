c36c67835478645f0d3229909a5d6767bf654ebe
Julien Lenoir <jlenoir@simquest.com>
2013-10-24 15:57:46 -0400

    Missing UnitTest in Blocks (MassSpringnD + TransferDeformableStateToVerticesBehavior)

---
+++ SurgSim/Blocks/UnitTests/MassSpring1DRepresentationTests.cpp
@@ -0,0 +1,106 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/// \file
+/// Tests for the MassSpring1DRepresentation class.
+
+#include <gtest/gtest.h>
+
+#include<SurgSim/Blocks/MassSpring1DRepresentation.h>
+using SurgSim::Blocks::MassSpring1DRepresentation;
+
+#include <SurgSim/Physics/DeformableRepresentationState.h>
+#include<SurgSim/Physics/LinearSpring.h>
+using SurgSim::Physics::DeformableRepresentationState;
+using SurgSim::Physics::LinearSpring;
+
+static void springTest(std::shared_ptr<LinearSpring> spring,
+	std::shared_ptr<DeformableRepresentationState> state,
+	unsigned int expectedNodeId0, unsigned int expectedNodeId1,
+	double expectedStiffness, double expectedDamping)
+{
+	EXPECT_DOUBLE_EQ(expectedStiffness, spring->getStiffness());
+	EXPECT_DOUBLE_EQ(expectedDamping, spring->getDamping());
+	EXPECT_EQ(expectedNodeId0, spring->getNodeId(0));
+	EXPECT_EQ(expectedNodeId1, spring->getNodeId(1));
+	Vector& x =state->getPositions();
+	Eigen::VectorBlock<Vector> x0 = SurgSim::Math::getSubVector(x, expectedNodeId0, 3);
+	Eigen::VectorBlock<Vector> x1 = SurgSim::Math::getSubVector(x, expectedNodeId1, 3);
+	EXPECT_DOUBLE_EQ((x1 - x0).norm(), spring->getRestLength());
+}
+
+TEST(MassSpring1DRepresentationTests, init1DTest)
+{
+	using SurgSim::Math::Vector3d;
+
+	MassSpring1DRepresentation m("MassSpring1D");
+
+	std::array<Vector3d, 2> extremities = {{ Vector3d::Random(), Vector3d::Random() }};
+	unsigned int numNodesPerDim[1] = {10};
+	std::vector<unsigned int> boundaryConditions;
+	double totalMass = 1.1;
+	double stiffnessStretching = 2.2;
+	double dampingStretching = 3.3;
+	double stiffnessBending = 4.4;
+	double dampingBending = 5.5;
+	boundaryConditions.push_back(0);
+	boundaryConditions.push_back(numNodesPerDim[0] - 1);
+	m.init1D(extremities, numNodesPerDim, boundaryConditions,
+		totalMass, stiffnessStretching, dampingStretching, stiffnessBending, dampingBending);
+
+	EXPECT_EQ(numNodesPerDim[0] * 3, m.getNumDof());
+	EXPECT_EQ(numNodesPerDim[0], m.getNumMasses());
+	EXPECT_EQ(numNodesPerDim[0] - 1 + numNodesPerDim[0] - 2, m.getNumSprings());
+
+	for (unsigned int massId = 0; massId < m.getNumMasses(); massId++)
+	{
+		EXPECT_DOUBLE_EQ(totalMass/numNodesPerDim[0], m.getMass(massId)->getMass());
+	}
+
+	// The 1st springs are the stretching springs
+	unsigned int springId = 0;
+	for (unsigned int nodeId = 0; nodeId < m.getNumMasses() - 1; nodeId++)
+	{
+		springTest(std::dynamic_pointer_cast<LinearSpring>(m.getSpring(springId)), m.getFinalState(),
+			nodeId, nodeId + 1, stiffnessStretching, dampingStretching);
+		springId++;
+	}
+
+	// Followed by bending springs
+	for (unsigned int nodeId = 0; nodeId < m.getNumMasses() - 2; nodeId++)
+	{
+		springTest(std::dynamic_pointer_cast<LinearSpring>(m.getSpring(springId)), m.getFinalState(),
+			nodeId, nodeId + 2, stiffnessBending, dampingBending);
+		springId++;
+	}
+
+	// States all equals
+	EXPECT_EQ(*m.getPreviousState(), *m.getFinalState());
+	EXPECT_EQ(*m.getCurrentState(), *m.getFinalState());
+	EXPECT_EQ(*m.getInitialState(), *m.getFinalState());
+
+	// States should contains expected values
+	Vector3d delta = (extremities[1] - extremities[0]) / (numNodesPerDim[0] - 1);
+	EXPECT_TRUE(m.getFinalState()->getVelocities().isZero());
+	EXPECT_TRUE(m.getFinalState()->getAccelerations().isZero());
+	EXPECT_FALSE(m.getFinalState()->getPositions().isZero());
+	for (unsigned int nodeId = 0; nodeId < numNodesPerDim[0]; nodeId++)
+	{
+		Vector3d piExpected = extremities[0] + delta * nodeId;
+		Eigen::VectorBlock<Vector> pi = SurgSim::Math::getSubVector(m.getFinalState()->getPositions(), nodeId, 3);
+		EXPECT_TRUE(pi.isApprox(piExpected));
+	}
+	EXPECT_EQ(boundaryConditions, m.getFinalState()->getBoundaryConditions());
+}
---
+++ SurgSim/Blocks/UnitTests/MassSpring2DRepresentationTests.cpp
@@ -0,0 +1,188 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/// \file
+/// Tests for the MassSpring2DRepresentation class.
+
+#include <gtest/gtest.h>
+
+#include<SurgSim/Blocks/MassSpring2DRepresentation.h>
+using SurgSim::Blocks::MassSpring2DRepresentation;
+
+#include <SurgSim/Physics/DeformableRepresentationState.h>
+#include<SurgSim/Physics/LinearSpring.h>
+using SurgSim::Physics::DeformableRepresentationState;
+using SurgSim::Physics::LinearSpring;
+
+static void springTest(std::shared_ptr<LinearSpring> spring,
+	std::shared_ptr<DeformableRepresentationState> state,
+	unsigned int expectedNodeId0, unsigned int expectedNodeId1,
+	double expectedStiffness, double expectedDamping)
+{
+	EXPECT_DOUBLE_EQ(expectedStiffness, spring->getStiffness());
+	EXPECT_DOUBLE_EQ(expectedDamping, spring->getDamping());
+	EXPECT_EQ(expectedNodeId0, spring->getNodeId(0));
+	EXPECT_EQ(expectedNodeId1, spring->getNodeId(1));
+	Vector& x = state->getPositions();
+	Eigen::VectorBlock<Vector> x0 = SurgSim::Math::getSubVector(x, expectedNodeId0, 3);
+	Eigen::VectorBlock<Vector> x1 = SurgSim::Math::getSubVector(x, expectedNodeId1, 3);
+	EXPECT_DOUBLE_EQ((x1 - x0).norm(), spring->getRestLength());
+}
+
+TEST(MassSpring2DRepresentationTests, init2DTest)
+{
+	using SurgSim::Math::Vector3d;
+
+	MassSpring2DRepresentation m("MassSpring2D");
+
+	std::array<std::array<Vector3d, 2>, 2> extremities =
+	{{
+		{{Vector3d::Random(), Vector3d::Random()}},
+		{{Vector3d::Random(), Vector3d::Random()}}
+	}};
+	unsigned int numNodesPerDim[2] = {10, 5};
+	std::vector<unsigned int> boundaryConditions;
+	double totalMass = 1.1;
+	double stiffnessStretching = 2.2;
+	double dampingStretching = 3.3;
+	double stiffnessBending = 4.4;
+	double dampingBending = 5.5;
+	double stiffnessFaceDiagonal = 6.6;
+	double dampingFaceDiagonal = 7.7;
+	boundaryConditions.push_back(0);
+	boundaryConditions.push_back(numNodesPerDim[0] * numNodesPerDim[1] - 1);
+	m.init2D(extremities, numNodesPerDim, boundaryConditions,
+		totalMass,
+		stiffnessStretching, dampingStretching,
+		stiffnessBending, dampingBending,
+		stiffnessFaceDiagonal, dampingFaceDiagonal);
+
+	const int numNodes = numNodesPerDim[0] * numNodesPerDim[1];
+	EXPECT_EQ(numNodes * 3, m.getNumDof());
+	EXPECT_EQ(numNodes, m.getNumMasses());
+	unsigned int numSpringsExpected = 0;
+	numSpringsExpected += numNodesPerDim[1] * (numNodesPerDim[0] - 1); // Stretching along Y
+	numSpringsExpected += numNodesPerDim[0] * (numNodesPerDim[1] - 1); // Stretching along X
+	numSpringsExpected += numNodesPerDim[1] * (numNodesPerDim[0] - 2); // Bending along Y
+	numSpringsExpected += numNodesPerDim[0] * (numNodesPerDim[1] - 2); // Bending along X
+	numSpringsExpected += (numNodesPerDim[0] - 1) * (numNodesPerDim[1] - 1) * 2; // Face diagonal
+	EXPECT_EQ(numSpringsExpected, m.getNumSprings());
+
+	for (unsigned int massId = 0; massId < m.getNumMasses(); massId++)
+	{
+		EXPECT_DOUBLE_EQ(totalMass/numNodes, m.getMass(massId)->getMass());
+	}
+
+	const int rowOffset = numNodesPerDim[0];
+	const int colOffset = 1;
+
+	unsigned int springId = 0;
+	// The 1st springs are the stretching springs along X
+	for (unsigned int row = 0; row < numNodesPerDim[1]; row++)
+	{
+		for (unsigned int col = 0; col < numNodesPerDim[0] - 1; col++)
+		{
+			unsigned int nodeId = row * rowOffset + col * colOffset;
+
+			springTest( std::dynamic_pointer_cast<LinearSpring>(m.getSpring(springId)), m.getFinalState(),
+				nodeId, nodeId + colOffset, stiffnessStretching, dampingStretching);
+			springId++;
+		}
+	}
+	// Followed by stretching springs along Y
+	for (unsigned int col = 0; col < numNodesPerDim[0]; col++)
+	{
+		for (unsigned int row = 0; row < numNodesPerDim[1] - 1; row++)
+		{
+			unsigned int nodeId = row * rowOffset + col * colOffset;
+
+			springTest( std::dynamic_pointer_cast<LinearSpring>(m.getSpring(springId)), m.getFinalState(),
+				nodeId, nodeId + rowOffset, stiffnessStretching, dampingStretching);
+			springId++;
+		}
+	}
+	// Followed by bending springs along X
+	for (unsigned int row = 0; row < numNodesPerDim[1]; row++)
+	{
+		for (unsigned int col = 0; col < numNodesPerDim[0] - 2; col++)
+		{
+			unsigned int nodeId = row * rowOffset + col * colOffset;
+
+			springTest( std::dynamic_pointer_cast<LinearSpring>(m.getSpring(springId)), m.getFinalState(),
+				nodeId, nodeId + 2 * colOffset, stiffnessBending, dampingBending);
+			springId++;
+		}
+	}
+	// Followed by bending springs along Y
+	for (unsigned int col = 0; col < numNodesPerDim[0]; col++)
+	{
+		for (unsigned int row = 0; row < numNodesPerDim[1] - 2; row++)
+		{
+			unsigned int nodeId = row * rowOffset + col * colOffset;
+
+			springTest( std::dynamic_pointer_cast<LinearSpring>(m.getSpring(springId)), m.getFinalState(),
+				nodeId, nodeId + 2 * rowOffset, stiffnessBending, dampingBending);
+			springId++;
+		}
+	}
+	// Followed by face diagonal springs
+	for (unsigned int row = 0; row < numNodesPerDim[1] - 1; row++)
+	{
+		for (unsigned int col = 0; col < numNodesPerDim[0] - 1; col++)
+		{
+			unsigned int nodeId = row * rowOffset + col * colOffset;
+
+			springTest( std::dynamic_pointer_cast<LinearSpring>(m.getSpring(springId)), m.getFinalState(),
+				nodeId, nodeId + rowOffset + colOffset, stiffnessFaceDiagonal, dampingFaceDiagonal);
+			springId++;
+
+			springTest( std::dynamic_pointer_cast<LinearSpring>(m.getSpring(springId)), m.getFinalState(),
+				nodeId + colOffset, nodeId + rowOffset, stiffnessFaceDiagonal, dampingFaceDiagonal);
+			springId++;
+		}
+	}
+
+	// States all equals
+	EXPECT_EQ(*m.getPreviousState(), *m.getFinalState());
+	EXPECT_EQ(*m.getCurrentState(), *m.getFinalState());
+	EXPECT_EQ(*m.getInitialState(), *m.getFinalState());
+
+	// States should contains expected values
+	EXPECT_TRUE(m.getFinalState()->getVelocities().isZero());
+	EXPECT_TRUE(m.getFinalState()->getAccelerations().isZero());
+	EXPECT_FALSE(m.getFinalState()->getPositions().isZero());
+	Vector3d rowExtremititiesDelta[2] =
+	{
+		(extremities[0][1] - extremities[0][0]) / static_cast<double>(numNodesPerDim[1] - 1) ,
+		(extremities[1][1] - extremities[1][0]) / static_cast<double>(numNodesPerDim[1] - 1)
+	};
+	unsigned int nodeId = 0;
+	for (unsigned int row = 0; row < numNodesPerDim[1]; row++)
+	{
+		Vector3d rowExtremities[2];
+		rowExtremities[0] = extremities[0][0] + rowExtremititiesDelta[0] * static_cast<double>(row);
+		rowExtremities[1] = extremities[1][0] + rowExtremititiesDelta[1] * static_cast<double>(row);
+
+		Vector3d delta = (rowExtremities[1] - rowExtremities[0]) / static_cast<double>(numNodesPerDim[0] - 1);
+		for (unsigned int col = 0; col < numNodesPerDim[0]; col++)
+		{
+			SurgSim::Math::Vector3d piExpected(rowExtremities[0] + col * delta);
+			Eigen::VectorBlock<Vector> pi = SurgSim::Math::getSubVector(m.getFinalState()->getPositions(), nodeId, 3);
+			EXPECT_TRUE(pi.isApprox(piExpected));
+			nodeId++;
+		}
+	}
+	EXPECT_EQ(boundaryConditions, m.getFinalState()->getBoundaryConditions());
+}
