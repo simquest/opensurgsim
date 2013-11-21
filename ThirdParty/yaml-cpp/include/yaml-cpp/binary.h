5d724372f81b9c2e79963875e08e0eda24dedba4
vientran <vtran@simquest.com>
2013-11-21 16:08:17 -0500

    Fix compiling failure under linux of stdext::check_array_iterator<> in yaml-cpp/binary.h.

--- ThirdParty/yaml-cpp/include/yaml-cpp/binary.h
+++ ThirdParty/yaml-cpp/include/yaml-cpp/binary.h
@@ -27,7 +27,7 @@ namespace YAML
                 m_data.swap(rhs);
                 rhs.clear();
                 rhs.resize(m_unownedSize);
-                std::copy(m_unownedData, m_unownedData + m_unownedSize, stdext::checked_array_iterator<unsigned char *>(&rhs[0], m_unownedSize));
+                std::copy(m_unownedData, m_unownedData + m_unownedSize, rhs.begin());
                 m_unownedData = 0;
                 m_unownedSize = 0;
             } else {
