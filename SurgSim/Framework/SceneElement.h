3079ac882b099ecc40ff661241bbd4f95f2b7589
haizhouW <hwang@simquest.com>
2013-11-25 10:11:31 -0500

    Separate implementation from declaration, add documentation in SceneElement

--- SurgSim/Framework/SceneElement.h
+++ SurgSim/Framework/SceneElement.h
@@ -37,29 +37,20 @@ class Runtime;
 /// has been initialized
 class SceneElement : public std::enable_shared_from_this<SceneElement>
 {
-
 public:
-	explicit SceneElement(const std::string& name) :
-		m_name(name),
-		m_isInitialized(false),
-		m_isAwake(false)
-	{
-	}
-
-	virtual ~SceneElement()
-	{
-	}
+	/// Constructor
+	/// \param name Name of this SceneElement
+	explicit SceneElement(const std::string& name);
+	/// Destructor
+	virtual ~SceneElement();
 
 	/// Adds a component
 	/// \param	component	The component.
 	/// \return	true if it succeeds, false if it fails.
 	bool addComponent(std::shared_ptr<Component> component);
-
-	/// Removes the give component.
 	/// \param	component	The component.
 	/// \return	true if it succeeds, false if it fails or the component cannot be found.
 	bool removeComponent(std::shared_ptr<Component> component);
-
 	/// Removes the component described by name.
 	/// \param	name	The name.
 	/// \return	true if it succeeds, false if it fails or the component cannot be found.
@@ -69,11 +60,9 @@ public:
 	/// \param	name	The name.
 	/// \return	The component or nullptr if the component cannot be found.
 	std::shared_ptr<Component> getComponent(const std::string& name) const;
-
 	/// Gets all the components of this SceneElement.
 	/// \return	The components.
 	std::vector<std::shared_ptr<Component> > getComponents() const;
-
 	/// Template version of getComponents method to get all the components with type T
 	/// \return The type T components
 	template <class T>
@@ -87,16 +76,13 @@ public:
 	/// \return	true if it succeeds, false if it fails.
 	bool wakeUp();
 
+	/// Return the name of this SceneElement
 	/// \return	The name.
-	std::string getName() const
-	{
-		return m_name;
-	}
+	std::string getName() const;
 
 	/// Sets the Scene.
 	/// \param scene Pointer to the scene.
 	void setScene(std::weak_ptr<Scene> scene);
-
 	/// Gets the Scene.
 	/// \return The scene.
 	std::shared_ptr<Scene> getScene();
@@ -104,39 +90,42 @@ public:
 	/// Sets the Runtime.
 	/// \param runtime Pointer to the runtime.
 	void setRuntime(std::shared_ptr<Runtime> runtime);
-
 	/// Gets the runtime.
 	/// \return	The runtime.
 	std::shared_ptr<Runtime> getRuntime();
 
-	bool isInitialized()
-	{
-		return m_isInitialized;
-	}
-
-	bool isAwake()
-	{
-		return m_isAwake;
-	}
+	/// Return if this SceneElement is initialized.
+	/// \return True if this SceneElement is initialized; Otherwise, false.
+	bool isInitialized() const;
+	/// Return if this SceneElement is awake.
+	/// \return True if this SceneElement is awake; Otherwise, false.
+	bool isAwake() const;
 
 	/// Gets a shared pointer to the runtime.
 	/// \return	The shared pointer.
 	std::shared_ptr<SceneElement> getSharedPtr();
 
-
 private:
-
+	/// Name of this SceneElement
 	std::string m_name;
+	/// A collection of Components
 	std::unordered_map<std::string, std::shared_ptr<Component>> m_components;
+	/// A (weak) back pointer to the Scene containing this SceneElement
 	std::weak_ptr<Scene> m_scene;
+	/// A (weak) back pointer to the Runtime containing this SceneElement
 	std::weak_ptr<Runtime> m_runtime;
 
+	/// Method to initialized this SceneElement. To be overridden by derived class(es).
+	/// \return Ture if initialization is successful; Otherwise, false.
 	virtual bool doInitialize() = 0;
+	/// Method to wake up this SceneElement. To be overridden by derived class(es).
+	/// \return Ture if initialization is successful; Otherwise, false.
 	virtual bool doWakeUp() = 0;
 
+	/// Indicates if this SceneElement has been initialized or not.
 	bool m_isInitialized;
+	/// Indicates if this SceneElement has been waken or not.
 	bool m_isAwake;
-
 };
 
 }; // namespace Framework
