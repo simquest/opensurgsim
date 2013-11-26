3aa9633afa9b175af32d2ec31feae8c4dd116692
Harald Scheirich <hscheirich@simquest.com>
2013-11-26 08:42:40 -0500

    Add behavior for simple behavior driven animation

---
+++ Examples/GraphicsScene/PoseInterpolator.cpp
@@ -0,0 +1,94 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <Examples/GraphicsScene/PoseInterpolator.h>
+
+#include <SurgSim/Framework/SceneElement.h>
+#include <SurgSim/Math/RigidTransform.h>
+#include <SurgSim/Graphics/Representation.h>
+
+#include <memory>
+
+using SurgSim::Math::RigidTransform3d;
+
+PoseInterpolator::PoseInterpolator(const std::string& name) :
+	Behavior(name),
+	m_optionalFrom(RigidTransform3d::Identity()),
+	m_to(RigidTransform3d::Identity()),
+	m_duration(1.0),
+	m_currentTime(0.0)
+{
+
+}
+
+void PoseInterpolator::setFrom(const SurgSim::Math::RigidTransform3d transform)
+{
+	m_optionalFrom.setValue(transform);
+}
+
+void PoseInterpolator::setTo(const SurgSim::Math::RigidTransform3d transform)
+{
+	m_to = transform;
+}
+
+void PoseInterpolator::setTarget(std::shared_ptr<SurgSim::Graphics::Representation> target)
+{
+	m_target = target;
+}
+
+void PoseInterpolator::setDuration(double t)
+{
+	m_duration = t;
+}
+
+bool PoseInterpolator::doInitialize()
+{
+	return true;
+}
+
+bool PoseInterpolator::doWakeUp()
+{
+	bool result = false;
+	if (m_target != nullptr)
+	{
+		m_from = (m_optionalFrom.hasValue()) ? m_optionalFrom.getValue() : m_target->getPose();
+		result = true;
+	}
+	return result;
+}
+
+void PoseInterpolator::update(double dt)
+{
+	bool result = true;
+	m_currentTime += dt;
+
+	m_target->setPose(SurgSim::Math::interpolate(m_from, m_to, m_currentTime/m_duration));
+
+	if (m_currentTime > m_duration)
+	{
+		if (isLoop())
+		{
+			m_currentTime = 0.0;
+		}
+		else if (isPingPong())
+		{
+			std::swap(m_from, m_to);
+		}
+		else
+		{
+			getSceneElement()->removeComponent(getName());
+		}
+	}
+}
---
+++ Examples/GraphicsScene/PoseInterpolator.h
@@ -0,0 +1,92 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SURGSIM_EXAMPLES_GRAPHICSSCENE_POSEINTERPOLATOR_H
+#define SURGSIM_EXAMPLES_GRAPHICSSCENE_POSEINTERPOLATOR_H
+
+#include <memory>
+#include <string>
+#include <SurgSim/Framework/Behavior.h>
+#include <SurgSim/Math/RigidTransform.h>
+#include <SurgSim/DataStructures/OptionalValue.h>
+
+namespace SurgSim
+{
+namespace Graphics
+{
+	class Representation;
+}
+}
+
+
+class PoseInterpolator : public SurgSim::Framework::Behavior
+{
+public:
+	explicit PoseInterpolator(const std::string& name);
+
+	void setFrom(const SurgSim::Math::RigidTransform3d transform);
+	void setTo(const SurgSim::Math::RigidTransform3d transform);
+	void setTarget(std::shared_ptr<SurgSim::Graphics::Representation> target);
+
+	void setDuration(double t);
+
+	bool doWakeUp() override;
+
+	bool doInitialize() override;
+
+	void update(double dt) override;
+
+	void setLoop(bool val)
+	{
+		m_loop = val;
+		if (m_loop)
+		{
+			m_pingpong = false;
+		}
+	}
+
+	bool isLoop()
+	{
+		return m_loop;
+	}
+
+	void setPingPong(bool val)
+	{
+		m_pingpong = val;
+		if (m_pingpong)
+		{
+			m_loop = false;
+		}
+	}
+
+	bool isPingPong()
+	{
+		return m_pingpong;
+	}
+
+private:
+	SurgSim::DataStructures::OptionalValue<SurgSim::Math::RigidTransform3d> m_optionalFrom;
+	SurgSim::Math::RigidTransform3d m_from;
+	SurgSim::Math::RigidTransform3d m_to;
+	std::shared_ptr<SurgSim::Graphics::Representation> m_target;
+
+	double m_duration;
+	double m_currentTime;
+
+	bool m_pingpong;
+	bool m_loop;
+};
+
+#endif
