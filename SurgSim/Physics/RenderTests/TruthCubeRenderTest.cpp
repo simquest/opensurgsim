feea5d680df4ab0ce6e3f8650fe9cf8b27d0f966
vientran <vtran@simquest.com>
2013-12-02 15:15:07 -0500

    Revise RenderTest for Truth Cube.
    1. Add origial uniaxial_positions.csv dataset for the test.
    2. Use ApplicationData to load data file.
    3. Rename the render test to TruthCubeRenderTest.

--- SurgSim/Physics/RenderTests/TruthCubeRenderTest.cpp
+++ SurgSim/Physics/RenderTests/TruthCubeRenderTest.cpp
@@ -19,6 +19,7 @@
 #include <memory>
 #include <vector>
 
+#include <SurgSim/Framework/ApplicationData.h>
 #include <SurgSim/Framework/Runtime.h>
 #include <SurgSim/Framework/Scene.h>
 #include <SurgSim/Graphics/PointCloudRepresentation.h>
@@ -51,25 +52,85 @@ namespace Physics
 
 	typedef SurgSim::DataStructures::Vertices<void> CloudMesh;
 
-	std::vector<Vector3d> ReadTruthCubeData(std::string filename)
+	struct TruthCube
 	{
-		std::ifstream datafile(filename);
-		std::vector<Vector3d> cubeData;
+		// Data positions of uncompressed data
+		std::vector<Vector3d> cubeData0;
+
+		// Data positions of 5% strain 
+		std::vector<Vector3d> cubeData1;
+
+		// Data positions of  12.5% strain
+		std::vector<Vector3d> cubeData2;
+
+		// Data positions of  18.25% strain
+		std::vector<Vector3d> cubeData3;
+	};
+
+	TruthCube truthCube;
+
+	/// Parsing Truth Cube data from an external file
+	/// The uniaxial compression experimental dataset is obtained from:
+	/// "Establishing Physical Standards for Real Time Soft Tissue Simulation" project.
+	///  URL: http://biorobotics.harvard.edu/truthcube/truthcube.html 
+	/// \param filename The filename
+	/// \param cubeData0 The positions of uncompressed cube
+	/// \param cubeData1 The positions of 5% strain cube
+	/// \param cubeData2 The positions of 12.5% strain cube
+	/// \param cubeData3 The positions of 18.25% strain cube
+
+	bool ParseTruthCubeData(TruthCube& truthCube)
+	{
+		// Position of uncompressed data
+		Vector3d position0;
+
+		// Position of 5% strain 
+		Vector3d position1;
+
+		// Position of  12.5% strain
+		Vector3d position2;
+
+		// Position of  18.25% strain
+		Vector3d position3;
+
 		std::string line;
-		Vector3d position;
+
 		char comma;
+		int i,j,k;
 
-		// Parsing header line
-		std::getline(datafile, line); 
+		// The number of lines for Truth cube header files
+		const int commentIndex = 7;
 
-		while (std::getline(datafile, line))
+		const SurgSim::Framework::ApplicationData data("config.txt");
+		std::string filename = data.findFile("uniaxial_positions.csv");
+
+		std::ifstream datafile(filename);
+
+		if (! datafile.good())
 		{
-			std::stringstream strstream(line);
-			strstream >> position.x() >> comma >> position.y() >> comma >> position.z();
-			cubeData.push_back(position);
+			return false;
 		}
 
-		return cubeData;
+		int index = 0;
+
+		while (std::getline(datafile, line))
+			if (++index > commentIndex)
+			{
+				std::stringstream strstream(line);
+				strstream >> i >> comma >> j >> comma >> k >> comma 
+				>> position0.x() >> comma >> position0.y() >> comma >> position0.z() >> comma
+				>> position1.x() >> comma >> position1.y() >> comma >> position1.z() >> comma
+				>> position2.x() >> comma >> position2.y() >> comma >> position2.z() >> comma
+				>> position3.x() >> comma >> position3.y() >> comma >> position3.z();
+
+				// Store proper strains for each cubeData
+				truthCube.cubeData0.push_back(position0);
+				truthCube.cubeData1.push_back(position1);
+				truthCube.cubeData2.push_back(position2);
+				truthCube.cubeData3.push_back(position3);
+			}
+
+		return true;
 
 	};
 
@@ -79,16 +140,18 @@ namespace Physics
 		{
 			runtime = std::make_shared<SurgSim::Framework::Runtime>();
 			graphicsManager = std::make_shared<SurgSim::Graphics::OsgManager>();
-			
+
 			runtime->addManager(graphicsManager);
 
 			scene = std::make_shared<SurgSim::Framework::Scene>();
-			
+
 			runtime->setScene(scene);
-	
+
 			viewElement = std::make_shared<SurgSim::Graphics::OsgViewElement>("Truth Cube");
 			scene->addSceneElement(viewElement);
 
+			// Parse Truth cube data
+			ParseTruthCubeData(truthCube);
 		}
 
 		virtual void TearDown()
@@ -105,15 +168,14 @@ namespace Physics
 
 	TEST_F(RenderTests, LoadTruthCubeData_Uncompressed)
 	{
-		std::vector<Vector3d> vertices = ReadTruthCubeData("Data/uncompressed.csv");
-		
-		auto representation = std::make_shared<SurgSim::Graphics::OsgPointCloudRepresentation<void>>("TruthCube uncompressred");
+		auto representation = std::make_shared<SurgSim::Graphics::OsgPointCloudRepresentation<void>>
+			("TruthCube uncompressed");
 		auto pointCloud = representation->getVertices();
-		
-		/// Add vertices 
-		for (size_t i = 0; i < vertices.size(); ++i)
+
+		/// Loading the Truth Cube data
+		for (size_t i = 0; i < truthCube.cubeData0.size(); ++i)
 		{
-			pointCloud->addVertex(CloudMesh::VertexType(vertices[i]));
+			pointCloud->addVertex(CloudMesh::VertexType(truthCube.cubeData0[i]));
 		}
 
 		representation->setPointSize(4.0);
@@ -122,8 +184,9 @@ namespace Physics
 
 		viewElement->addComponent(representation);
 		viewElement->enableManipulator(true);
-		viewElement->setManipulatorParameters(SurgSim::Math::Vector3d(0.0, 140.0, 0.0), SurgSim::Math::Vector3d(0.0, 0.0, 0.0));
-		
+		viewElement->setManipulatorParameters(SurgSim::Math::Vector3d(0.0, 140.0, 0.0),
+			SurgSim::Math::Vector3d(0.0, 0.0, 0.0));
+
 		/// Run the thread
 		runtime->start();
 
@@ -132,15 +195,14 @@ namespace Physics
 
 	TEST_F(RenderTests, LoadTruthCubeData_Compressed_5)
 	{
-		std::vector<Vector3d> vertices = ReadTruthCubeData("Data/compressed_5.csv");
-
-		auto representation = std::make_shared<SurgSim::Graphics::OsgPointCloudRepresentation<void>>("TruthCube 5% strain");
+		auto representation = std::make_shared<SurgSim::Graphics::OsgPointCloudRepresentation<void>>
+			("TruthCube 5% strain");
 		auto pointCloud = representation->getVertices();
 
-		/// Add vertices 
-		for (size_t i = 0; i < vertices.size(); ++i)
+		/// Load Truth Cube data
+		for (size_t i = 0; i < truthCube.cubeData1.size(); ++i)
 		{
-			pointCloud->addVertex(CloudMesh::VertexType(vertices[i]));
+			pointCloud->addVertex(CloudMesh::VertexType(truthCube.cubeData1[i]));
 		}
 
 		representation->setPointSize(4.0);
@@ -149,7 +211,8 @@ namespace Physics
 
 		viewElement->addComponent(representation);
 		viewElement->enableManipulator(true);
-		viewElement->setManipulatorParameters(SurgSim::Math::Vector3d(0.0, 140.0, 0.0), SurgSim::Math::Vector3d(0.0, 0.0, 0.0));
+		viewElement->setManipulatorParameters(SurgSim::Math::Vector3d(0.0, 140.0, 0.0),
+			SurgSim::Math::Vector3d(0.0, 0.0, 0.0));
 
 		/// Run the thread
 		runtime->start();
@@ -159,15 +222,14 @@ namespace Physics
 
 	TEST_F(RenderTests, LoadTruthCubeData_Compressed_12_5)
 	{
-		std::vector<Vector3d> vertices = ReadTruthCubeData("Data/compressed_12_5.csv");
-
-		auto representation = std::make_shared<SurgSim::Graphics::OsgPointCloudRepresentation<void>>("TruthCube 12.5 strain");
+		auto representation = std::make_shared<SurgSim::Graphics::OsgPointCloudRepresentation<void>>
+			("TruthCube 12.5 strain");
 		auto pointCloud = representation->getVertices();
 
-		/// Add vertices 
-		for (size_t i = 0; i < vertices.size(); ++i)
+		/// Load Truth Cube data
+		for (size_t i = 0; i < truthCube.cubeData2.size(); ++i)
 		{
-			pointCloud->addVertex(CloudMesh::VertexType(vertices[i]));
+			pointCloud->addVertex(CloudMesh::VertexType(truthCube.cubeData2[i]));
 		}
 
 		representation->setPointSize(4.0);
@@ -176,25 +238,25 @@ namespace Physics
 
 		viewElement->addComponent(representation);
 		viewElement->enableManipulator(true);
-		viewElement->setManipulatorParameters(SurgSim::Math::Vector3d(0.0, 140.0, 0.0), SurgSim::Math::Vector3d(0.0, 0.0, 0.0));
+		viewElement->setManipulatorParameters(SurgSim::Math::Vector3d(0.0, 140.0, 0.0),
+			SurgSim::Math::Vector3d(0.0, 0.0, 0.0));
 
 		/// Run the thread
 		runtime->start();
 
 		boost::this_thread::sleep(boost::posix_time::milliseconds(3000));		
 	}
-			
+
 	TEST_F(RenderTests, LoadTruthCubeData_Compressed_18_25)
 	{
-		std::vector<Vector3d> vertices = ReadTruthCubeData("Data/compressed_18_25.csv");
-
-		auto representation = std::make_shared<SurgSim::Graphics::OsgPointCloudRepresentation<void>>("TruthCube 18.25%");
+		auto representation = std::make_shared<SurgSim::Graphics::OsgPointCloudRepresentation<void>>
+			("TruthCube 18.25%");
 		auto pointCloud = representation->getVertices();
 
-		/// Add vertices 
-		for (size_t i = 0; i < vertices.size(); ++i)
+		/// Load Truth Cube data
+		for (size_t i = 0; i < truthCube.cubeData3.size(); ++i)
 		{
-			pointCloud->addVertex(CloudMesh::VertexType(vertices[i]));
+			pointCloud->addVertex(CloudMesh::VertexType(truthCube.cubeData3[i]));
 		}
 
 		representation->setPointSize(4.0);
@@ -203,7 +265,8 @@ namespace Physics
 
 		viewElement->addComponent(representation);
 		viewElement->enableManipulator(true);
-		viewElement->setManipulatorParameters(SurgSim::Math::Vector3d(0.0, 140.0, 0.0), SurgSim::Math::Vector3d(0.0, 0.0, 0.0));
+		viewElement->setManipulatorParameters(SurgSim::Math::Vector3d(0.0, 140.0, 0.0),
+			SurgSim::Math::Vector3d(0.0, 0.0, 0.0));
 
 		/// Run the thread
 		runtime->start();
