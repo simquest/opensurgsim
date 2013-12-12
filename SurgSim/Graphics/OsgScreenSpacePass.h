7d5417a3af9ad2f342b213c37319055af4290c7c
Harald Scheirich <hscheirich@simquest.com>
2013-12-12 08:32:34 -0500

    Add OsgScreenSpacePass
    it is a special renderpass that is used for any kind of ScreenSpace Hud functionality

---
+++ SurgSim/Graphics/OsgScreenSpacePass.h
@@ -0,0 +1,57 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SURGSIM_GRAPHICS_OSGSCREENSPACEPASS_H
+#define SURGSIM_GRAPHICS_OSGSCREENSPACEPASS_H
+
+#include <string>
+
+#include "SurgSim/Graphics/RenderPass.h"
+
+#include <osg/ref_ptr>
+
+namespace osg
+{
+class Camera;
+}
+
+namespace SurgSim
+{
+namespace Graphics
+{
+
+class OsgScreenSpacePass : public RenderPass
+{
+public:
+	explicit OsgScreenSpacePass(const std::string& name);
+
+	virtual ~OsgScreenSpacePass();
+
+	void setViewPort(int width, int height);
+
+	virtual bool doInitialize() override;
+
+	void updateViewport(int width, int height);
+
+private:
+	osg::ref_ptr<osg::Camera> m_camera;
+	int m_width;
+	int m_height;
+};
+
+}
+}
+
+#endif
