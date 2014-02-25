f50ae659af6c3ed0c32ae34e1ac827226decd78e
Walter Schoen <wschoen@simquest.com>
2014-02-25 16:44:01 -0500

    Add fem 3d tetrahedron ply reader delegate
    - Ply reader for fem 3d representations can generate the list of
    vertices, the list of tetrahedrons, and the list of boundary conditions.
    - Adds unit tests

---
+++ SurgSim/Physics/Fem3DRepresentationPlyReaderDelegate.cpp
@@ -0,0 +1,157 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2014, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "SurgSim/DataStructures/PlyReader.h"
+#include "SurgSim/Physics/DeformableRepresentationState.h"
+#include "SurgSim/Physics/Fem3DRepresentation.h"
+#include "SurgSim/Physics/Fem3DRepresentationPlyReaderDelegate.h"
+#include "SurgSim/Physics/FemElement3DTetrahedron.h"
+
+using SurgSim::DataStructures::PlyReader;
+
+namespace SurgSim
+{
+namespace Physics
+{
+
+Fem3DRepresentationPlyReaderDelegate::Fem3DRepresentationPlyReaderDelegate()
+	: m_fem(nullptr), m_state(nullptr), vertexIterator(nullptr)
+{
+
+}
+
+bool Fem3DRepresentationPlyReaderDelegate::fileIsAcceptable(const PlyReader& reader)
+{
+	bool result = true;
+
+	// Shortcut test if one fails ...
+	result = result && reader.hasProperty("vertex", "x");
+	result = result && reader.hasProperty("vertex", "y");
+	result = result && reader.hasProperty("vertex", "z");
+
+	result = result && reader.hasProperty("tetrahedron", "vertex_index_0");
+	result = result && reader.hasProperty("tetrahedron", "vertex_index_1");
+	result = result && reader.hasProperty("tetrahedron", "vertex_index_2");
+	result = result && reader.hasProperty("tetrahedron", "vertex_index_3");
+
+	result = result && reader.hasProperty("boundary_condition", "vertex_index");
+
+	return result;
+}
+
+bool Fem3DRepresentationPlyReaderDelegate::registerDelegate(PlyReader* reader)
+{
+	// Vertex processing
+	reader->requestElement(
+		"vertex",
+		std::bind(
+			&Fem3DRepresentationPlyReaderDelegate::beginVertices, this, std::placeholders::_1, std::placeholders::_2),
+		std::bind(&Fem3DRepresentationPlyReaderDelegate::processVertex, this, std::placeholders::_1),
+		std::bind(&Fem3DRepresentationPlyReaderDelegate::endVertices, this, std::placeholders::_1));
+	reader->requestScalarProperty("vertex", "x", PlyReader::TYPE_DOUBLE, 0 * sizeof(m_vertexData[0]));
+	reader->requestScalarProperty("vertex", "y", PlyReader::TYPE_DOUBLE, 1 * sizeof(m_vertexData[0]));
+	reader->requestScalarProperty("vertex", "z", PlyReader::TYPE_DOUBLE, 2 * sizeof(m_vertexData[0]));
+
+	// Tetrahedron Processing
+	reader->requestElement(
+		"tetrahedron",
+		std::bind(&Fem3DRepresentationPlyReaderDelegate::beginTetrahedrons,
+				  this,
+				  std::placeholders::_1,
+				  std::placeholders::_2),
+		std::bind(&Fem3DRepresentationPlyReaderDelegate::processTetrahedron, this, std::placeholders::_1),
+		std::bind(&Fem3DRepresentationPlyReaderDelegate::endTetrahedrons, this, std::placeholders::_1));
+	reader->requestScalarProperty(
+		"tetrahedron", "vertex_index_0", PlyReader::TYPE_UNSIGNED_INT, 0 * sizeof(m_tetrahedronData[0]));
+	reader->requestScalarProperty(
+		"tetrahedron", "vertex_index_1", PlyReader::TYPE_UNSIGNED_INT, 1 * sizeof(m_tetrahedronData[0]));
+	reader->requestScalarProperty(
+		"tetrahedron", "vertex_index_2", PlyReader::TYPE_UNSIGNED_INT, 2 * sizeof(m_tetrahedronData[0]));
+	reader->requestScalarProperty(
+		"tetrahedron", "vertex_index_3", PlyReader::TYPE_UNSIGNED_INT, 3 * sizeof(m_tetrahedronData[0]));
+
+	// Boundary Condition Processing
+	reader->requestElement(
+		"boundary_condition",
+		std::bind(&Fem3DRepresentationPlyReaderDelegate::beginBoundaryConditions,
+				  this,
+				  std::placeholders::_1,
+				  std::placeholders::_2),
+		std::bind(&Fem3DRepresentationPlyReaderDelegate::processBoundaryCondition, this, std::placeholders::_1),
+		std::bind(&Fem3DRepresentationPlyReaderDelegate::endBoundaryConditions, this, std::placeholders::_1));
+	reader->requestScalarProperty("boundary_condition", "vertex_index", PlyReader::TYPE_UNSIGNED_INT, 0);
+
+	return true;
+}
+
+std::shared_ptr<Fem3DRepresentation> Fem3DRepresentationPlyReaderDelegate::getFem()
+{
+	return m_fem;
+}
+
+void* Fem3DRepresentationPlyReaderDelegate::beginVertices(const std::string& elementName, size_t vertexCount)
+{
+	m_fem = std::make_shared<Fem3DRepresentation>("Ply loaded Fem3d");
+	m_state = std::make_shared<DeformableRepresentationState>();
+
+	m_state->setNumDof(3, vertexCount);
+	vertexIterator = m_state->getPositions().data();
+
+	return m_vertexData.data();
+}
+
+void Fem3DRepresentationPlyReaderDelegate::processVertex(const std::string& elementName)
+{
+	std::copy(std::begin(m_vertexData), std::end(m_vertexData), vertexIterator);
+	vertexIterator += 3;
+}
+
+void Fem3DRepresentationPlyReaderDelegate::endVertices(const std::string& elementName)
+{
+	vertexIterator = nullptr;
+}
+
+void* Fem3DRepresentationPlyReaderDelegate::beginTetrahedrons(const std::string& elementName, size_t faceCount)
+{
+	return m_tetrahedronData.data();
+}
+
+void Fem3DRepresentationPlyReaderDelegate::processTetrahedron(const std::string& elementName)
+{
+	m_fem->addFemElement(std::make_shared<FemElement3DTetrahedron>(m_tetrahedronData, *m_state));
+}
+
+void Fem3DRepresentationPlyReaderDelegate::endTetrahedrons(const std::string& elementName)
+{
+}
+
+void* Fem3DRepresentationPlyReaderDelegate::beginBoundaryConditions(const std::string& elementName, size_t faceCount)
+{
+	return &m_boundaryConditionData;
+}
+
+void Fem3DRepresentationPlyReaderDelegate::processBoundaryCondition(const std::string& elementName)
+{
+	m_state->addBoundaryCondition(m_boundaryConditionData);
+}
+
+void Fem3DRepresentationPlyReaderDelegate::endBoundaryConditions(const std::string& elementName)
+{
+	// We must defer setting state to the very last moment
+	m_fem->setInitialState(m_state);
+}
+
+} // namespace SurgSim
+} // namespace DataStructures
---
+++ SurgSim/Physics/Fem3DRepresentationPlyReaderDelegate.h
@@ -0,0 +1,119 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2014, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SURGSIM_PHYSICS_FEM3DREPRESENTATIONPLYREADERDELEGATE_H
+#define SURGSIM_PHYSICS_FEM3DREPRESENTATIONPLYREADERDELEGATE_H
+
+#include <array>
+#include <vector>
+#include <memory>
+
+#include "SurgSim/DataStructures/PlyReaderDelegate.h"
+
+namespace SurgSim
+{
+namespace Physics
+{
+
+class DeformableRepresentationState;
+class Fem3DRepresentation;
+
+/// Implementation of PlyReaderDelegate for simple tetrahedral meshes
+class Fem3DRepresentationPlyReaderDelegate : public SurgSim::DataStructures::PlyReaderDelegate
+{
+public:
+	/// Default constructor
+	Fem3DRepresentationPlyReaderDelegate();
+
+	/// Gets the fem.
+	/// \return The stored fem.
+	std::shared_ptr<Fem3DRepresentation> getFem();
+
+	/// Registers the delegate with the reader, overridden from \sa PlyReaderDelegate.
+	/// \param reader The reader that should be used.
+	/// \return true if it succeeds, false otherwise.
+	virtual bool registerDelegate(SurgSim::DataStructures::PlyReader* reader) override;
+
+	/// Check whether this file is acceptable to the delegate, overridden from \sa PlyReaderDelegate.
+	/// \param reader The reader that should be used.
+	/// \return true if it succeeds, false otherwise.
+	virtual bool fileIsAcceptable(const SurgSim::DataStructures::PlyReader& reader) override;
+
+	/// Callback function, begin the processing of vertices.
+	/// \param elementName Name of the element.
+	/// \param vertexCount Number of vertices.
+	/// \return memory for vertex data to the reader.
+	void* beginVertices(const std::string& elementName, size_t vertexCount);
+
+	/// Callback function to process one vertex.
+	/// \param elementName Name of the element.
+	void processVertex(const std::string& elementName);
+
+	/// Callback function to finalize processing of vertices.
+	/// \param elementName Name of the element.
+	void endVertices(const std::string& elementName);
+
+	/// Callback function, begin the processing of tetrahedrons.
+	/// \param elementName Name of the element.
+	/// \param tetrahedronCount Number of tetrahedrons.
+	/// \return memory for tetrahedron data to the reader.
+	void* beginTetrahedrons(const std::string& elementName, size_t tetrahedronCount);
+
+	/// Callback function to process one tetrahedron.
+	/// \param elementName Name of the element.
+	void processTetrahedron(const std::string& elementName);
+
+	/// Callback function to finalize processing of tetrahedrons.
+	/// \param elementName Name of the element.
+	void endTetrahedrons(const std::string& elementName);
+
+	/// Callback function, begin the processing of boundary conditions.
+	/// \param elementName Name of the element.
+	/// \param boundaryConditionCount Number of boundary conditions.
+	/// \return memory for boundary conditions data to the reader.
+	void* beginBoundaryConditions(const std::string& elementName, size_t boundaryConditionCount);
+
+	/// Callback function to process one boundary condition.
+	/// \param elementName Name of the element.
+	void processBoundaryCondition(const std::string& elementName);
+
+	/// Callback function to finalize processing of boundary conditions.
+	/// \param elementName Name of the element.
+	void endBoundaryConditions(const std::string& elementName);
+
+private:
+	/// Internal data to receive the "vertex" element
+	std::array<double, 3> m_vertexData;
+
+	/// Internal iterator to save the "vertex" element
+	double *vertexIterator;
+
+	/// Internal data to receive the "tetrahedron" element
+	std::array<unsigned int, 4> m_tetrahedronData;
+
+	/// Internal data to receive the "boundary_condition" element
+	unsigned int m_boundaryConditionData;
+
+	/// The fem that will be created by loading
+	std::shared_ptr<Fem3DRepresentation> m_fem;
+
+	/// The state that will be created by loading
+	std::shared_ptr<DeformableRepresentationState> m_state;
+};
+
+} // namespace Physics
+} // namespace SurgSim
+
+#endif // SURGSIM_PHYSICS_FEM3DREPRESENTATIONPLYREADERDELEGATE_H
