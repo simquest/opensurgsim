661181e04abf9fb79b6d58fa1f2b5c323c9110ca
Walter Schoen <wschoen@simquest.com>
2014-02-27 15:46:25 -0500

    fem 3d contact test, modify testing formatting
    - Introduce new function for testing eigen matrices, which prints out
    data on their differences.  Indicates whether they have different
    dimensions, whether their normed difference is significantly different,
    and if they are vectors which values are different.

--- SurgSim/Physics/UnitTests/Fem3DRepresentationContactTests.cpp
+++ SurgSim/Physics/UnitTests/Fem3DRepresentationContactTests.cpp
@@ -15,6 +15,7 @@
 
 #include <gtest/gtest.h>
 #include <memory>
+#include <tuple>
 
 #include "SurgSim/Framework/Runtime.h"
 #include "SurgSim/Math/Vector.h"
@@ -42,6 +43,61 @@ namespace
 	const double epsilon = 1e-10;
 	const double dt = 1e-3;
 };
+
+::testing::AssertionResult AssertMatricesEqual(const char *expected_expr,
+											   const char *actual_expr,
+											   const char *abs_err_expr,
+											   const Eigen::MatrixXd &expected,
+											   const Eigen::MatrixXd &actual,
+											   double abs_err)
+{
+	if ((expected.rows() != actual.rows()) || (expected.cols() != actual.cols()))
+	{
+		return ::testing::AssertionFailure()
+			<< "Sizes do not match." << std::endl
+			<< "Actual: [" << actual.rows() << ", " << actual.cols() << "]" << std::endl
+			<< "Expected: [" << expected.rows() << ", " << expected.cols() << "]" << std::endl;
+	}
+
+	if (!expected.isApprox(actual, abs_err))
+	{
+		::testing::AssertionResult result = ::testing::AssertionFailure()
+			<< "Matrices are not within epsilon of each other." << std::endl
+			<< "Difference: " << (actual - expected).norm() << std::endl
+			<< "Epsilon: " << abs_err << std::endl;
+
+		if (expected.cols() == 1 || expected.rows() == 1)
+		{
+			typedef std::tuple<int, double, double> DifferenceTuple;
+			std::vector<DifferenceTuple> differences;
+			for (int index = 0; index < expected.size(); ++index)
+			{
+				if (std::abs(expected(index) - actual(index)) > abs_err)
+				{
+					differences.emplace_back(index, expected(index), actual(index));
+				}
+			}
+
+			result << "Total differing values: " << differences.size() << std::endl;
+
+			for (std::vector<DifferenceTuple>::iterator it = std::begin(differences); it != std::end(differences); ++it)
+			{
+				result
+					<< "[Index, Expected, Actual]: "
+					<< std::get<0>(*it) << ", " << std::get<1>(*it)  << ", " << std::get<2>(*it)  << std::endl;
+			}
+		}
+
+		return result;
+	}
+
+	return ::testing::AssertionSuccess();
+}
+
+#define EXPECT_NEAR_EIGEN(expected, actual, abs_err) \
+	EXPECT_PRED_FORMAT3(AssertMatricesEqual, (expected), (actual), (abs_err))
+#define ASSERT_NEAR_EIGEN(expected, actual, abs_err) \
+	ASSERT_PRED_FORMAT3(AssertMatricesEqual, (expected), (actual), (abs_err))
 
 static void addTetraheadron(Fem3DRepresentation *fem,
 							unsigned int node0,
@@ -173,57 +229,17 @@ TEST_F(Fem3DRepresentationContactTests, BuildMlcpTest)
 
 	EXPECT_NEAR(-9.81 * dt * dt * m_n[1], mlcpPhysicsProblem.b[0], epsilon);
 
-	EXPECT_NEAR(m_n[0], mlcpPhysicsProblem.H(0,  0), epsilon);
-	EXPECT_NEAR(m_n[1], mlcpPhysicsProblem.H(0,  1), epsilon);
-	EXPECT_NEAR(m_n[2], mlcpPhysicsProblem.H(0,  2), epsilon);
-	EXPECT_NEAR(0.0,    mlcpPhysicsProblem.H(0,  3), epsilon);
-	EXPECT_NEAR(0.0,    mlcpPhysicsProblem.H(0,  4), epsilon);
-	EXPECT_NEAR(0.0,    mlcpPhysicsProblem.H(0,  5), epsilon);
-	EXPECT_NEAR(0.0,    mlcpPhysicsProblem.H(0,  6), epsilon);
-	EXPECT_NEAR(0.0,    mlcpPhysicsProblem.H(0,  7), epsilon);
-	EXPECT_NEAR(0.0,    mlcpPhysicsProblem.H(0,  8), epsilon);
-	EXPECT_NEAR(0.0,    mlcpPhysicsProblem.H(0,  9), epsilon);
-	EXPECT_NEAR(0.0,    mlcpPhysicsProblem.H(0, 10), epsilon);
-	EXPECT_NEAR(0.0,    mlcpPhysicsProblem.H(0, 11), epsilon);
-	EXPECT_NEAR(0.0,    mlcpPhysicsProblem.H(0, 12), epsilon);
-	EXPECT_NEAR(0.0,    mlcpPhysicsProblem.H(0, 13), epsilon);
-	EXPECT_NEAR(0.0,    mlcpPhysicsProblem.H(0, 14), epsilon);
-	EXPECT_NEAR(0.0,    mlcpPhysicsProblem.H(0, 15), epsilon);
-	EXPECT_NEAR(0.0,    mlcpPhysicsProblem.H(0, 16), epsilon);
-	EXPECT_NEAR(0.0,    mlcpPhysicsProblem.H(0, 17), epsilon);
+	Eigen::Matrix<double, 1, 18> H = Eigen::Matrix<double, 1, 18>::Zero();
+	SurgSim::Math::setSubVector(m_n, 0, 3, &H);
+
+	EXPECT_NEAR_EIGEN(H, mlcpPhysicsProblem.H, epsilon);
 
 	// C = dt * m^{-1}
-	//
-	// => CHt = [C[0,0:3]*n] = [(C[0,0]*nx + C[0,1]*ny + C[0,2]*nz])]
-	//			[C[1,0:3]*n]   [(C[1,0]*nx + C[1,1]*ny + C[1,2]*nz])]
-	//			[...	   ]   [ ...								]
-	//
-	// Note computeM is verified in BuildMlcpMassTest.
 	Eigen::Matrix<double, 18, 18> C = dt * m_fem->computeM(*m_fem->getPreviousState()).inverse();
 
-	EXPECT_NEAR(C( 0, 0) * m_n[0] + C( 0, 1) * m_n[1] + C( 0, 2) * m_n[2], mlcpPhysicsProblem.CHt( 0, 0), epsilon);
-	EXPECT_NEAR(C( 1, 0) * m_n[0] + C( 1, 1) * m_n[1] + C( 1, 2) * m_n[2], mlcpPhysicsProblem.CHt( 1, 0), epsilon);
-	EXPECT_NEAR(C( 2, 0) * m_n[0] + C( 2, 1) * m_n[1] + C( 2, 2) * m_n[2], mlcpPhysicsProblem.CHt( 2, 0), epsilon);
-	EXPECT_NEAR(C( 3, 0) * m_n[0] + C( 3, 1) * m_n[1] + C( 3, 2) * m_n[2], mlcpPhysicsProblem.CHt( 3, 0), epsilon);
-	EXPECT_NEAR(C( 4, 0) * m_n[0] + C( 4, 1) * m_n[1] + C( 4, 2) * m_n[2], mlcpPhysicsProblem.CHt( 4, 0), epsilon);
-	EXPECT_NEAR(C( 5, 0) * m_n[0] + C( 5, 1) * m_n[1] + C( 5, 2) * m_n[2], mlcpPhysicsProblem.CHt( 5, 0), epsilon);
-	EXPECT_NEAR(C( 6, 0) * m_n[0] + C( 6, 1) * m_n[1] + C( 6, 2) * m_n[2], mlcpPhysicsProblem.CHt( 6, 0), epsilon);
-	EXPECT_NEAR(C( 7, 0) * m_n[0] + C( 7, 1) * m_n[1] + C( 7, 2) * m_n[2], mlcpPhysicsProblem.CHt( 7, 0), epsilon);
-	EXPECT_NEAR(C( 8, 0) * m_n[0] + C( 8, 1) * m_n[1] + C( 8, 2) * m_n[2], mlcpPhysicsProblem.CHt( 8, 0), epsilon);
-	EXPECT_NEAR(C( 9, 0) * m_n[0] + C( 9, 1) * m_n[1] + C( 9, 2) * m_n[2], mlcpPhysicsProblem.CHt( 9, 0), epsilon);
-	EXPECT_NEAR(C(10, 0) * m_n[0] + C(10, 1) * m_n[1] + C(10, 2) * m_n[2], mlcpPhysicsProblem.CHt(10, 0), epsilon);
-	EXPECT_NEAR(C(11, 0) * m_n[0] + C(11, 1) * m_n[1] + C(11, 2) * m_n[2], mlcpPhysicsProblem.CHt(11, 0), epsilon);
-	EXPECT_NEAR(C(12, 0) * m_n[0] + C(12, 1) * m_n[1] + C(12, 2) * m_n[2], mlcpPhysicsProblem.CHt(12, 0), epsilon);
-	EXPECT_NEAR(C(13, 0) * m_n[0] + C(13, 1) * m_n[1] + C(13, 2) * m_n[2], mlcpPhysicsProblem.CHt(13, 0), epsilon);
-	EXPECT_NEAR(C(14, 0) * m_n[0] + C(14, 1) * m_n[1] + C(14, 2) * m_n[2], mlcpPhysicsProblem.CHt(14, 0), epsilon);
-	EXPECT_NEAR(C(15, 0) * m_n[0] + C(15, 1) * m_n[1] + C(15, 2) * m_n[2], mlcpPhysicsProblem.CHt(15, 0), epsilon);
-	EXPECT_NEAR(C(16, 0) * m_n[0] + C(16, 1) * m_n[1] + C(16, 2) * m_n[2], mlcpPhysicsProblem.CHt(16, 0), epsilon);
-	EXPECT_NEAR(C(17, 0) * m_n[0] + C(17, 1) * m_n[1] + C(17, 2) * m_n[2], mlcpPhysicsProblem.CHt(17, 0), epsilon);
+	EXPECT_NEAR_EIGEN(C * H.transpose(), mlcpPhysicsProblem.CHt, epsilon);
 
-	EXPECT_NEAR(
-		  m_n[0] * (C(0, 0) * m_n[0] + C(0, 1) * m_n[1] + C(0, 2) * m_n[2])
-		+ m_n[1] * (C(1, 0) * m_n[0] + C(1, 1) * m_n[1] + C(1, 2) * m_n[2])
-		+ m_n[2] * (C(2, 0) * m_n[0] + C(2, 1) * m_n[1] + C(2, 2) * m_n[2]), mlcpPhysicsProblem.A(0, 0), epsilon);
+	EXPECT_NEAR_EIGEN(H * C * H.transpose(), mlcpPhysicsProblem.A, epsilon);
 
 	EXPECT_EQ(0u, mlcpPhysicsProblem.constraintTypes.size());
 }
@@ -247,69 +263,20 @@ TEST_F(Fem3DRepresentationContactTests, BuildMlcpCoordinateTest)
 
 	EXPECT_NEAR(-9.81 * dt * dt * m_n[1], mlcpPhysicsProblem.b[0], epsilon);
 
-	Eigen::Matrix<double, 18, 1> H;
-	H << 0.25 * m_n[0],
-		 0.25 * m_n[1],
-		 0.25 * m_n[2],
-		 0.33 * m_n[0],
-		 0.33 * m_n[1],
-		 0.33 * m_n[2],
-		 0.0,
-		 0.0,
-		 0.0,
-		 0.28 * m_n[0],
-		 0.28 * m_n[1],
-		 0.28 * m_n[2],
-		 0.14 * m_n[0],
-		 0.14 * m_n[1],
-		 0.14 * m_n[2],
-		 0.0,
-		 0.0,
-		 0.0;
+	Eigen::Matrix<double, 1, 18> H = Eigen::Matrix<double, 1, 18>::Zero();
+	SurgSim::Math::setSubVector(0.25 * m_n, 0, 3, &H);
+	SurgSim::Math::setSubVector(0.33 * m_n, 1, 3, &H);
+	SurgSim::Math::setSubVector(0.28 * m_n, 3, 3, &H);
+	SurgSim::Math::setSubVector(0.14 * m_n, 4, 3, &H);
 
-	EXPECT_NEAR(H( 0), mlcpPhysicsProblem.H(0,  0), epsilon);
-	EXPECT_NEAR(H( 1), mlcpPhysicsProblem.H(0,  1), epsilon);
-	EXPECT_NEAR(H( 2), mlcpPhysicsProblem.H(0,  2), epsilon);
-	EXPECT_NEAR(H( 3), mlcpPhysicsProblem.H(0,  3), epsilon);
-	EXPECT_NEAR(H( 4), mlcpPhysicsProblem.H(0,  4), epsilon);
-	EXPECT_NEAR(H( 5), mlcpPhysicsProblem.H(0,  5), epsilon);
-	EXPECT_NEAR(H( 6), mlcpPhysicsProblem.H(0,  6), epsilon);
-	EXPECT_NEAR(H( 7), mlcpPhysicsProblem.H(0,  7), epsilon);
-	EXPECT_NEAR(H( 8), mlcpPhysicsProblem.H(0,  8), epsilon);
-	EXPECT_NEAR(H( 9), mlcpPhysicsProblem.H(0,  9), epsilon);
-	EXPECT_NEAR(H(10), mlcpPhysicsProblem.H(0, 10), epsilon);
-	EXPECT_NEAR(H(11), mlcpPhysicsProblem.H(0, 11), epsilon);
-	EXPECT_NEAR(H(12), mlcpPhysicsProblem.H(0, 12), epsilon);
-	EXPECT_NEAR(H(13), mlcpPhysicsProblem.H(0, 13), epsilon);
-	EXPECT_NEAR(H(14), mlcpPhysicsProblem.H(0, 14), epsilon);
-	EXPECT_NEAR(H(15), mlcpPhysicsProblem.H(0, 15), epsilon);
-	EXPECT_NEAR(H(16), mlcpPhysicsProblem.H(0, 16), epsilon);
-	EXPECT_NEAR(H(17), mlcpPhysicsProblem.H(0, 17), epsilon);
+	EXPECT_NEAR_EIGEN(H, mlcpPhysicsProblem.H, epsilon);
 
-	// CHt
+	// C = dt * m^{-1}
 	Eigen::Matrix<double, 18, 18> C = dt * m_fem->computeM(*m_fem->getPreviousState()).inverse();
 
-	EXPECT_NEAR(C.row( 0) * H, mlcpPhysicsProblem.CHt( 0, 0), epsilon);
-	EXPECT_NEAR(C.row( 1) * H, mlcpPhysicsProblem.CHt( 1, 0), epsilon);
-	EXPECT_NEAR(C.row( 2) * H, mlcpPhysicsProblem.CHt( 2, 0), epsilon);
-	EXPECT_NEAR(C.row( 3) * H, mlcpPhysicsProblem.CHt( 3, 0), epsilon);
-	EXPECT_NEAR(C.row( 4) * H, mlcpPhysicsProblem.CHt( 4, 0), epsilon);
-	EXPECT_NEAR(C.row( 5) * H, mlcpPhysicsProblem.CHt( 5, 0), epsilon);
-	EXPECT_NEAR(C.row( 6) * H, mlcpPhysicsProblem.CHt( 6, 0), epsilon);
-	EXPECT_NEAR(C.row( 7) * H, mlcpPhysicsProblem.CHt( 7, 0), epsilon);
-	EXPECT_NEAR(C.row( 8) * H, mlcpPhysicsProblem.CHt( 8, 0), epsilon);
-	EXPECT_NEAR(C.row( 9) * H, mlcpPhysicsProblem.CHt( 9, 0), epsilon);
-	EXPECT_NEAR(C.row(10) * H, mlcpPhysicsProblem.CHt(10, 0), epsilon);
-	EXPECT_NEAR(C.row(11) * H, mlcpPhysicsProblem.CHt(11, 0), epsilon);
-	EXPECT_NEAR(C.row(12) * H, mlcpPhysicsProblem.CHt(12, 0), epsilon);
-	EXPECT_NEAR(C.row(13) * H, mlcpPhysicsProblem.CHt(13, 0), epsilon);
-	EXPECT_NEAR(C.row(14) * H, mlcpPhysicsProblem.CHt(14, 0), epsilon);
-	EXPECT_NEAR(C.row(15) * H, mlcpPhysicsProblem.CHt(15, 0), epsilon);
-	EXPECT_NEAR(C.row(16) * H, mlcpPhysicsProblem.CHt(16, 0), epsilon);
-	EXPECT_NEAR(C.row(17) * H, mlcpPhysicsProblem.CHt(17, 0), epsilon);
+	EXPECT_NEAR_EIGEN(C * H.transpose(), mlcpPhysicsProblem.CHt, epsilon);
 
-	// HCHt
-	EXPECT_NEAR((H.transpose() * C * H)(0, 0), mlcpPhysicsProblem.A(0, 0), epsilon);
+	EXPECT_NEAR_EIGEN(H * C * H.transpose(), mlcpPhysicsProblem.A, epsilon);
 
 	EXPECT_EQ(0u, mlcpPhysicsProblem.constraintTypes.size());
 }
@@ -359,69 +326,21 @@ TEST_F(Fem3DRepresentationContactTests, BuildMlcpIndiciesTest)
 
 	EXPECT_NEAR(-9.81 * dt * dt * m_n[1], mlcpPhysicsProblem.b[indexOfConstraint], epsilon);
 
-	Eigen::Matrix<double, 18, 1> H;
-	H << 0.25 * m_n[0],
-		 0.25 * m_n[1],
-		 0.25 * m_n[2],
-		 0.33 * m_n[0],
-		 0.33 * m_n[1],
-		 0.33 * m_n[2],
-		 0.0,
-		 0.0,
-		 0.0,
-		 0.28 * m_n[0],
-		 0.28 * m_n[1],
-		 0.28 * m_n[2],
-		 0.14 * m_n[0],
-		 0.14 * m_n[1],
-		 0.14 * m_n[2],
-		 0.0,
-		 0.0,
-		 0.0;
+	Eigen::Matrix<double, 1, 18> H = Eigen::Matrix<double, 1, 18>::Zero();
+	SurgSim::Math::setSubVector(0.25 * m_n, 0, 3, &H);
+	SurgSim::Math::setSubVector(0.33 * m_n, 1, 3, &H);
+	SurgSim::Math::setSubVector(0.28 * m_n, 3, 3, &H);
+	SurgSim::Math::setSubVector(0.14 * m_n, 4, 3, &H);
 
-	EXPECT_NEAR(H( 0), mlcpPhysicsProblem.H(indexOfConstraint, indexOfRepresentation +  0), epsilon);
-	EXPECT_NEAR(H( 1), mlcpPhysicsProblem.H(indexOfConstraint, indexOfRepresentation +  1), epsilon);
-	EXPECT_NEAR(H( 2), mlcpPhysicsProblem.H(indexOfConstraint, indexOfRepresentation +  2), epsilon);
-	EXPECT_NEAR(H( 3), mlcpPhysicsProblem.H(indexOfConstraint, indexOfRepresentation +  3), epsilon);
-	EXPECT_NEAR(H( 4), mlcpPhysicsProblem.H(indexOfConstraint, indexOfRepresentation +  4), epsilon);
-	EXPECT_NEAR(H( 5), mlcpPhysicsProblem.H(indexOfConstraint, indexOfRepresentation +  5), epsilon);
-	EXPECT_NEAR(H( 6), mlcpPhysicsProblem.H(indexOfConstraint, indexOfRepresentation +  6), epsilon);
-	EXPECT_NEAR(H( 7), mlcpPhysicsProblem.H(indexOfConstraint, indexOfRepresentation +  7), epsilon);
-	EXPECT_NEAR(H( 8), mlcpPhysicsProblem.H(indexOfConstraint, indexOfRepresentation +  8), epsilon);
-	EXPECT_NEAR(H( 9), mlcpPhysicsProblem.H(indexOfConstraint, indexOfRepresentation +  9), epsilon);
-	EXPECT_NEAR(H(10), mlcpPhysicsProblem.H(indexOfConstraint, indexOfRepresentation + 10), epsilon);
-	EXPECT_NEAR(H(11), mlcpPhysicsProblem.H(indexOfConstraint, indexOfRepresentation + 11), epsilon);
-	EXPECT_NEAR(H(12), mlcpPhysicsProblem.H(indexOfConstraint, indexOfRepresentation + 12), epsilon);
-	EXPECT_NEAR(H(13), mlcpPhysicsProblem.H(indexOfConstraint, indexOfRepresentation + 13), epsilon);
-	EXPECT_NEAR(H(14), mlcpPhysicsProblem.H(indexOfConstraint, indexOfRepresentation + 14), epsilon);
-	EXPECT_NEAR(H(15), mlcpPhysicsProblem.H(indexOfConstraint, indexOfRepresentation + 15), epsilon);
-	EXPECT_NEAR(H(16), mlcpPhysicsProblem.H(indexOfConstraint, indexOfRepresentation + 16), epsilon);
-	EXPECT_NEAR(H(17), mlcpPhysicsProblem.H(indexOfConstraint, indexOfRepresentation + 17), epsilon);
+	EXPECT_NEAR_EIGEN(H, mlcpPhysicsProblem.H.block(indexOfConstraint, indexOfRepresentation, 1, 18), epsilon);
 
-	// CHt
 	Eigen::Matrix<double, 18, 18> C = dt * m_fem->computeM(*m_fem->getPreviousState()).inverse();
 
-	EXPECT_NEAR(C.row( 0) * H, mlcpPhysicsProblem.CHt(indexOfRepresentation +  0, indexOfConstraint), epsilon);
-	EXPECT_NEAR(C.row( 1) * H, mlcpPhysicsProblem.CHt(indexOfRepresentation +  1, indexOfConstraint), epsilon);
-	EXPECT_NEAR(C.row( 2) * H, mlcpPhysicsProblem.CHt(indexOfRepresentation +  2, indexOfConstraint), epsilon);
-	EXPECT_NEAR(C.row( 3) * H, mlcpPhysicsProblem.CHt(indexOfRepresentation +  3, indexOfConstraint), epsilon);
-	EXPECT_NEAR(C.row( 4) * H, mlcpPhysicsProblem.CHt(indexOfRepresentation +  4, indexOfConstraint), epsilon);
-	EXPECT_NEAR(C.row( 5) * H, mlcpPhysicsProblem.CHt(indexOfRepresentation +  5, indexOfConstraint), epsilon);
-	EXPECT_NEAR(C.row( 6) * H, mlcpPhysicsProblem.CHt(indexOfRepresentation +  6, indexOfConstraint), epsilon);
-	EXPECT_NEAR(C.row( 7) * H, mlcpPhysicsProblem.CHt(indexOfRepresentation +  7, indexOfConstraint), epsilon);
-	EXPECT_NEAR(C.row( 8) * H, mlcpPhysicsProblem.CHt(indexOfRepresentation +  8, indexOfConstraint), epsilon);
-	EXPECT_NEAR(C.row( 9) * H, mlcpPhysicsProblem.CHt(indexOfRepresentation +  9, indexOfConstraint), epsilon);
-	EXPECT_NEAR(C.row(10) * H, mlcpPhysicsProblem.CHt(indexOfRepresentation + 10, indexOfConstraint), epsilon);
-	EXPECT_NEAR(C.row(11) * H, mlcpPhysicsProblem.CHt(indexOfRepresentation + 11, indexOfConstraint), epsilon);
-	EXPECT_NEAR(C.row(12) * H, mlcpPhysicsProblem.CHt(indexOfRepresentation + 12, indexOfConstraint), epsilon);
-	EXPECT_NEAR(C.row(13) * H, mlcpPhysicsProblem.CHt(indexOfRepresentation + 13, indexOfConstraint), epsilon);
-	EXPECT_NEAR(C.row(14) * H, mlcpPhysicsProblem.CHt(indexOfRepresentation + 14, indexOfConstraint), epsilon);
-	EXPECT_NEAR(C.row(15) * H, mlcpPhysicsProblem.CHt(indexOfRepresentation + 15, indexOfConstraint), epsilon);
-	EXPECT_NEAR(C.row(16) * H, mlcpPhysicsProblem.CHt(indexOfRepresentation + 16, indexOfConstraint), epsilon);
-	EXPECT_NEAR(C.row(17) * H, mlcpPhysicsProblem.CHt(indexOfRepresentation + 17, indexOfConstraint), epsilon);
+	EXPECT_NEAR_EIGEN(
+		C * H.transpose(), mlcpPhysicsProblem.CHt.block(indexOfRepresentation, indexOfConstraint, 18, 1), epsilon);
 
-	// HCHt
-	EXPECT_NEAR((H.transpose() * C * H)(0, 0), mlcpPhysicsProblem.A(indexOfConstraint, indexOfConstraint), epsilon);
+	EXPECT_NEAR_EIGEN(
+		H * C * H.transpose(), mlcpPhysicsProblem.A.block(indexOfConstraint, indexOfConstraint, 1, 1), epsilon);
 
 	EXPECT_EQ(0u, mlcpPhysicsProblem.constraintTypes.size());
 }
