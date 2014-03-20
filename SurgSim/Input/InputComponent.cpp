2f5c1e0a300d4b11b86ed367cc1fd40dcb299a80
Ryan Beasley <rbeasley@simquest.com>
2014-03-20 08:57:22 -0500

    Fix include order in InputComponent.

--- SurgSim/Input/InputComponent.cpp
+++ SurgSim/Input/InputComponent.cpp
@@ -16,10 +16,10 @@
 #include "SurgSim/Input/InputComponent.h"
 
 #include "SurgSim/DataStructures/DataGroup.h"
+#include "SurgSim/Framework/LockedContainer.h"
 #include "SurgSim/Framework/Log.h"
 #include "SurgSim/Input/DeviceInterface.h"
 #include "SurgSim/Input/InputConsumerInterface.h"
-#include "SurgSim/Framework/LockedContainer.h"
 
 namespace SurgSim
 {
