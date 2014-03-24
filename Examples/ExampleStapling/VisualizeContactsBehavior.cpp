8e4cd962bec435c4cda2bf8d32088a8602acda2b
haizhouW <hwang@simquest.com>
2014-03-24 10:00:00 -0400

    Add VisualizeContactsBehavior to show vector field when there's contact

---
+++ Examples/ExampleStapling/VisualizeContactsBehavior.cpp
@@ -0,0 +1,113 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "Examples/ExampleStapling/VisualizeContactsBehavior.h"
+
+#include <algorithm>
+
+#include "SurgSim/Collision/CollisionPair.h"
+#include "SurgSim/Collision/Representation.h"
+#include "SurgSim/DataStructures/Vertex.h"
+#include "SurgSim/Framework/SceneElement.h"
+#include "SurgSim/Graphics/OsgVectorFieldRepresentation.h"
+#include "SurgSim/Graphics/VectorField.h"
+#include "SurgSim/Graphics/VectorFieldRepresentation.h"
+
+using SurgSim::DataStructures::Vertex;
+using SurgSim::Collision::Contact;
+using SurgSim::Collision::Representation;
+using SurgSim::Graphics::OsgVectorFieldRepresentation;
+using SurgSim::Graphics::VectorField;
+using SurgSim::Graphics::VectorFieldData;
+using SurgSim::Math::Vector3d;
+
+VisualizeContactsBehavior::VisualizeContactsBehavior(const std::string& name):
+	SurgSim::Framework::Behavior(name),
+	m_vectorFiled(std::make_shared<OsgVectorFieldRepresentation>("VisualizeContacts"))
+{
+}
+
+void VisualizeContactsBehavior::setCollisionRepresentation(
+	std::shared_ptr<SurgSim::Collision::Representation> collisionRepresentation)
+{
+	m_collisionRepresentation = collisionRepresentation;
+}
+
+void VisualizeContactsBehavior::update(double dt)
+{
+	if (m_collisionRepresentation->hasCollision())
+	{
+		std::unordered_map<std::shared_ptr<Representation>, std::list<std::shared_ptr<Contact>>> collisions =
+																			 m_collisionRepresentation->getCollisions();
+
+		unsigned int totalContacts = 0;
+		std::for_each(std::begin(collisions), std::end(collisions),
+			[&totalContacts](const std::pair<std::shared_ptr<Representation>,
+											 std::list<std::shared_ptr<Contact>>>& collision)
+			{
+				totalContacts += static_cast<int>(collision.second.size());
+			});
+
+		std::shared_ptr<VectorField> vectorField = m_vectorFiled->getVectorField();
+		int itemsToPop = static_cast<int>(vectorField->getNumVertices()) - static_cast<int>(2 * totalContacts);
+		if (itemsToPop < 0)
+		{
+			vectorField->getVertices().reserve(2 * totalContacts);
+		}
+		m_vectorFiled->getVectorField()->clear();
+
+		unsigned int index = 0;
+		for (auto it = std::begin(collisions); it != std::end(collisions); ++it)
+		{
+			for (auto iter = std::begin((*it).second); iter != std::end((*it).second); ++iter)
+			{
+				VectorFieldData vectorData1;
+				VectorFieldData vectorData2;
+				vectorData1.direction = -(*iter)->normal * (*iter)->depth * 10;
+				vectorData2.direction =  (*iter)->normal * (*iter)->depth * 10;
+
+				Vertex<VectorFieldData> vertex1 =
+					Vertex<VectorFieldData>((*iter)->penetrationPoints.first.globalPosition.getValue(), vectorData1);
+				Vertex<VectorFieldData> vertex2 =
+					Vertex<VectorFieldData>((*iter)->penetrationPoints.second.globalPosition.getValue(), vectorData2);
+
+				vectorField->addVertex(vertex1);
+				vectorField->addVertex(vertex2);
+			}
+		}
+		m_vectorFiled->setVisible(true);
+	}
+	else
+	{
+		m_vectorFiled->setVisible(false);
+	}
+}
+
+int VisualizeContactsBehavior::getTargetManagerType() const
+{
+	return SurgSim::Framework::MANAGER_TYPE_GRAPHICS;
+}
+
+bool VisualizeContactsBehavior::doInitialize()
+{
+	SURGSIM_ASSERT(m_collisionRepresentation) << "VisualizeContactsBehavior: no collision representation held.";
+	return true;
+}
+
+bool VisualizeContactsBehavior::doWakeUp()
+{
+	getSceneElement()->addComponent(m_vectorFiled);
+	return true;
+}
---
+++ Examples/ExampleStapling/VisualizeContactsBehavior.h
@@ -0,0 +1,79 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef EXAMPLES_EXAMPLESTAPLING_VISUALIZECONTACTSBEHAVIOR_H
+#define EXAMPLES_EXAMPLESTAPLING_VISUALIZECONTACTSBEHAVIOR_H
+
+#include <memory>
+#include <string>
+
+#include "SurgSim/Framework/Behavior.h"
+
+namespace SurgSim
+{
+
+namespace Graphics
+{
+class VectorFieldRepresentation;
+}
+
+namespace Collision
+{
+class Representation;
+}
+
+} // namespace SurgSim
+
+/// This behavior is used to visualize the contacts
+/// on collision representation through vector field
+class VisualizeContactsBehavior: public SurgSim::Framework::Behavior
+{
+public:
+	/// Constructor
+	/// \param	name	Name of the behavior
+	explicit VisualizeContactsBehavior(const std::string& name);
+
+	/// Set the collision representation whose contacts, if any, will be visualized.
+	/// \param	staplerRepresentation The representation of a stapler
+	void setCollisionRepresentation(std::shared_ptr<SurgSim::Collision::Representation> collisionRepresentation);
+
+	/// Update the behavior, show vector field for contacts if there is any.
+	/// \param dt	The length of time (seconds) between update calls.
+	virtual void update(double dt) override;
+
+	/// Return the type of manager that should be responsible for this behavior
+	/// \return An integer indicating which manger should be responsible for this behavior.
+	virtual int getTargetManagerType() const override;
+
+protected:
+	/// Initialize this behavior
+	/// \return True on success, otherwise false.
+	/// \note: In current implementation, this method always returns "true".
+	virtual bool doInitialize() override;
+
+	/// Wakeup this behavior
+	/// \return True on success, otherwise false.
+	/// \note: In current implementation, this method always returns "true".
+	virtual bool doWakeUp() override;
+
+private:
+	/// The collision representation to get contacts for visualizing.
+	std::shared_ptr<SurgSim::Collision::Representation> m_collisionRepresentation;
+
+	/// The osg vector field for visualizing contacts on collision representation
+	std::shared_ptr<SurgSim::Graphics::VectorFieldRepresentation> m_vectorFiled;
+};
+
+#endif  // EXAMPLES_EXAMPLESTAPLING_VISUALIZECONTACTSBEHAVIOR_H
