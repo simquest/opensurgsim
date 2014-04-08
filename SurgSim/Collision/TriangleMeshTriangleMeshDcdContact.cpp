b1b26c04cb12ed1dc9e6514ec12f6050dd542c7e
Walter Schoen <wschoen@simquest.com>
2014-04-08 14:07:45 -0400

    TriangleMeshTriangleMeshDcd, add verification
    - Verify penetration points are coplanar
    - Verify penetration point is within the triangle
    - Verify penetration depth and normal minimally resolves
    penetration

--- SurgSim/Collision/TriangleMeshTriangleMeshDcdContact.cpp
+++ SurgSim/Collision/TriangleMeshTriangleMeshDcdContact.cpp
@@ -43,6 +43,73 @@ std::pair<int,int> TriangleMeshTriangleMeshDcdContact::getShapeTypes()
 	return std::pair<int,int>(SurgSim::Math::SHAPE_TYPE_MESH, SurgSim::Math::SHAPE_TYPE_MESH);
 }
 
+static void assertIsCoplanar(const Vector3d& triangle0, const Vector3d& triangle1, const Vector3d& triangle2,
+					   const Vector3d& point)
+{
+	SURGSIM_ASSERT(abs((triangle2 - triangle0).dot((triangle1 - triangle0).cross(point - triangle2)))
+				   < SurgSim::Math::Geometry::ScalarEpsilon)
+		<< "Coplanarity failed with: "
+		<< "t0 " << triangle0 << ", t1 " << triangle1 << ", t2 " << triangle2 << ", pt " << point;
+}
+
+static void assertIsConstrained(const Vector3d& point,
+								const Vector3d& triangle0,
+								const Vector3d& triangle1,
+								const Vector3d& triangle2,
+								const Vector3d& normal)
+{
+	Vector3d barycentricCoordinate;
+
+	SurgSim::Math::barycentricCoordinates(point, triangle0, triangle1, triangle2, normal, &barycentricCoordinate);
+
+	SURGSIM_ASSERT(barycentricCoordinate.x() >= -SurgSim::Math::Geometry::ScalarEpsilon
+				   && barycentricCoordinate.x() <= 1.0 + SurgSim::Math::Geometry::ScalarEpsilon)
+		<< "Constrained failed with: "
+		<< "t0 " << triangle0 << ", t1 " << triangle1 << ", t2 " << triangle2 << ", n " << normal << ", pt " << point;
+	SURGSIM_ASSERT(barycentricCoordinate.y() >= -SurgSim::Math::Geometry::ScalarEpsilon
+				   && barycentricCoordinate.y() <= 1.0 + SurgSim::Math::Geometry::ScalarEpsilon)
+		<< "Constrained failed with: "
+		<< "t0 " << triangle0 << ", t1 " << triangle1 << ", t2 " << triangle2 << ", n " << normal << ", pt " << point;
+	SURGSIM_ASSERT(barycentricCoordinate.z() >= -SurgSim::Math::Geometry::ScalarEpsilon
+				   && barycentricCoordinate.z() <= 1.0 + SurgSim::Math::Geometry::ScalarEpsilon)
+		<< "Constrained failed with: "
+		<< "t0 " << triangle0 << ", t1 " << triangle1 << ", t2 " << triangle2 << ", n " << normal << ", pt " << point;
+}
+
+static void assertIsCorrectNormalAndDepth(const Vector3d& normal,
+										  double penetrationDepth,
+										  Vector3d triangleA0,
+										  Vector3d triangleA1,
+										  Vector3d triangleA2,
+										  Vector3d triangleB0,
+										  Vector3d triangleB1,
+										  Vector3d triangleB2)
+{
+	Vector3d correction = normal * (0.5 * (penetrationDepth + 2.0 * SurgSim::Math::Geometry::DistanceEpsilon));
+	triangleA0 += correction;
+	triangleA1 += correction;
+	triangleA2 += correction;
+	triangleB0 += correction;
+	triangleB1 += correction;
+	triangleB2 += correction;
+
+	Vector3d temp1, temp2;
+	double expectedDistance = SurgSim::Math::distanceTriangleTriangle(triangleA0, triangleA1, triangleA2,
+																	  triangleB0, triangleB1, triangleB2,
+																	  &temp1, &temp2);
+
+	SURGSIM_ASSERT(expectedDistance >= 0.0)
+		<< "Normal and depth failed with: "
+		<< "n " << normal << ", d " << penetrationDepth 
+		<< ", a0 " << triangleA0 << ", a1 " << triangleA1 << ", a2 " << triangleA2 
+		<< ", b0 " << triangleB0 << ", b1 " << triangleB1 << ", b2 " << triangleB2;
+	SURGSIM_ASSERT(expectedDistance <= 2.1 * SurgSim::Math::Geometry::DistanceEpsilon)
+		<< "Normal and depth failed with: "
+		<< "n " << normal << ", d " << penetrationDepth 
+		<< ", a0 " << triangleA0 << ", a1 " << triangleA1 << ", a2 " << triangleA2 
+		<< ", b0 " << triangleB0 << ", b1 " << triangleB1 << ", b2 " << triangleB2;
+}
+
 void TriangleMeshTriangleMeshDcdContact::doCalculateContact(std::shared_ptr<CollisionPair> pair)
 {
 	std::shared_ptr<Representation> representationMeshA = pair->getFirst();
@@ -104,6 +171,18 @@ void TriangleMeshTriangleMeshDcdContact::doCalculateContact(std::shared_ptr<Coll
 																&penetrationPointA, &penetrationPointB,
 																&normal))
 			{
+#ifdef SURGSIM_DEBUG_TRIANGLETRIANGLECONTACT
+				assertIsCoplanar(triangleA0InLocalB, triangleA1InLocalB, triangleA2InLocalB, penetrationPointA);
+				assertIsCoplanar(triangleB0, triangleB1, triangleB2, penetrationPointB);
+
+				assertIsConstrained(
+					penetrationPointA, triangleA0InLocalB, triangleA1InLocalB, triangleA2InLocalB, normalAInLocalB);
+				assertIsConstrained(penetrationPointB, triangleB0, triangleB1, triangleB2, normalB);
+
+				assertIsCorrectNormalAndDepth(normal, depth, triangleA0InLocalB, triangleA1InLocalB, triangleA2InLocalB,
+					triangleB0, triangleB1, triangleB2);
+#endif
+
 				// Create the contact.
 				std::pair<Location, Location> penetrationPoints;
 				penetrationPoints.first.globalPosition.setValue(globalCoordinatesFromMeshBCoordinates
