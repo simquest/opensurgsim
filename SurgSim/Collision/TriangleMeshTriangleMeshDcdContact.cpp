c8238ee0d5bd5256feeaa5a645e5ce95cffe570d
Gughan Sathyaseelan <gsathyaseelan@simquest.com>
2014-05-07 14:50:02 -0400

    Add triangleId to Collision::Location.
    - Used by TriangleMeshTriangleMeshDcdContact to store the triangleId that
      is part of a collision.

--- SurgSim/Collision/TriangleMeshTriangleMeshDcdContact.cpp
+++ SurgSim/Collision/TriangleMeshTriangleMeshDcdContact.cpp
@@ -106,6 +106,8 @@ void TriangleMeshTriangleMeshDcdContact::doCalculateContact(std::shared_ptr<Coll
 			{
 				// Create the contact.
 				std::pair<Location, Location> penetrationPoints;
+				penetrationPoints.first.triangleId.setValue(i);
+				penetrationPoints.second.triangleId.setValue(j);
 				penetrationPoints.first.globalPosition.setValue(globalCoordinatesFromMeshBCoordinates
 																* penetrationPointA);
 				penetrationPoints.second.globalPosition.setValue(globalCoordinatesFromMeshBCoordinates
