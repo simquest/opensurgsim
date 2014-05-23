46ad9b19427c22bac8fb69d2cec5e6b3d789e062
Julien Lenoir <jlenoir@simquest.com>
2014-05-23 01:02:51 -0400

    Use all OdeSolver in all PhysicsRenderTest
    Add RungeKutta4 solver
    Add Static solver which requested some changes on the MassSpring1D and MassSpring2D RenderTests.

--- SurgSim/Physics/RenderTests/RenderTestFem2D.cpp
+++ SurgSim/Physics/RenderTests/RenderTestFem2D.cpp
@@ -51,7 +51,7 @@ void createFem2DCylinder(std::shared_ptr<Fem2DRepresentation> physicsRepresentat
 	const double massDensity = 5000.0;
 	// Geometrical properties
 	const double length = 1.0;
-	const double radius = 1e-1;
+	const double radius = 0.8e-1;
 	const double thickness = 3e-2;
 	// Number of cross-sections and their discretization in nodes
 	const size_t numSections = 7;
@@ -211,21 +211,33 @@ TEST_F(RenderTests, VisualTestFem2D)
 
 	scene->addSceneElement(
 		createFem2D("Euler Explicit",                                          // name
-					makeRigidTransform(quaternion, Vector3d(0.0, 0.4, 0.0)),   // graphics pose (rot., trans.)
+					makeRigidTransform(quaternion, Vector3d(0.0, 0.6, 0.0)),   // graphics pose (rot., trans.)
 					Vector4d(1, 0, 0, 1),                                      // color (r, g, b, a)
 					SurgSim::Math::INTEGRATIONSCHEME_LINEAR_EXPLICIT_EULER));  // technique to update object
 
 	scene->addSceneElement(
 		createFem2D("Modified Euler Explicit",
-					makeRigidTransform(quaternion, Vector3d(0.0, 0.0, 0.0)),
-					Vector4d(0, 1, 0, 1),
+					makeRigidTransform(quaternion, Vector3d(0.0, 0.3, 0.0)),
+					Vector4d(0.5, 0, 0, 1),
 					SurgSim::Math::INTEGRATIONSCHEME_LINEAR_MODIFIED_EXPLICIT_EULER));
 
 	scene->addSceneElement(
+		createFem2D("Runge Kutta 4",
+					makeRigidTransform(quaternion, Vector3d(0.0, 0.0, 0.0)),
+					Vector4d(0, 1, 0, 1),
+					SurgSim::Math::INTEGRATIONSCHEME_LINEAR_RUNGE_KUTTA_4));
+
+	scene->addSceneElement(
 		createFem2D("Euler Implicit",
-					makeRigidTransform(quaternion, Vector3d(0.0, -0.4, 0.0)),
+					makeRigidTransform(quaternion, Vector3d(0.0, -0.3, 0.0)),
 					Vector4d(0, 0, 1, 1),
 					SurgSim::Math::INTEGRATIONSCHEME_LINEAR_IMPLICIT_EULER));
+
+	scene->addSceneElement(
+		createFem2D("Static",
+					makeRigidTransform(quaternion, Vector3d(0.0, -0.6, 0.0)),
+					Vector4d(1, 1, 1, 1),
+					SurgSim::Math::INTEGRATIONSCHEME_LINEAR_STATIC));
 
 	runTest(Vector3d(0.0, 0.0, 2.0), Vector3d::Zero(), 5000.0);
 }
