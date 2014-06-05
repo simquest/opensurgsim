86b645b37bcb50816ce886a5e803684041e8a4f0
haizhouW <hwang@simquest.com>
2014-06-05 13:24:14 -0400

    Let Serializable StaplingDemo load a scene instead of SceneElements

--- Examples/SerializableStapling/SerializableStapling.cpp
+++ Examples/SerializableStapling/SerializableStapling.cpp
@@ -86,24 +86,10 @@ int main(int argc, char* argv[])
 	view->enableKeyboardDevice(true);
 	inputManager->addDevice(view->getKeyboardDevice());
 
-	YAML::Node node = YAML::LoadFile("Data/FemWound.yaml");
-	std::shared_ptr<SceneElement> wound = node.as<std::shared_ptr<SceneElement>>();
+	YAML::Node node = YAML::LoadFile("Data/StaplingDemo.yaml");
 
-	YAML::Node node2 = YAML::LoadFile("Data/ArmSceneElement.yaml");
-	std::shared_ptr<SceneElement> arm = node2.as<std::shared_ptr<SceneElement>>();
-
-	RigidTransform3d armPose = makeRigidTransform(Quaterniond::Identity(), Vector3d(0.0, -0.2, 0.0));
-	wound->setPose(armPose);
-	arm->setPose(armPose);
-
-	std::shared_ptr<Scene> scene = runtime->getScene();
-	scene->addSceneElement(view);
-	scene->addSceneElement(wound);
-	scene->addSceneElement(arm);
-
-	physicsManager->addExcludedCollisionPair(
-		getComponentChecked<SurgSim::Collision::Representation>(wound, "Collision"),
-		getComponentChecked<SurgSim::Collision::Representation>(arm, "Collision"));
+	runtime->getScene()->decode(node);
+	runtime->getScene()->addSceneElement(view);
 
 	runtime->execute();
 
