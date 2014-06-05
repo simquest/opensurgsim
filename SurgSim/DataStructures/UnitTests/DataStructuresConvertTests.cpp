61e0cb6501ec0de73377222dc15d8609b0a8f8aa
Gughan Sathyaseelan <gsathyaseelan@simquest.com>
2014-06-05 16:47:57 -0400

    Updated testcases to check decoding into smaller/larger arrays.

--- SurgSim/DataStructures/UnitTests/DataStructuresConvertTests.cpp
+++ SurgSim/DataStructures/UnitTests/DataStructuresConvertTests.cpp
@@ -29,6 +29,8 @@ template <typename Type, size_t Size>
 void testStdArraySerialization(const std::array<Type, Size>& value)
 {
 	{
+		SCOPED_TRACE("Normal test");
+
 		// Encode
 		YAML::Node node;
 		EXPECT_NO_THROW(node = value;);
@@ -44,8 +46,37 @@ void testStdArraySerialization(const std::array<Type, Size>& value)
 		}
 	}
 
-	// Test for an empty node
 	{
+		SCOPED_TRACE("Decode into smaller array");
+
+		typedef std::array<Type, Size - 1> SmallerArray;
+
+		// Encode
+		YAML::Node node;
+		EXPECT_NO_THROW(node = value;);
+
+		// Try decoding into a smaller array.
+		SmallerArray smallerNewValue;
+		EXPECT_ANY_THROW(smallerNewValue = node.as<SmallerArray>());
+	}
+
+	{
+		SCOPED_TRACE("Decode into larger array");
+
+		typedef std::array<Type, Size + 1> LargerArray;
+
+		// Encode
+		YAML::Node node;
+		EXPECT_NO_THROW(node = value;);
+
+		// Try decoding into a larger array.
+		LargerArray largerNewValue;
+		EXPECT_ANY_THROW(largerNewValue = node.as<LargerArray>());
+	}
+
+	{
+		SCOPED_TRACE("Serialize empty node");
+
 		std::array<Type, Size> emptyValue;
 
 		// Encode
@@ -69,157 +100,26 @@ void testStdArraySerialization(const std::array<Type, Size>& value)
 TEST(DataStructuresConvertTests, StdArray)
 {
 	{
-		SCOPED_TRACE("std::array<bool> serialization.");
-		std::array<bool, 3> boolArray;
-		boolArray[0] = true;
-		boolArray[1] = false;
-		boolArray[2] = true;
-		testStdArraySerialization(boolArray);
-	}
-	{
-		SCOPED_TRACE("std::array<int> serialization.");
-		std::array<int, 3> intArray;
-		intArray[0] = 534;
-		intArray[1] = 84;
-		intArray[2] = -567;
-		testStdArraySerialization(intArray);
-	}
-	{
-		SCOPED_TRACE("std::array<unsigned int> serialization.");
-		std::array<unsigned int, 3> uintArray;
-		uintArray[0] = 53434;
-		uintArray[1] = 8435;
-		uintArray[2] = 567543;
-		testStdArraySerialization(uintArray);
-	}
-	{
-		SCOPED_TRACE("std::array<float> serialization.");
-		std::array<float, 3> floatArray;
-		floatArray[0] = 534.34f;
-		floatArray[1] = 0.845f;
-		floatArray[2] = -56754.3f;
-		testStdArraySerialization(floatArray);
-	}
-	{
-		SCOPED_TRACE("std::array<double> serialization.");
+		SCOPED_TRACE("Serialization of std::array of size 3");
 		std::array<double, 3> doubleArray;
 		doubleArray[0] = 534.34;
 		doubleArray[1] = 0.8435e56;
 		doubleArray[2] = -56754.3;
 		testStdArraySerialization(doubleArray);
 	}
-	{
-		SCOPED_TRACE("std::array<char> serialization.");
-		std::array<char, 3> charArray;
-		charArray[0] = 'a';
-		charArray[1] = 'b';
-		charArray[2] = 'c';
-		testStdArraySerialization(charArray);
-	}
-	{
-		SCOPED_TRACE("std::array<std::string> serialization.");
-		std::array<std::string, 3> stringArray;
-		stringArray[0] = "TestStringOne";
-		stringArray[1] = "TestStringTwo";
-		stringArray[2] = "TestStringThree";
-		testStdArraySerialization(stringArray);
-	}
-}
 
-template <typename Type>
-void testStdListSerialization(const std::list<Type>& value)
-{
 	{
-		// Encode
-		YAML::Node node;
-		EXPECT_NO_THROW(node = value;);
+		SCOPED_TRACE("Serialization of std::array of size 0");
 
-		// Decode
-		std::list<Type> newValue;
-		newValue = node.as<std::list<Type>>();
-
-		// Verify
-		EXPECT_EQ(value.size(), newValue.size());
-		for (auto it = value.cbegin(), it2 = newValue.cbegin(); it != value.cend(); ++it, ++it2)
-		{
-			EXPECT_EQ(*it, *it2);
-		}
-	}
-
-	// Test for an empty node
-	{
-		std::list<Type> emptyValue;
+		std::array<double, 0> doubleEmptyArray;
 
 		// Encode
 		YAML::Node node;
-		EXPECT_NO_THROW(node = emptyValue;);
+		EXPECT_NO_THROW(node = doubleEmptyArray;);
 
 		// Decode
-		std::list<Type> receiver = value;
-		receiver = node.as<std::list<Type>>();
-
-		// Verify
-		EXPECT_TRUE(receiver.empty());
-	}
-}
-
-TEST(DataStructuresConvertTests, StdList)
-{
-	{
-		SCOPED_TRACE("std::list<bool> serialization.");
-		std::list<bool> boolList;
-		boolList.push_back(true);
-		boolList.push_back(false);
-		boolList.push_back(true);
-		testStdListSerialization(boolList);
-	}
-	{
-		SCOPED_TRACE("std::list<int> serialization.");
-		std::list<int> intList;
-		intList.push_back(534);
-		intList.push_back(84);
-		intList.push_back(-567);
-		testStdListSerialization(intList);
-	}
-	{
-		SCOPED_TRACE("std::list<unsigned int> serialization.");
-		std::list<unsigned int> uintList;
-		uintList.push_back(53434);
-		uintList.push_back(8435);
-		uintList.push_back(567543);
-		testStdListSerialization(uintList);
-	}
-	{
-		SCOPED_TRACE("std::list<float> serialization.");
-		std::list<float> floatList;
-		floatList.push_back(534.34f);
-		floatList.push_back(0.845f);
-		floatList.push_back(-56754.3f);
-		testStdListSerialization(floatList);
-	}
-	{
-		SCOPED_TRACE("std::list<double> serialization.");
-		std::list<double> doubleList;
-		doubleList.push_back(534.34);
-		doubleList.push_back(0.8435e56);
-		doubleList.push_back(-56754.3);
-		testStdListSerialization(doubleList);
-	}
-	{
-		SCOPED_TRACE("std::list<char> serialization.");
-		std::list<char> charList;
-		charList.push_back('a');
-		charList.push_back('b');
-		charList.push_back('c');
-		testStdListSerialization(charList);
-	}
-	{
-		SCOPED_TRACE("std::list<std::string> serialization.");
-		std::list<std::string> stringList;
-		stringList.push_back("TestStringOne");
-		stringList.push_back("TestStringTwo");
-		stringList.push_back("TestStringThree");
-		testStdListSerialization(stringList);
+		std::array<double, 0> newValue;
+		newValue = node.as<std::array<double, 0>>();
 	}
 }
 
