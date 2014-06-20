7920d423eb916cee3f7a39dad788a55a7bd46474
haizhouW <hwang@simquest.com>
2014-06-20 15:11:17 -0400

    Not loading mesh if no file name is given in RigidRepresentationBase

--- SurgSim/Physics/RigidRepresentationBase.cpp
+++ SurgSim/Physics/RigidRepresentationBase.cpp
@@ -47,7 +47,7 @@ bool RigidRepresentationBase::doInitialize()
 {
 	auto meshShape =
 		std::dynamic_pointer_cast<SurgSim::Math::MeshShape>(getInitialParameters().getShapeUsedForMassInertia());
-	if (nullptr != meshShape)
+	if (nullptr != meshShape && !meshShape->getFileName().empty())
 	{
 		SURGSIM_ASSERT(meshShape->initialize(*(getRuntime()->getApplicationData()))) <<
 			"Failed to initialize the mesh shape in this representation.";
