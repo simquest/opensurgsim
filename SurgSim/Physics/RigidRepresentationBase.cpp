simquest/opensurgsim

commit 13ab10ec2e5b31e8820194c78ac8e58a75e380f4

Moves RigidRepresentation pose initialization to doInitialize.
Now that the PoseComponent (if any) is created before the Representation
initializes, we can set the RigidRepresentation's pose in doInitialize
instead of doWakeUp.

The Representation::getPose code changed from getPoseComponent->getPose
to the equivalent getSceneElement->getPose because
Component::getPoseComponent asserts before initialization.

SurgSim/Physics/RigidRepresentationBase.cpp
@@ -53,16 +53,17 @@ bool RigidRepresentationBase::doInitialize()
 			"An invalid shape is used in this RigidRepresentationBase.";
 	}
 
-	return true;
-}
-
-bool RigidRepresentationBase::doWakeUp()
-{
 	m_initialState.setPose(getPose());
 	m_currentState = m_initialState;
 	m_finalState = m_initialState;
 	m_previousState = m_initialState;
 	updateGlobalInertiaMatrices(m_currentState);
+
+	return true;
+}
+
+bool RigidRepresentationBase::doWakeUp()
+{
 	return true;
 }
 

SurgSim/Framework/Representation.cpp
@@ -54,7 +54,7 @@ SurgSim::Math::RigidTransform3d Representation::getPose() const
 {
 	if (getSceneElement() != nullptr)
 	{
-		return getPoseComponent()->getPose() * getLocalPose();
+		return getSceneElement()->getPose() * getLocalPose();
 	}
 	else
 	{

commit e144481c911e07e7661c56c6954e20c36d615a29

Fix Physics Render Tests
The rigid body physics render tests were not working correctly. This was
caused by a change 13ab10ec2e5b31e8820194c78ac8e58a75e380f4, where the
rigid representation's state was set on doInitialize instead of
doWakeUp. If the pose of a an element being driven by a rigid
representation was set after doInitialize (as is done in these render
tests), it would be overwritten the first time the rigid representation
is updated. This change, put the setting of the rigid representation's
state back in doWakeUp so any changes to the scene element pose before
wakeup are accounted for.

SurgSim/Physics/RigidRepresentationBase.cpp
@@ -53,17 +53,17 @@ bool RigidRepresentationBase::doInitialize()
 			"An invalid shape is used in this RigidRepresentationBase.";
 	}
 
+	return true;
+}
+
+bool RigidRepresentationBase::doWakeUp()
+{
 	m_initialState.setPose(getPose());
 	m_currentState = m_initialState;
 	m_finalState = m_initialState;
 	m_previousState = m_initialState;
 	updateGlobalInertiaMatrices(m_currentState);
 
-	return true;
-}
-
-bool RigidRepresentationBase::doWakeUp()
-{
 	return true;
 }
 

SurgSim/Physics/UnitTests/VirtualToolCouplerTest.cpp
@@ -206,6 +206,7 @@ TEST_F(VirtualToolCouplerTest, LinearDisplacement)
 	virtualToolCoupler->initialize(runtime);
 	rigidBody->initialize(runtime);
 	virtualToolCoupler->wakeUp();
+	rigidBody->wakeUp();
 
 	EXPECT_FALSE(rigidBody->getCurrentState().getPose().translation().isZero(epsilon));
 
@@ -251,6 +252,7 @@ TEST_F(VirtualToolCouplerTest, LinearDisplacementWithInertialTorques)
 	virtualToolCoupler->initialize(runtime);
 	rigidBody->initialize(runtime);
 	virtualToolCoupler->wakeUp();
+	rigidBody->wakeUp();
 
 	RigidRepresentationState state = rigidBody->getCurrentState();
 	EXPECT_TRUE(state.getAngularVelocity().isZero(epsilon));
@@ -288,6 +290,7 @@ TEST_F(VirtualToolCouplerTest, AngularDisplacement)
 	virtualToolCoupler->initialize(runtime);
 	rigidBody->initialize(runtime);
 	virtualToolCoupler->wakeUp();
+	rigidBody->wakeUp();
 
 	Eigen::AngleAxisd angleAxis = Eigen::AngleAxisd(rigidBody->getCurrentState().getPose().linear());
 	EXPECT_NEAR(M_PI_4, angleAxis.angle(), epsilon);
@@ -329,6 +332,7 @@ TEST_F(VirtualToolCouplerTest, WithGravity)
 	virtualToolCoupler->initialize(runtime);
 	rigidBody->initialize(runtime);
 	virtualToolCoupler->wakeUp();
+	rigidBody->wakeUp();
 
 	EXPECT_TRUE(rigidBody->getCurrentState().getPose().translation().isApprox(Vector3d::Zero(), epsilon));
 