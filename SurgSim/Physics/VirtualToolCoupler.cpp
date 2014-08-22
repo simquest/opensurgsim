6f8be7a689fb03d623a7925b9ccd5cfd3ef8cf4b
Julien Lenoir <jlenoir@simquest.com>
2014-08-22 15:32:15 -0400

    Avoid extra terms computed twice in VTC/Rigid
    The extra terms from the lever are computed in the VTC and passed on to the Rigid, instead of calling addExternalGeneralizedForce taking an application point.

--- SurgSim/Physics/VirtualToolCoupler.cpp
+++ SurgSim/Physics/VirtualToolCoupler.cpp
@@ -164,6 +164,7 @@ void VirtualToolCoupler::update(double dt)
 		Vector3d rotationVector;
 		SurgSim::Math::computeRotationVector(inputPose, objectPose, &rotationVector);
 		Vector3d torque = m_angularStiffness * rotationVector;
+		torque += leverArm.cross(force);
 		torque += m_angularDamping * (inputAngularVelocity - objectState.getAngularVelocity());
 
 		const Matrix33d identity3x3 = Matrix33d::Identity();
@@ -172,44 +173,34 @@ void VirtualToolCoupler::update(double dt)
 		const Matrix33d linearDampingMatrix = m_linearDamping * identity3x3;
 		const Matrix33d angularStiffnessMatrix = m_angularStiffness * identity3x3;
 		const Matrix33d angularDampingMatrix = m_angularDamping * identity3x3;
+		const Matrix33d skewLeverArm = SurgSim::Math::makeSkewSymmetricMatrix(leverArm);
 
 		Vector6d generalizedForce;
 		generalizedForce << force, torque;
 		Matrix66d generalizedStiffness;
 		generalizedStiffness << linearStiffnessMatrix, zero3x3,
-								zero3x3, angularStiffnessMatrix;
+								m_linearStiffness * skewLeverArm, angularStiffnessMatrix;
 		Matrix66d generalizedDamping;
 		generalizedDamping << linearDampingMatrix, zero3x3,
-							  zero3x3, angularDampingMatrix;
+							  m_linearDamping * skewLeverArm, angularDampingMatrix;
 
-		// By adding an external force applied on the attachment point, the extra torque (leverArm.cross(force))
-		// and its derivatives will be added automatically in the rigid.
-		std::shared_ptr<RigidRepresentationLocalization> localization =
-			std::make_shared<RigidRepresentationLocalization>();
-		localization->setRepresentation(m_rigid);
-		localization->setLocalPosition(m_localAttachmentPoint);
-		m_rigid->addExternalGeneralizedForce(localization, generalizedForce, generalizedStiffness, generalizedDamping);
+		// Here, we could add the external generalized force applied on the attachment point,
+		// the extra torque (leverArm.cross(force)) and its derivatives would be added automatically in the rigid.
+		// But, we do need these informations as well below to pass it on to the haptic, so we compute them
+		// locally and simply add them to the rigid.
+		m_rigid->addExternalGeneralizedForce(generalizedForce, generalizedStiffness, generalizedDamping);
 
 		if (m_output != nullptr)
 		{
 			m_outputData.vectors().set(SurgSim::DataStructures::Names::FORCE, -force);
-			m_outputData.vectors().set(SurgSim::DataStructures::Names::TORQUE, -torque - leverArm.cross(force));
+			m_outputData.vectors().set(SurgSim::DataStructures::Names::TORQUE, -torque);
 			m_outputData.vectors().set(SurgSim::DataStructures::Names::INPUT_LINEAR_VELOCITY, inputLinearVelocity);
 			m_outputData.vectors().set(SurgSim::DataStructures::Names::INPUT_ANGULAR_VELOCITY, inputAngularVelocity);
 
 			m_outputData.poses().set(SurgSim::DataStructures::Names::INPUT_POSE, inputPose);
 
-			const Matrix33d skewLeverArm = SurgSim::Math::makeSkewSymmetricMatrix(leverArm);
-
-			Matrix66d springJacobian;
-			springJacobian << -linearStiffnessMatrix, Matrix33d::Zero(),
-							  -m_linearStiffness * skewLeverArm, -angularStiffnessMatrix;
-			m_outputData.matrices().set(SurgSim::DataStructures::Names::SPRING_JACOBIAN, springJacobian);
-
-			Matrix66d damperJacobian;
-			damperJacobian << -linearDampingMatrix, Matrix33d::Zero(),
-							  m_linearDamping * skewLeverArm, -angularDampingMatrix;
-			m_outputData.matrices().set(SurgSim::DataStructures::Names::DAMPER_JACOBIAN, damperJacobian);
+			m_outputData.matrices().set(SurgSim::DataStructures::Names::SPRING_JACOBIAN, -generalizedStiffness);
+			m_outputData.matrices().set(SurgSim::DataStructures::Names::DAMPER_JACOBIAN, -generalizedDamping);
 
 			m_output->setData(m_outputData);
 		}
