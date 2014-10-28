2c455b61abbc612759dc061e3d1cbb548d1ad916
Julien Lenoir <jlenoir@simquest.com>
2014-10-28 19:40:44 -0400

    Add 2D Gauss quadrature on a triangle.

--- SurgSim/Math/GaussLegendreQuadrature.h
+++ SurgSim/Math/GaussLegendreQuadrature.h
@@ -28,6 +28,7 @@ namespace SurgSim
 namespace Math
 {
 
+/// 1D Gauss-Legendre quadrature
 struct gaussQuadraturePoint
 {
 	gaussQuadraturePoint(double p, double w) : point(p), weight(w){}
@@ -36,7 +37,18 @@ struct gaussQuadraturePoint
 	const double weight;
 };
 
-/// 1-point Gauss-Legendre quadrature {<x_1, w_1>}
+/// 2D Gauss-Legendre quadrature on a triangle
+/// \note For a triangle ABC, a point is defined by its parametrized coordinate \f$(\xi, \eta)\f$
+/// \note as \f$P = A + \xi.AB + \eta.AC\f$
+struct gaussQuadratureTrianglePoint
+{
+	gaussQuadratureTrianglePoint(double xi, double eta, double w) : coordinateXi(xi), coordinateEta(eta), weight(w){}
+
+	const double coordinateXi, coordinateEta; //< Parametrized coordinates xi >= 0, eta >= 0 and xi + eta <= 1.0
+	const double weight;
+};
+
+/// 1D 1-point Gauss-Legendre quadrature {<x_1, w_1>}
 /// \note Gauss-Legendre quadrature numerically evaluates the integral of a function \f$f\f$ with a finite sum
 /// using some weights and specific points of evaluation of the function \f$f\f$:
 /// \note \f$\int_{-1}^{+1} f(x) dx = \sum_{i=1}^n w_i f(x_i)\f$
@@ -45,7 +57,7 @@ struct gaussQuadraturePoint
 /// \note \f$w_i\f$ is the weight to assign to the function evaluation at the given point \f$x_i\f$
 extern std::array<gaussQuadraturePoint, 1> gaussQuadrature1Point;
 
-/// 2-points Gauss-Legendre quadrature {<x_1, w_1>, <x_2, w_2>}
+/// 1D 2-points Gauss-Legendre quadrature {<x_1, w_1>, <x_2, w_2>}
 /// \note Gauss-Legendre quadrature numerically evaluates the integral of a function \f$f\f$ with a finite sum
 /// using some weights and specific points of evaluation of the function \f$f\f$:
 /// \note \f$\int_{-1}^{+1} f(x) dx = \sum_{i=1}^n w_i f(x_i)\f$
@@ -63,7 +75,7 @@ extern std::array<gaussQuadraturePoint, 2> gaussQuadrature2Points;
 /// \note \f$w_i\f$ is the weight to assign to the function evaluation at the given point \f$x_i\f$
 extern std::array<gaussQuadraturePoint, 3> gaussQuadrature3Points;
 
-/// 4-points Gauss-Legendre quadrature {<x_1, w_1>, <x_2, w_2>, <x_3, w_3>, <x_4, w_4>}
+/// 1D 4-points Gauss-Legendre quadrature {<x_1, w_1>, <x_2, w_2>, <x_3, w_3>, <x_4, w_4>}
 /// \note Gauss-Legendre quadrature numerically evaluates the integral of a function \f$f\f$ with a finite sum
 /// using some weights and specific points of evaluation of the function \f$f\f$:
 /// \note \f$\int_{-1}^{+1} f(x) dx = \sum_{i=1}^n w_i f(x_i)\f$
@@ -72,7 +84,7 @@ extern std::array<gaussQuadraturePoint, 3> gaussQuadrature3Points;
 /// \note \f$w_i\f$ is the weight to assign to the function evaluation at the given point \f$x_i\f$
 extern std::array<gaussQuadraturePoint, 4> gaussQuadrature4Points;
 
-/// 5-points Gauss-Legendre quadrature {<x_1, w_1>, <x_2, w_2>, <x_3, w_3>, <x_4, w_4>, <x_5, w_5>}
+/// 1D 5-points Gauss-Legendre quadrature {<x_1, w_1>, <x_2, w_2>, <x_3, w_3>, <x_4, w_4>, <x_5, w_5>}
 /// \note Gauss-Legendre quadrature numerically evaluates the integral of a function \f$f\f$ with a finite sum
 /// using some weights and specific points of evaluation of the function \f$f\f$:
 /// \note \f$\int_{-1}^{+1} f(x) dx = \sum_{i=1}^n w_i f(x_i)\f$
@@ -81,7 +93,7 @@ extern std::array<gaussQuadraturePoint, 4> gaussQuadrature4Points;
 /// \note \f$w_i\f$ is the weight to assign to the function evaluation at the given point \f$x_i\f$
 extern std::array<gaussQuadraturePoint, 5> gaussQuadrature5Points;
 
-/// 100-points Gauss-Legendre quadrature {<x_1, w_1>, <x_2, w_2>, <x_3, w_3>, ..., <x_100, w_100>}
+/// 1D 100-points Gauss-Legendre quadrature {<x_1, w_1>, <x_2, w_2>, <x_3, w_3>, ..., <x_100, w_100>}
 /// \note Gauss-Legendre quadrature numerically evaluates the integral of a function \f$f\f$ with a finite sum
 /// using some weights and specific points of evaluation of the function \f$f\f$:
 /// \note \f$\int_{-1}^{+1} f(x) dx = \sum_{i=1}^n w_i f(x_i)\f$
@@ -89,6 +101,26 @@ extern std::array<gaussQuadraturePoint, 5> gaussQuadrature5Points;
 /// \note \f$x_i\f$ is the point to evaluate the function \f$f\f$ with
 /// \note \f$w_i\f$ is the weight to assign to the function evaluation at the given point \f$x_i\f$
 extern std::array<gaussQuadraturePoint, 100> gaussQuadrature100Points;
+
+/// 2D triangle Gauss-Legendre quadrature 6-points \f${<\xi_1, \eta_1, w_1>, ..., <\xi_6, \eta_6, \w_6>}\f$
+/// \note Gauss-Legendre quadrature numerically evaluates the integral of a function \f$f(\xi, \eta)\f$ with a
+/// finite sum using some weights and specific points on the triangle.
+/// \note \f$\int_{0}^{1} \int_{0}^{1-\eta} f(\xi, \eta) d\xi d\eta = \sum_{i=1}^n w_i f(\xi_i, \eta_i)\f$
+/// \note n is the number of points used to discretized the integral
+/// \note \f$(\xi_i, \eta_i)\f$ is the parametrized location of the triangle point to evaluate the function with
+/// \note \f$w_i\f$ is the weight to assign to the function evaluation at the given point
+/// \note A 6-points Gauss-Legendre quadrature on the triangle is exact for polynomial functions of degree 4 or less.
+extern std::array<gaussQuadratureTrianglePoint, 6> gaussQuadrature2DTriangle6Points;
+
+/// 2D triangle Gauss-Legendre quadrature 12-points \f${<\xi_1, \eta_1, w_1>, ..., <\xi_12, \eta_12, w_12>}\f$
+/// \note Gauss-Legendre quadrature numerically evaluates the integral of a function \f$f(\xi, \eta)\f$ with a
+/// finite sum using some weights and specific points on the triangle.
+/// \note \f$\int_{0}^{1} \int_{0}^{1-\eta} f(\xi, \eta) d\xi d\eta = \sum_{i=1}^n w_i f(\xi_i, \eta_i)\f$
+/// \note n is the number of points used to discretized the integral
+/// \note \f$(\xi_i, \eta_i)\f$ is the parametrized location of the triangle point to evaluate the function with
+/// \note \f$w_i\f$ is the weight to assign to the function evaluation at the given point
+/// \note A 12-points Gauss-Legendre quadrature on the triangle is exact for polynomial functions of degree 6 or less.
+extern std::array<gaussQuadratureTrianglePoint, 12> gaussQuadrature2DTriangle12Points;
 
 };  // namespace Math
 };  // namespace SurgSim
