cfd79ca56a854ba0094c49f7a253f8d96e852fe0
haizhouW <hwang@simquest.com>
2014-11-06 15:31:10 -0500

    Add RandomBoxPointGenerator and RandomSpherePontGenerator
    Classes that can generate points inside or on the surface of a given
    shape.

---
+++ SurgSim/Particles/RandomBoxPointGenerator.cpp
@@ -0,0 +1,80 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "SurgSim/Particles/RandomBoxPointGenerator.h"
+
+#include "SurgSim/Framework/Assert.h"
+#include "SurgSim/Math/BoxShape.h"
+
+namespace SurgSim
+{
+namespace Particles
+{
+using SurgSim::Math::Vector3d;
+
+RandomBoxPointGenerator::~RandomBoxPointGenerator()
+{
+}
+
+Vector3d RandomBoxPointGenerator::pointInShape(std::shared_ptr<SurgSim::Math::Shape> shape)
+{
+	auto box = std::static_pointer_cast<SurgSim::Math::BoxShape>(shape);
+	auto halfSize = box->getSize() * 0.5;
+
+	Vector3d result;
+	result.x() = m_openOneOneDistribution(m_generator) * halfSize.x();
+	result.y() = m_openOneOneDistribution(m_generator) * halfSize.y();
+	result.z() = m_openOneOneDistribution(m_generator) * halfSize.z();
+
+	return result;
+}
+
+Vector3d RandomBoxPointGenerator::pointOnShape(std::shared_ptr<SurgSim::Math::Shape> shape)
+{
+	auto box = std::static_pointer_cast<SurgSim::Math::BoxShape>(shape);
+	auto halfSize = box->getSize() * 0.5;
+
+	Vector3d result;
+
+	// First choose one out of (x,y,z) to be fixed, and then randomly generate the other two coordinates.
+	std::uniform_int_distribution<int> axisSelector(0, 2); // 0: X-Axis, 1: Y-Axis, 2: Z-Axis.
+	std::uniform_int_distribution<int> valueSelector(0, 1); // 0: negative size value, 1: positive size value.
+	switch (axisSelector(m_generator))
+	{
+	case 0:
+		result.x() = (valueSelector(m_generator) == 0 ? -halfSize.x() : halfSize.x());
+		result.y() = m_closedOneOneDistribution(m_generator) * halfSize.y();
+		result.z() = m_closedOneOneDistribution(m_generator) * halfSize.z();;
+		break;
+	case 1:
+		result.x() = m_closedOneOneDistribution(m_generator) * halfSize.x();;
+		result.y() = (valueSelector(m_generator) == 0 ? -halfSize.y() : halfSize.y());
+		result.z() = m_closedOneOneDistribution(m_generator) * halfSize.z();;
+		break;
+	case 2:
+		result.x() = m_closedOneOneDistribution(m_generator) * halfSize.x();;
+		result.y() = m_closedOneOneDistribution(m_generator) * halfSize.y();;
+		result.z() = (valueSelector(m_generator) == 0 ? -halfSize.z() : halfSize.z());
+		break;
+	default:
+		SURGSIM_FAILURE() << "Failed to generate a point";
+		break;
+	}
+
+	return result;
+}
+
+}; // namespace Particles
+}; // namespace SurgSim
---
+++ SurgSim/Particles/RandomSpherePointGenerator.cpp
@@ -0,0 +1,71 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "SurgSim/Particles/RandomSpherePointGenerator.h"
+
+#include "SurgSim/Framework/Assert.h"
+#include "SurgSim/Math/SphereShape.h"
+
+namespace SurgSim
+{
+namespace Particles
+{
+using SurgSim::Math::Vector3d;
+
+RandomSpherePointGenerator::~RandomSpherePointGenerator()
+{
+}
+
+Vector3d RandomSpherePointGenerator::pointInShape(std::shared_ptr<SurgSim::Math::Shape> shape)
+{
+	auto sphere = std::static_pointer_cast<SurgSim::Math::SphereShape>(shape);
+
+	// A point on the sphere, use it as direction and then multiple it by a non-negative length less than the radius.
+	Vector3d result = pointOnShape(shape) * m_zeroOneDistribution(m_generator) * sphere->getRadius();
+
+	return result;
+}
+
+Vector3d RandomSpherePointGenerator::pointOnShape(std::shared_ptr<SurgSim::Math::Shape> shape)
+{
+	auto sphere = std::static_pointer_cast<SurgSim::Math::SphereShape>(shape);
+	double radius = sphere->getRadius();
+
+	// Based on http://www.cs.cmu.edu/~mws/rpos.html
+	double z = 0.0;
+	double phi = 0.0;
+	double theta = 0.0;
+
+	Vector3d result = Vector3d::Zero();
+	// If the origin (0.0, 0.0, 0.0) is produced, regenerate.
+	while (result.isZero())
+	{
+		z = m_closedOneOneDistribution(m_generator) * radius;
+		phi = m_closedZeroOneDistribution(m_generator) * 2.0 * M_PI;
+		theta = 1.0 / std::sin(z / radius);
+
+		result.x() = radius * std::cos(theta) * std::cos(phi);
+		result.y() = radius * std::cos(theta) * std::sin(phi);
+		result.z() = z;
+	}
+
+	result.normalize();
+	result *= radius;
+
+	return result;
+}
+
+}; // namespace Particles
+}; // namespace SurgSim
