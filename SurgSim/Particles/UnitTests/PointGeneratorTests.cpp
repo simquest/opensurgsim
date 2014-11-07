0cdeffca7ab1758a3b26059e6e4a809da5785992
haizhouW <hwang@simquest.com>
2014-11-07 16:36:37 -0500

    Revise UnitTest

--- SurgSim/Particles/UnitTests/PointGeneratorTests.cpp
+++ SurgSim/Particles/UnitTests/PointGeneratorTests.cpp
@@ -45,18 +45,25 @@ TEST(PointGeneratorTest, BoxPointGeneratorTest)
 	auto boxPointGenerator = std::make_shared<RandomBoxPointGenerator>();
 
 	auto pointInsideBox = boxPointGenerator->pointInShape(boxShape);
-	std::vector<Vector3d> intersections(0);
-	SurgSim::Math::intersectionsSegmentBox(Vector3d(0.0, 0.0, 0.0), pointInsideBox, aabb, &intersections);
-	EXPECT_EQ(0u, intersections.size());
+	bool isInOrOnBox = ((pointInsideBox - aabb.min()).array() > -SurgSim::Math::Geometry::ScalarEpsilon).all() && 
+					   ((pointInsideBox - aabb.max()).array() < SurgSim::Math::Geometry::ScalarEpsilon).all();
+	EXPECT_TRUE(isInOrOnBox); // The point is located in the aabb
+	size_t intersection = aabb.min().cwiseEqual(pointInsideBox).count() + 
+						  aabb.max().cwiseEqual(pointInsideBox).count();
+	EXPECT_EQ(intersection, 0u); // The point is NOT on the surface of the aabb
 
 	auto pointOnBox = boxPointGenerator->pointOnShape(boxShape);
-	SurgSim::Math::intersectionsSegmentBox(Vector3d(0.0, 0.0, 0.0), pointOnBox, aabb, &intersections);
-	EXPECT_NE(0u, intersections.size());
+	isInOrOnBox = ((pointOnBox - aabb.min()).array() >= -SurgSim::Math::Geometry::ScalarEpsilon).all() && 
+				  ((pointOnBox - aabb.max()).array() <= SurgSim::Math::Geometry::ScalarEpsilon).all();
+	EXPECT_TRUE(isInOrOnBox); // The point is located in the aabb
+	intersection = aabb.min().cwiseEqual(pointOnBox).count() + 
+				   aabb.max().cwiseEqual(pointOnBox).count();
+	EXPECT_GT(intersection, 0u); // The point IS on the surface of the aabb
 }
 
 TEST(PointGeneratorTest, SpherePointGeneratorTest)
 {
-	auto sphereShape = std::make_shared<SphereShape>(0.1);
+	auto sphereShape = std::make_shared<SphereShape>(2.0);
 	auto spherePointGenerator = std::make_shared<RandomSpherePointGenerator>();
 
 	auto pointInsideSphere = spherePointGenerator->pointInShape(sphereShape);
