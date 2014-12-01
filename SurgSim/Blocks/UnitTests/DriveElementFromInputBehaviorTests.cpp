b4021a6f0e672362c929941068645b7550cc9862
Paul Novotny <pnovotny@simquest.com>
2014-12-01 11:29:16 -0500

    DriveElementFromInputBehavior serialization tests

--- SurgSim/Blocks/UnitTests/DriveElementFromInputBehaviorTests.cpp
+++ SurgSim/Blocks/UnitTests/DriveElementFromInputBehaviorTests.cpp
@@ -17,6 +17,7 @@
 /// Tests for the DriveElementFromInputBehavior class.
 
 #include <gtest/gtest.h>
+#include <yaml-cpp/yaml.h>
 
 #include "SurgSim/Blocks/DriveElementFromInputBehavior.h"
 #include "SurgSim/Devices/IdentityPoseDevice/IdentityPoseDevice.h"
@@ -24,6 +25,7 @@
 #include "SurgSim/Input/OutputComponent.h"
 #include "SurgSim/Framework/Assert.h"
 #include "SurgSim/Framework/BasicSceneElement.h"
+#include "SurgSim/Framework/FrameworkConvert.h"
 #include "SurgSim/Math/RigidTransform.h"
 #include "SurgSim/Math/Vector.h"
 
@@ -92,6 +94,30 @@ TEST(DriveElementFromInputBehaviorTest, Update)
 	inputComponent->setLocalPose(inputOffset);
 	behavior->update(0.1);
 	EXPECT_TRUE(element->getPose().isApprox(inputOffset));
+}
+
+TEST(DriveElementFromInputBehaviorTest, Serialization)
+{
+	auto behavior = std::make_shared<DriveElementFromInputBehavior>("DriveElementFromInputBehavior");
+	EXPECT_EQ("SurgSim::Blocks::DriveElementFromInputBehavior", behavior->getClassName());
+
+	auto inputComponent = std::make_shared<InputComponent>("InputComponent");
+	EXPECT_NO_THROW(behavior->setValue("Source",
+				std::static_pointer_cast<SurgSim::Framework::Component>(inputComponent)));
+	EXPECT_NO_THROW(behavior->setValue("PoseName", std::string("Test")));
+
+	YAML::Node node;
+	EXPECT_NO_THROW(node = YAML::convert<SurgSim::Framework::Component>::encode(*behavior));
+	EXPECT_TRUE(node.IsMap());
+	EXPECT_EQ(5, node[behavior->getClassName()].size());
+
+	std::shared_ptr<DriveElementFromInputBehavior> decodedBehavior;
+	EXPECT_NO_THROW(decodedBehavior = std::dynamic_pointer_cast<DriveElementFromInputBehavior>(
+		node.as<std::shared_ptr<SurgSim::Framework::Component>>()));
+
+	EXPECT_EQ("InputComponent", SurgSim::Framework::convert<std::shared_ptr<SurgSim::Framework::Component>>(
+					decodedBehavior->getValue("Source"))->getName());
+	EXPECT_EQ("Test", SurgSim::Framework::convert<std::string>(decodedBehavior->getValue("PoseName")));
 }
 
 }; //namespace Blocks
