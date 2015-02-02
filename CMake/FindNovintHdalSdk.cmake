431cbce041556544a1e1aec09e2eaf478abbb755
Paul Novotny <pnovotny@simquest.com>
2015-02-02 12:10:33 -0500

    Match cmake variables for Novint and Sixense
    Match the cmake variables produced in FindNovintHdalSdk and
    FindSixenseSdk so they match their respecting _FOUND variable, ie
    removing the '_' so they are all consistent.

--- CMake/FindNovintHdalSdk.cmake
+++ CMake/FindNovintHdalSdk.cmake
@@ -2,8 +2,8 @@
 #
 # Once done this will define
 #  NOVINTHDALSDK_FOUND - system has the Novint HDAL SDK directory
-#  NOVINT_HDAL_SDK_INCLUDE_DIR - the Novint HDAL SDK include directory
-#  NOVINT_HDAL_SDK_LIBRARIES - the Novint HDAL SDK libraries
+#  NOVINTHDALSDK_INCLUDE_DIR - the Novint HDAL SDK include directory
+#  NOVINTHDALSDK_LIBRARIES - the Novint HDAL SDK libraries
 
 # This file is a part of the OpenSurgSim project.
 # Copyright 2013, SimQuest Solutions Inc.
@@ -12,7 +12,7 @@
 # Cache settings and Novint HDAL SDK environment variables take
 # precedence, or we try to fall back to the default search.
 
-find_path(NOVINT_HDAL_SDK_INCLUDE_DIR
+find_path(NOVINTHDALSDK_INCLUDE_DIR
 	NAMES hdl/hdl.h
 	PATHS "$ENV{NOVINT_DEVICE_SUPPORT}"
 	PATH_SUFFIXES "include"
@@ -23,25 +23,25 @@ find_path(NOVINT_HDAL_SDK_INCLUDE_DIR
 )
 
 # TODO(advornik): Shake down some usual suspects under Windows/Linux?
-find_path(NOVINT_HDAL_SDK_INCLUDE_DIR
+find_path(NOVINTHDALSDK_INCLUDE_DIR
 	NAMES hdl/hdl.h
 	PATH_SUFFIXES "include"
 )
 
-if(NOVINT_HDAL_SDK_INCLUDE_DIR)
-	get_filename_component(PARENT_DIR "${NOVINT_HDAL_SDK_INCLUDE_DIR}" PATH)
-	set(NOVINT_HDAL_SDK_ROOT_DIRS "${PARENT_DIR}")
+if(NOVINTHDALSDK_INCLUDE_DIR)
+	get_filename_component(PARENT_DIR "${NOVINTHDALSDK_INCLUDE_DIR}" PATH)
+	set(NOVINTHDALSDK_ROOT_DIRS "${PARENT_DIR}")
 
 	# include all parent directories, too
 	get_filename_component(PARENT_DIR "${PARENT_DIR}" PATH)
 	get_filename_component(NEXT_PARENT_DIR "${PARENT_DIR}" PATH)
 	while(NOT "${NEXT_PARENT_DIR}" STREQUAL "${PARENT_DIR}")
 		# use PARENT_DIR, nor NEXT_PARENT_DIR, to stay out of the root directory
-		list(APPEND NOVINT_HDAL_SDK_ROOT_DIRS "${PARENT_DIR}")
+		list(APPEND NOVINTHDALSDK_ROOT_DIRS "${PARENT_DIR}")
 		set(PARENT_DIR "${NEXT_PARENT_DIR}")
 		get_filename_component(NEXT_PARENT_DIR "${PARENT_DIR}" PATH)
 	endwhile()
-endif(NOVINT_HDAL_SDK_INCLUDE_DIR)
+endif(NOVINTHDALSDK_INCLUDE_DIR)
 
 # Look for (shared) libraries.
 set(LIB_SUFFIX "")
@@ -78,22 +78,22 @@ macro(novint_shared_from_link OUTPUT)
 endmacro()
 
 macro(novint_find_library LIB_NAME)
-	find_library(NOVINT_HDAL_SDK_${LIB_NAME}_LIBRARY
+	find_library(NOVINTHDALSDK_${LIB_NAME}_LIBRARY
 		NAMES "${LIB_NAME}${LIB_SUFFIX}"
-		HINTS ${NOVINT_HDAL_SDK_ROOT_DIRS}
+		HINTS ${NOVINTHDALSDK_ROOT_DIRS}
 		PATH_SUFFIXES "lib"
 		NO_CMAKE_ENVIRONMENT_PATH NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH
 	)
-	find_library(NOVINT_HDAL_SDK_${LIB_NAME}_LIBRARY
+	find_library(NOVINTHDALSDK_${LIB_NAME}_LIBRARY
 		NAMES "${LIB_NAME}${LIB_SUFFIX}"
 		PATH_SUFFIXES "lib"
 	)
-	mark_as_advanced(NOVINT_HDAL_SDK_${LIB_NAME}_LIBRARY)
+	mark_as_advanced(NOVINTHDALSDK_${LIB_NAME}_LIBRARY)
 
-	if(NOVINT_HDAL_SDK_${LIB_NAME}_LIBRARY AND
-			NOT NOVINT_HDAL_SDK_${LIB_NAME}_SHARED)
-		novint_shared_from_link(NOVINT_HDAL_SDK_${LIB_NAME}_SHARED
-			"${NOVINT_HDAL_SDK_${LIB_NAME}_LIBRARY}")
+	if(NOVINTHDALSDK_${LIB_NAME}_LIBRARY AND
+			NOT NOVINTHDALSDK_${LIB_NAME}_SHARED)
+		novint_shared_from_link(NOVINTHDALSDK_${LIB_NAME}_SHARED
+			"${NOVINTHDALSDK_${LIB_NAME}_LIBRARY}")
 	endif()
 endmacro()
 
@@ -108,10 +108,10 @@ novint_find_library(hdl)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(NovintHdalSdk
-	DEFAULT_MSG NOVINT_HDAL_SDK_INCLUDE_DIR NOVINT_HDAL_SDK_hdl_LIBRARY)
+	DEFAULT_MSG NOVINTHDALSDK_INCLUDE_DIR NOVINTHDALSDK_hdl_LIBRARY)
 
 if(NOVINTHDALSDK_FOUND)
-	set(NOVINT_HDAL_SDK_LIBRARIES ${NOVINT_HDAL_SDK_hdl_LIBRARY})
+	set(NOVINTHDALSDK_LIBRARIES ${NOVINTHDALSDK_hdl_LIBRARY})
 endif(NOVINTHDALSDK_FOUND)
 
-mark_as_advanced(NOVINT_HDAL_SDK_LIBRARIES)
+mark_as_advanced(NOVINTHDALSDK_LIBRARIES)
