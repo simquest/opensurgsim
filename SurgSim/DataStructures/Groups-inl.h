33ed4b6c7a86c301bea543ba0433ba895c1ca12c
Harald Scheirich <hscheirich@simquest.com>
2015-02-03 11:25:14 -0500

    Add class to manage groups of things

---
+++ SurgSim/DataStructures/Groups-inl.h
@@ -0,0 +1,124 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SURGSIM_DATASTRUCTURES_GROUPS_INL_H
+#define SURGSIM_DATASTRUCTURES_GROUPS_INL_H
+
+template <typename Key, typename T>
+bool SurgSim::DataStructures::Groups<Key, T>::add(const Key& group, const T& value)
+{
+	UniqueLock lock(m_mutex);
+	auto result = m_groups[group].insert(value);
+	if (result.second == true)
+	{
+		m_membership[value].insert(group);
+	}
+	return result.second;
+}
+
+
+template <typename Key, typename T>
+bool SurgSim::DataStructures::Groups<Key, T>::remove(const Key& group, const T& value)
+{
+	bool result = false;
+	UniqueLock lock(m_mutex);
+	auto found = m_groups.find(group);
+	if (found != m_groups.end())
+	{
+		auto count = found->second.erase(value);
+		if (count > 0)
+		{
+			if (found->second.empty())
+			{
+				m_groups.erase(group);
+			}
+			m_membership[value].erase(group);
+			result = true;
+		}
+	}
+	return result;
+}
+
+template <typename Key, typename T>
+std::vector<T> SurgSim::DataStructures::Groups<Key, T>::getMembers(const Key& group) const
+{
+	std::vector<T> result;
+	SharedLock lock(m_mutex);
+	auto found = m_groups.find(group);
+	if (found != m_groups.end())
+	{
+		std::vector<T> temp(found->second.cbegin(), found->second.cend());
+		std::swap(result, temp);
+	}
+	return std::move(result);
+}
+
+template <typename Key, typename T>
+std::vector<Key> SurgSim::DataStructures::Groups<Key, T>::getGroups(const T& value) const
+{
+	std::vector<Key> result;
+	SharedLock lock(m_mutex);
+	auto found = m_membership.find(value);
+	if (found != m_membership.end())
+	{
+		std::vector<Key> temp(found->second.cbegin(), found->second.cend());
+		std::swap(result, temp);
+	}
+	return std::move(result);
+}
+
+template <typename Key, typename T>
+std::vector<Key> SurgSim::DataStructures::Groups<Key, T>::getGroups() const
+{
+	std::unordered_set<Key> groups;
+	{
+		SharedLock lock(m_mutex);
+		std::for_each(m_groups.cbegin(), m_groups.cend(),
+					  [&groups](const std::pair<Key, std::unordered_set<T>>& value)
+		{
+			groups.insert(value.first);
+		});
+	}
+	std::vector<Key> result(groups.begin(), groups.end());
+	std::copy(groups.begin(), groups.end(), result.begin());
+	return std::move(result);
+}
+
+template <typename Key, typename T>
+bool SurgSim::DataStructures::Groups<Key, T>::remove(const T& value)
+{
+	bool result = false;
+	UniqueLock lock(m_mutex);
+	if (m_membership.find(value) != m_membership.end())
+	{
+		for (auto group : m_membership[value])
+		{
+			m_groups[group].erase(value);
+		}
+		m_membership.erase(value);
+		result = true;
+	}
+	return result;
+}
+
+template <typename Key, typename T>
+std::vector<T> SurgSim::DataStructures::Groups<Key, T>::operator[](const Key& group) const
+{
+	// Get member does the locking, not needed here
+	return getMembers(group);
+}
+
+
+#endif
---
+++ SurgSim/DataStructures/Groups.h
@@ -0,0 +1,100 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SURGSIM_DATASTRUCTURES_GROUPS_H
+#define SURGSIM_DATASTRUCTURES_GROUPS_H
+
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include <boost/thread.hpp>
+
+namespace SurgSim
+{
+
+
+namespace DataStructures
+{
+
+/// Class to wrap grouping operations, gives access to the members of a group and the groups of members.
+/// Groups is threadsafe with regard to add and remove operations, and observations
+/// \tparam Key the label to be used for the groups
+/// \tparam T the type of the group members
+template <typename Key, typename T>
+class Groups
+{
+public:
+
+	typedef Key IdentifierType;
+	typedef T MemberType;
+
+	/// Add a member to the given group, if the group doesn't exist it will be created, if the element
+	/// is already a member of the group, nothing happens
+	/// \param group the group to use
+	/// \param member the member to add
+	/// \return true if the member was actually added
+	bool add(const Key& group, const T& member);
+
+	/// Remove a member from a given group, if the group does not exist or the member is not a member of that
+	/// group, nothing will happen.
+	/// \param group the group to use
+	/// \param member the member to remove
+	/// \return true if the member was member of that group
+	bool remove(const Key& group, const T& member);
+
+	/// Remove a member from all known groups, if the member is not a member of any group, nothing happens
+	/// \param member the member to remove
+	/// \return true if there was an actual removal that was executed
+	bool remove(const T& member);
+
+	/// Return all the members of the given group
+	/// \param group group to query
+	/// \return members of the given group, empty if the group has no members
+	std::vector<T> getMembers(const Key& group) const;
+
+	/// Return all the groups that the given member is a member of
+	/// \param member member to query
+	/// \return groups which contain the given member, empty if the member is not member of any group
+	std::vector<Key> getGroups(const T& member) const;
+
+	/// \return all the know groups with members
+	std::vector<Key> getGroups() const;
+
+	/// Return all the members of the given group
+	/// \param group group to query
+	/// \return members of the given group, empty if the group has no members
+	std::vector<T> operator[](const Key& key) const;
+private:
+
+	typedef boost::shared_lock<boost::shared_mutex> SharedLock;
+	typedef boost::unique_lock<boost::shared_mutex> UniqueLock;
+
+	/// The mutex used to lock for reading and writing
+	mutable boost::shared_mutex m_mutex;
+
+	/// Map groups to members
+	std::unordered_map<Key, std::unordered_set<T>> m_groups;
+
+	/// Map members to groups
+	std::unordered_map<T, std::unordered_set<Key>> m_membership;
+};
+
+}
+}
+
+#include "SurgSim/DataStructures/Groups-inl.h"
+
+#endif
