cb8da66a9a36547b14969b857f0a489a0dd00e5f
Harald Scheirich <hscheirich@simquest.com>
2015-03-26 13:28:58 -0400

    Add s_mapping_material
    Update Graphics Utilities
    
    Add normalization in material

--- SurgSim/Blocks/GraphicsUtilities.cpp
+++ SurgSim/Blocks/GraphicsUtilities.cpp
@@ -16,28 +16,61 @@ namespace SurgSim
 namespace Blocks
 {
 
-void add2DTexture(
+void enable2DTexture(
 	std::shared_ptr<Graphics::OsgMaterial> material,
 	const std::string& uniform, int unit, const std::string& filename, bool repeat)
 {
-	std::string path;
-	SURGSIM_ASSERT(Framework::Runtime::getApplicationData()->tryFindFile(filename, &path))
-			<< "Could not find texture file " << filename;
-	auto texture = std::make_shared<Graphics::OsgTexture2d>();
-	texture->loadImage(path);
-	auto textureUniform = std::make_shared<Graphics::OsgTextureUniform<Graphics::OsgTexture2d>>(uniform);
-
-	if (repeat)
+	if (!filename.empty())
 	{
-		auto osgTexture = texture->getOsgTexture();
-		osgTexture->setWrap(osg::Texture::WRAP_R, osg::Texture::REPEAT);
-		osgTexture->setWrap(osg::Texture::WRAP_S, osg::Texture::REPEAT);
-		osgTexture->setWrap(osg::Texture::WRAP_T, osg::Texture::REPEAT);
-	}
+		std::string path;
+		SURGSIM_ASSERT(Framework::Runtime::getApplicationData()->tryFindFile(filename, &path))
+				<< "Could not find texture file " << filename;
+		auto texture = std::make_shared<Graphics::OsgTexture2d>();
+		texture->loadImage(path);
+		auto textureUniform = std::make_shared<Graphics::OsgTextureUniform<Graphics::OsgTexture2d>>(uniform);
 
-	textureUniform->set(texture);
-	textureUniform->setMinimumTextureUnit(unit);
-	material->addUniform(textureUniform);
+		if (repeat)
+		{
+			auto osgTexture = texture->getOsgTexture();
+			osgTexture->setWrap(osg::Texture::WRAP_R, osg::Texture::REPEAT);
+			osgTexture->setWrap(osg::Texture::WRAP_S, osg::Texture::REPEAT);
+			osgTexture->setWrap(osg::Texture::WRAP_T, osg::Texture::REPEAT);
+		}
+
+		textureUniform->set(texture);
+		textureUniform->setMinimumTextureUnit(unit);
+		material->addUniform(textureUniform);
+	}
+	else
+	{
+		// If there is no texture provided, just set the texture unit on the material
+		material->addUniform("int", uniform);
+		material->setValue(uniform, unit);
+	}
+}
+
+std::shared_ptr<SurgSim::Graphics::OsgMaterial> createPlainMaterial(
+	const std::string& name,
+	SurgSim::Math::Vector4f diffuseColor,
+	SurgSim::Math::Vector4f specularColor,
+	float shininess)
+{
+	auto material = std::make_shared<Graphics::OsgMaterial>(name);
+
+	auto program = Graphics::loadProgram(*Framework::Runtime::getApplicationData(), "Shaders/s_mapping_material");
+	SURGSIM_ASSERT(program != nullptr) << "Could not load program" << "Shaders/s_mapping_material" ;
+	material->setProgram(program);
+
+	material->addUniform("vec4", "specularColor");
+	material->setValue("specularColor", specularColor);
+
+	material->addUniform("vec4", "diffuseColor");
+	material->setValue("diffuseColor", diffuseColor);
+
+	material->addUniform("float", "shininess");
+	material->setValue("shininess", shininess);
+
+	return material;
 }
 
 std::shared_ptr<SurgSim::Graphics::OsgMaterial> createTexturedMaterial(
@@ -62,17 +95,8 @@ std::shared_ptr<SurgSim::Graphics::OsgMaterial> createTexturedMaterial(
 	material->addUniform("float", "shininess");
 	material->setValue("shininess", shininess);
 
-	add2DTexture(material, "shadowMap", Graphics::SHADOW_TEXTURE_UNIT, "Textures/black.png");
-	if (diffuseMap.empty())
-	{
-		// If there is no texture provided, just use unit 0 for the diffuse map
-		material->addUniform("int", "diffuseMap");
-		material->setValue("diffuseMap", Graphics::DIFFUSE_TEXTURE_UNIT);
-	}
-	else
-	{
-		add2DTexture(material, "diffuseMap", Graphics::DIFFUSE_TEXTURE_UNIT, diffuseMap, false);
-	}
+	enable2DTexture(material, "shadowMap", Graphics::SHADOW_TEXTURE_UNIT, "Textures/black.png");
+	enable2DTexture(material, "diffuseMap", Graphics::DIFFUSE_TEXTURE_UNIT, diffuseMap, false);
 
 	return material;
 }
@@ -100,28 +124,9 @@ std::shared_ptr<SurgSim::Graphics::OsgMaterial> createNormalMappedMaterial(
 	material->addUniform("float", "shininess");
 	material->setValue("shininess", shininess);
 
-	add2DTexture(material, "shadowMap", Graphics::SHADOW_TEXTURE_UNIT, "Textures/black.png");
-	if (diffuseMap.empty())
-	{
-		// If there is no texture provided, just use unit 0 for the diffuse map
-		material->addUniform("int", "diffuseMap");
-		material->setValue("diffuseMap", Graphics::DIFFUSE_TEXTURE_UNIT);
-	}
-	else
-	{
-		add2DTexture(material, "diffuseMap", Graphics::DIFFUSE_TEXTURE_UNIT, diffuseMap, false);
-	}
-
-	if (normalMap.empty())
-	{
-		// If there is no texture provided, just use unit 0 for the diffuse map
-		material->addUniform("int", "normalMap");
-		material->setValue("normalMap", Graphics::NORMAL_TEXTURE_UNIT);
-	}
-	else
-	{
-		add2DTexture(material, "normalMap", Graphics::NORMAL_TEXTURE_UNIT, normalMap, false);
-	}
+	enable2DTexture(material, "shadowMap", Graphics::SHADOW_TEXTURE_UNIT, "Textures/black.png");
+	enable2DTexture(material, "diffuseMap", Graphics::DIFFUSE_TEXTURE_UNIT, diffuseMap, false);
+	enable2DTexture(material, "normalMap", Graphics::NORMAL_TEXTURE_UNIT, normalMap, false);
 
 	return material;
 }
--- SurgSim/Blocks/GraphicsUtilities.h
+++ SurgSim/Blocks/GraphicsUtilities.h
@@ -23,10 +23,23 @@ namespace Blocks
 /// \param unit The texture unit to use
 /// \param filename The file to use for the texture,
 /// \param repeat whether to create the texture as repeating
-void add2DTexture(std::shared_ptr<SurgSim::Graphics::OsgMaterial> material,
-				  const std::string& uniform,
-				  int unit,
-				  const std::string& filename, bool repeat = false);
+/// \note if the texture filename is empty a placeholder uniform will be created using the unit as a value
+/// this us use objects with textures builtin without having to assign the texture to the material on creation
+void enable2DTexture(std::shared_ptr<SurgSim::Graphics::OsgMaterial> material,
+					 const std::string& uniform,
+					 int unit,
+					 const std::string& filename = "", bool repeat = false);
+
+/// Create a basic material
+/// \param name name of the material
+/// \param diffuseColor Base diffuse color to use
+/// \param specularColor Base specular color to use
+/// \param shininess Phong shininess exponent
+std::shared_ptr<SurgSim::Graphics::OsgMaterial> createPlainMaterial(
+	const std::string& name,
+	SurgSim::Math::Vector4f diffuseColor,
+	SurgSim::Math::Vector4f specularColor,
+	float shininess);
 
 /// Create a basic textured material
 /// \param name name of the material
