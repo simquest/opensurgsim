d09e96ccf026b07e92335204bfe0b73a1e3e52b5
Ryan Beasley <rbeasley@simquest.com>
2015-09-14 13:38:27 -0700

    NovintDevice has properties for initialization parameters.
    Also, the regular 3Dof device and the 7Dof device were differentiated
    solely by a boolean, so they have been combined into the
    NovintCommonDevice base code.

--- SurgSim/Devices/Novint/NovintDevice.cpp
+++ SurgSim/Devices/Novint/NovintDevice.cpp
@@ -15,6 +15,11 @@
 
 #include "SurgSim/Devices/Novint/NovintDevice.h"
 
+#include "SurgSim/DataStructures/DataStructuresConvert.h"
+#include "SurgSim/Devices/Novint/NovintScaffold.h"
+
+using SurgSim::DataStructures::OptionalValue;
+
 namespace SurgSim
 {
 namespace Devices
@@ -23,12 +28,147 @@ namespace Devices
 SURGSIM_REGISTER(SurgSim::Input::DeviceInterface, SurgSim::Devices::NovintDevice, NovintDevice);
 
 NovintDevice::NovintDevice(const std::string& uniqueName) :
-	NovintCommonDevice(uniqueName)
+	Input::CommonDevice(uniqueName, NovintScaffold::buildDeviceInputData()),
+	m_positionScale(1.0), m_orientationScale(1.0), m_7DofDevice(false)
 {
+	SURGSIM_ADD_SERIALIZABLE_PROPERTY(NovintDevice, OptionalValue<std::string>, InitializationName,
+		getOptionalInitializationName, setOptionalInitializationName);
+	SURGSIM_ADD_SERIALIZABLE_PROPERTY(NovintDevice, OptionalValue<std::string>, SerialNumber,
+		getOptionalSerialNumber, setOptionalSerialNumber);
+	SURGSIM_ADD_SERIALIZABLE_PROPERTY(NovintDevice, bool, 7DofDevice, is7DofDevice, set7DofDevice);
 }
 
 NovintDevice::~NovintDevice()
 {
+	if (isInitialized())
+	{
+		finalize();
+	}
+}
+
+void NovintDevice::setSerialNumber(const std::string& serialNumber)
+{
+	SURGSIM_ASSERT(!m_initializationName.hasValue()) << "Cannot set serialNumber for a NovintDevice named " <<
+		getName() << ", which already has an initializationName.";
+	SURGSIM_ASSERT(!isInitialized()) <<
+		"Cannot setSerialNumber after the device named " << getName() << " has been initialized.";
+	m_serialNumber.setValue(serialNumber);
+}
+
+bool NovintDevice::getSerialNumber(std::string* serialNumber) const
+{
+	const bool hasValue = m_serialNumber.hasValue();
+	if (hasValue)
+	{
+		*serialNumber = m_serialNumber.getValue();
+	}
+	return hasValue;
+}
+
+void NovintDevice::setInitializationName(const std::string& initializationName)
+{
+	SURGSIM_ASSERT(!m_serialNumber.hasValue()) << "Cannot set initializationName for a NovintDevice named " <<
+		getName() << ", which already has a serialNumber.";
+	SURGSIM_ASSERT(!isInitialized()) <<
+		"Cannot setInitializationName after the device named " << getName() << " has been initialized.";
+	m_initializationName.setValue(initializationName);
+}
+
+bool NovintDevice::getInitializationName(std::string* initializationName) const
+{
+	const bool hasValue = m_initializationName.hasValue();
+	if (hasValue)
+	{
+		*initializationName = m_initializationName.getValue();
+	}
+	return hasValue;
+}
+
+bool NovintDevice::initialize()
+{
+	SURGSIM_ASSERT(!isInitialized());
+	std::shared_ptr<NovintScaffold> scaffold = NovintScaffold::getOrCreateSharedInstance();
+	SURGSIM_ASSERT(scaffold);
+
+	if (! scaffold->registerDevice(this))
+	{
+		return false;
+	}
+
+	m_scaffold = std::move(scaffold);
+	return true;
+}
+
+bool NovintDevice::finalize()
+{
+	SURGSIM_ASSERT(isInitialized());
+	bool result = m_scaffold->unregisterDevice(this);
+	m_scaffold.reset();
+	return result;
+}
+
+bool NovintDevice::isInitialized() const
+{
+	return (m_scaffold != nullptr);
+}
+
+void NovintDevice::setPositionScale(double scale)
+{
+	m_positionScale = scale;
+	if (m_scaffold)
+	{
+		m_scaffold->setPositionScale(this, m_positionScale);
+	}
+}
+
+double NovintDevice::getPositionScale() const
+{
+	return m_positionScale;
+}
+
+void NovintDevice::setOrientationScale(double scale)
+{
+	m_orientationScale = scale;
+	if (m_scaffold)
+	{
+		m_scaffold->setOrientationScale(this, m_orientationScale);
+	}
+}
+
+double NovintDevice::getOrientationScale() const
+{
+	return m_orientationScale;
+}
+
+void NovintDevice::set7DofDevice(bool val)
+{
+	SURGSIM_ASSERT(!isInitialized()) << "Cannot set 7Dof status after initialization.";
+	m_7DofDevice = val;
+}
+
+bool NovintDevice::is7DofDevice() const
+{
+	return m_7DofDevice;
+}
+
+const OptionalValue<std::string>& NovintDevice::getOptionalInitializationName() const
+{
+	return m_initializationName;
+}
+
+void NovintDevice::setOptionalInitializationName(const OptionalValue<std::string>& name)
+{
+	m_initializationName = name;
+}
+
+const OptionalValue<std::string>& NovintDevice::getOptionalSerialNumber() const
+{
+	return m_serialNumber;
+}
+
+void NovintDevice::setOptionalSerialNumber(const OptionalValue<std::string>& serial)
+{
+	m_serialNumber = serial;
 }
 
 };  // namespace Devices
--- SurgSim/Devices/Novint/NovintDevice.h
+++ SurgSim/Devices/Novint/NovintDevice.h
@@ -18,12 +18,14 @@
 
 #include <string>
 
-#include "SurgSim/Devices/Novint/NovintCommonDevice.h"
+#include "SurgSim/DataStructures/OptionalValue.h"
+#include "SurgSim/Input/CommonDevice.h"
 
 namespace SurgSim
 {
 namespace Devices
 {
+class NovintScaffold;
 
 SURGSIM_STATIC_REGISTRATION(NovintDevice);
 
@@ -35,14 +37,16 @@ SURGSIM_STATIC_REGISTRATION(NovintDevice);
 /// Grip will not be able to produce torques unless it is accessed using the Novint7DofHapticDevice class.
 ///
 /// \par Application input provided by the device:
-///   | type       | name        |                                                                |
-///   | ----       | ----        | ---                                                            |
-///   | pose       | "pose"      | %Device pose (units are meters).                               |
-///   | bool       | "button1"   | %State of the first device button if present.                  |
-///   | bool       | "button2"   | %State of the second device button if present.                 |
-///   | bool       | "button3"   | %State of the third device button if present.                  |
-///   | bool       | "button4"   | %State of the third device button if present.                  |
-///   | bool       | "isHomed"   | %Device homing status.                                         |
+///   | type       | name                  |                                                      |
+///   | ----       | ----                  | ---                                                  |
+///   | pose       | "pose"                | %Device pose (units are meters).                     |
+///   | bool       | "button1"             | %State of the first device button if present.        |
+///   | bool       | "button2"             | %State of the second device button if present.       |
+///   | bool       | "button3"             | %State of the third device button if present.        |
+///   | bool       | "button4"             | %State of the third device button if present.        |
+///   | bool       | "isHomed"             | %Device homing status.                               |
+///   | bool       | "isPositionHomed"     | %Device homing status, position only.                |
+///   | bool       | "isOrientationHomed"  | %Device homing status, orientation only.             |
 /// Note that \c button1 through \c 4 correspond to the buttons 0 through 3 provided by the
 /// HDAL SDK, but a custom Novint device might have fewer than 4 buttons.
 ///
@@ -50,22 +54,104 @@ SURGSIM_STATIC_REGISTRATION(NovintDevice);
 ///   | type       | name                  |                                                      |
 ///   | ----       | ----                  | ---                                                  |
 ///   | vector     | "force"               | %Device output force (units are newtons).            |
+///   | vector     | "torque"              | %Device output torque (in newton-meters, 7Dof only). |
 ///   | bool       | "gravityCompensation" | Enable or disable hardware gravity compensation.     |
 ///
-/// \sa Novint7DofHapticDevice
-/// \sa NovintCommonDevice, SurgSim::Input::CommonDevice, SurgSim::Input::DeviceInterface
-class NovintDevice : public NovintCommonDevice
+class NovintDevice : public SurgSim::Input::CommonDevice
 {
 public:
 	/// Constructor.
-	///
-	/// \param uniqueName A unique name for the device that will be used by the application.
-	explicit NovintDevice(const std::string& uniqueName);
+	/// \param name A unique name for the device that will be used by the application.
+	explicit NovintDevice(const std::string& name);
 
 	SURGSIM_CLASSNAME(SurgSim::Devices::NovintDevice);
 
 	/// Destructor.
 	virtual ~NovintDevice();
+
+	/// Sets the serial number used to register this device with the hardware library.
+	/// An empty string indicates the first available device.
+	/// \param serialNumber The serial number.
+	/// \sa setInitializationName
+	void setSerialNumber(const std::string& serialNumber);
+
+	/// Gets the serial number used to register this device with the hardware library.
+	/// \param [out] serialNumber The serial number, if set.
+	/// \return	true if the serialNumber has been set.
+	bool getSerialNumber(std::string* serialNumber) const;
+
+	/// Sets the name used to register this device with the hardware library.
+	/// To use an initialization name, the 'devices.yaml' file must be in the working directory,
+	/// (see example at Data/devices.yaml) with an entry of the form "initializationName: serialNumber".
+	/// An empty string indicates the first available device.
+	/// \param initializationName The initialization name.
+	/// \sa setSerialNumber
+	void setInitializationName(const std::string& initializationName);
+
+	/// Gets the name used to register this device with the hardware library.
+	/// \param [out] initializationName The initialization name, if set.
+	/// \return	true if the initializationName has been set
+	bool getInitializationName(std::string* initializationName) const;
+
+	bool initialize() override;
+
+	bool finalize() override;
+
+	/// Check whether this device is initialized.
+	bool isInitialized() const;
+
+	/// Sets the position scale for this device.
+	/// The position scale controls how much the pose changes for a given device translation.
+	/// The default value for a raw device tries to correspond to the actual physical motion of the device.
+	/// \param scale The multiplicative factor to apply to the position.
+	void setPositionScale(double scale);
+	/// Gets the position scale for this device.
+	double getPositionScale() const;
+
+	/// Sets the orientation scale for this device.
+	/// The orientation scale controls how much the pose changes for a given device rotation.
+	/// The default value for a raw device tries to correspond to the actual physical motion of the device.
+	/// \param scale The multiplicative factor to apply to the rotation angles.
+	void setOrientationScale(double scale);
+	/// Gets the orientation scale for this device.
+	double getOrientationScale() const;
+
+	/// Sets whether or not this is supposed to be a 7Dof device.
+	/// If the hardware is not actually 7Dof, the resulting device will never home.
+	/// If the hardware is 7Dof, and this is not called, the resulting device will act like a typical 3Dof.
+	/// \param val true for a 7Dof device.
+	void set7DofDevice(bool val);
+	/// Query if this object represents a 7 degree of freedom hardware device.
+	/// \return	true if 7 degree of freedom device, false if not.
+	virtual bool is7DofDevice() const;
+
+private:
+	friend class NovintScaffold;
+
+	///@{
+	/// Used for serializing optional properties
+	const DataStructures::OptionalValue<std::string>& getOptionalInitializationName() const;
+	void setOptionalInitializationName(const DataStructures::OptionalValue<std::string>& name);
+	const DataStructures::OptionalValue<std::string>& getOptionalSerialNumber() const;
+	void setOptionalSerialNumber(const DataStructures::OptionalValue<std::string>& serial);
+	///@}
+
+	/// The scaffold handles all the communication with the SDK.
+	std::shared_ptr<NovintScaffold> m_scaffold;
+
+	/// The name passed to the SDK to specify which hardware device should be used.
+	SurgSim::DataStructures::OptionalValue<std::string> m_initializationName;
+
+	/// The serial number passed to the SDK to specify which hardware device should be used.
+	SurgSim::DataStructures::OptionalValue<std::string> m_serialNumber;
+
+	/// Scale factor for the position axes; stored locally before the device is initialized.
+	double m_positionScale;
+	/// Scale factor for the orientation axes; stored locally before the device is initialized.
+	double m_orientationScale;
+
+	/// True if the device is 7Dof, false if the device is 3Dof.
+	bool m_7DofDevice;
 };
 
 };  // namespace Devices
