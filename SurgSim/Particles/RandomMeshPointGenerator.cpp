dc629863e3a5bc44b0f8744b29a6f067ce5bcdb3
Paul Novotny <pnovotny@simquest.com>
2015-09-21 15:41:28 -0400

    Adds RandomMeshPointGenerator
    With the RandomMeshPointGenerator Particle Emitters can be made with
    mesh shapes. This only adds support for emitting from the surface of a
    mesh at the moment, not from the mesh volume.

---
+++ SurgSim/Particles/RandomMeshPointGenerator.cpp
@@ -0,0 +1,75 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013-2015, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "SurgSim/Particles/RandomMeshPointGenerator.h"
+
+#include "SurgSim/Math/MeshShape.h"
+
+using SurgSim::Math::Vector2d;
+using SurgSim::Math::Vector3d;
+
+
+namespace SurgSim
+{
+namespace Particles
+{
+
+Math::Vector3d RandomMeshPointGenerator::pointInShape(std::shared_ptr<Math::Shape> shape)
+{
+	SURGSIM_LOG_SEVERE(SurgSim::Framework::Logger::getDefaultLogger()) <<
+		"PointGenerator does not support generating points in shape: "<< shape->getType();
+
+	return Vector3d::Zero();
+}
+
+Math::Vector3d RandomMeshPointGenerator::pointOnShape(std::shared_ptr<Math::Shape> shape)
+{
+	Vector3d point;
+	auto mesh = std::static_pointer_cast<Math::MeshShape>(shape);
+
+	if (mesh->getNumTriangles() > 0)
+	{
+		auto& triangles = mesh->getTriangles();
+		std::uniform_int_distribution<int> triangleSelector(0, triangles.size() - 1);
+
+		bool validTriangleFound = false;
+		size_t index;
+		while (!validTriangleFound)
+		{
+			index = triangleSelector(m_generator);
+			validTriangleFound = triangles[index].isValid;
+		}
+		auto vertices = mesh->getTrianglePositions(index);
+
+		// Find a random point on the triangle using algorithm developed by Osada et al.
+		//   R. Osada, T. Funkhouser, B. Chazelle, D. Dobkin, "Shape Distributions",
+		//   ACM Transactions on Graphics, vol. 21, no. 4, pp. 807–832, October 2002
+		Vector2d r = Vector2d::NullaryExpr([&](int index){return m_closedZeroOneDistribution(m_generator);});
+		r[0] = sqrt(r[0]);
+		point = (1 - r[0]) * vertices[0] + r[0] * (1 - r[1]) * vertices[1] + r[0] * r[1] * vertices[2];
+	}
+	else
+	{
+		SURGSIM_LOG_SEVERE(SurgSim::Framework::Logger::getDefaultLogger()) <<
+			"Mesh does not contain any triangles, cannot generate point.";
+		point = Vector3d::Zero();
+	}
+	return point;
+}
+
+
+}; // namespace Particles
+}; // namespace SurgSim
+
---
+++ SurgSim/Particles/RandomMeshPointGenerator.h
@@ -0,0 +1,46 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013-2015, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SURGSIM_PARTICLES_RANDOMMESHPOINTGENERATOR_H
+#define SURGSIM_PARTICLES_RANDOMMESHPOINTGENERATOR_H
+
+#include "SurgSim/Particles/PointGenerator.h"
+
+
+namespace SurgSim
+{
+
+namespace Math
+{
+class Shape;
+}
+
+namespace Particles
+{
+
+/// Class to generate points on the surface of a mesh
+class RandomMeshPointGenerator: public PointGenerator
+{
+public:
+	Math::Vector3d pointInShape(std::shared_ptr<Math::Shape> shape) override;
+
+	Math::Vector3d pointOnShape(std::shared_ptr<Math::Shape> shape) override;
+};
+
+}; // namespace Particles
+}; // namespace SurgSim
+
+
+#endif // SURGSIM_PARTICLES_RANDOMMESHPOINTGENERATOR_H
