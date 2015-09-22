c8af2c0d8a30d5c03aed6ca6d5c597a481440152
Wesley D. Turner <wturner@simquest.com>
2015-09-22 14:54:33 -0400

    SegmentSegmentCcdCheck: Use the interval classes to collide segments
    SegmentSegmentCcdCheck uses the Interval derived classes including linear
    motion and polynomials to detect if two segments collide during a
    specified time interval.

---
+++ SurgSim/Collision/SegmentSegmentCcdCheck.cpp
@@ -0,0 +1,283 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013-2015, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "SurgSim/Collision/SegmentSegmentCcdCheck.h"
+
+namespace SurgSim
+{
+namespace Collision
+{
+
+SegmentSegmentCcdCheck::SegmentSegmentCcdCheck(
+	const std::array<Math::Vector3d, 2>& pT0,
+	const std::array<Math::Vector3d, 2>& pT1,
+	const std::array<Math::Vector3d, 2>& qT0,
+	const std::array<Math::Vector3d, 2>& qT1,
+	double thicknessP, double thicknessQ)
+	: m_motionP1(pT0[0], pT1[0]), m_motionP2(pT0[1], pT1[1]), m_motionQ1(qT0[0], qT1[0]), m_motionQ2(qT0[1], qT1[1]),
+	  m_relativeP1Q1(m_motionQ1 - m_motionP1), m_relativeP1P2(m_motionP2 - m_motionP1),
+	  m_relativeQ1Q2(m_motionQ2 - m_motionQ1),
+	  m_P1Q1_P1P2_Q1Q2(Math::analyticTripleProduct(m_relativeP1Q1, m_relativeP1P2, m_relativeQ1Q2)),
+	  m_P1P2_P1Q1(Math::analyticDotProduct(m_relativeP1P2, m_relativeP1Q1)),
+	  m_Q1Q2_P1Q1(Math::analyticDotProduct(m_relativeQ1Q2, m_relativeP1Q1)),
+	  m_P1P2_Q1Q2(Math::analyticDotProduct(m_relativeP1P2, m_relativeQ1Q2)),
+	  m_P1P2_sq(Math::analyticMagnitudeSquared(m_relativeP1P2)),
+	  m_Q1Q2_sq(Math::analyticMagnitudeSquared(m_relativeQ1Q2)),
+	  m_P1P2xQ1Q2_x(Math::analyticCrossProductXAxis(m_relativeP1P2, m_relativeQ1Q2)),
+	  m_P1P2xQ1Q2_y(Math::analyticCrossProductYAxis(m_relativeP1P2, m_relativeQ1Q2)),
+	  m_P1P2xQ1Q2_z(Math::analyticCrossProductZAxis(m_relativeP1P2, m_relativeQ1Q2)),
+	  m_thicknessP(thicknessP), m_thicknessQ(thicknessQ),
+	  m_volumeEpsilonTimes6(0), m_muNuEpsilon(0)
+{
+}
+
+// Accessors
+const Math::LinearMotionND<double, 3>& SegmentSegmentCcdCheck::motionP1() const
+{
+	return m_motionP1;
+}
+
+const Math::LinearMotionND<double, 3>& SegmentSegmentCcdCheck::motionP2() const
+{
+	return m_motionP2;
+}
+
+const Math::LinearMotionND<double, 3>& SegmentSegmentCcdCheck::motionQ1() const
+{
+	return m_motionQ1;
+}
+
+const Math::LinearMotionND<double, 3>& SegmentSegmentCcdCheck::motionQ2() const
+{
+	return m_motionQ2;
+}
+
+Math::Vector3d SegmentSegmentCcdCheck::p11() const
+{
+	return m_motionP1.getStart();
+}
+
+Math::Vector3d SegmentSegmentCcdCheck::p12() const
+{
+	return m_motionP1.getEnd();
+}
+
+Math::Vector3d SegmentSegmentCcdCheck::p21() const
+{
+	return m_motionP2.getStart();
+}
+
+Math::Vector3d SegmentSegmentCcdCheck::p22() const
+{
+	return m_motionP2.getEnd();
+}
+
+Math::Vector3d SegmentSegmentCcdCheck::q11() const
+{
+	return m_motionQ1.getStart();
+}
+
+Math::Vector3d SegmentSegmentCcdCheck::q12() const
+{
+	return m_motionQ1.getEnd();
+}
+
+Math::Vector3d SegmentSegmentCcdCheck::q21() const
+{
+	return m_motionQ2.getStart();
+}
+
+Math::Vector3d SegmentSegmentCcdCheck::q22() const
+{
+	return m_motionQ2.getEnd();
+}
+
+const Math::PolynomialValues<double, 3>& SegmentSegmentCcdCheck::P1Q1_P1P2_Q1Q2() const
+{
+	return m_P1Q1_P1P2_Q1Q2;
+}
+
+const Math::PolynomialValues<double, 2>& SegmentSegmentCcdCheck::P1P2_P1Q1() const
+{
+	return m_P1P2_P1Q1;
+}
+
+const Math::PolynomialValues<double, 2>& SegmentSegmentCcdCheck::Q1Q2_P1Q1() const
+{
+	return m_Q1Q2_P1Q1;
+}
+
+const Math::PolynomialValues<double, 2>& SegmentSegmentCcdCheck::P1P2_Q1Q2() const
+{
+	return m_P1P2_Q1Q2;
+}
+
+const Math::PolynomialValues<double, 2>& SegmentSegmentCcdCheck::P1P2_sq() const
+{
+	return m_P1P2_sq;
+}
+
+const Math::PolynomialValues<double, 2>& SegmentSegmentCcdCheck::Q1Q2_sq() const
+{
+	return m_Q1Q2_sq;
+}
+
+Math::Interval<double> SegmentSegmentCcdCheck::crossValueOnInterval(
+	const Math::Interval<double>& range) const
+{
+	return (
+			   m_P1P2xQ1Q2_x.valuesOverInterval(range).square() +
+			   m_P1P2xQ1Q2_y.valuesOverInterval(range).square() +
+			   m_P1P2xQ1Q2_z.valuesOverInterval(range).square()
+		   );
+}
+
+double SegmentSegmentCcdCheck::thicknessP() const
+{
+	return m_thicknessP;
+}
+double SegmentSegmentCcdCheck::thicknessQ() const
+{
+	return m_thicknessQ;
+}
+
+double SegmentSegmentCcdCheck::tripleProductEpsilon() const
+{
+	return m_volumeEpsilonTimes6;
+}
+
+double SegmentSegmentCcdCheck::muNuEpsilon() const
+{
+	return m_muNuEpsilon;
+}
+
+int SegmentSegmentCcdCheck::possibleCollisionTestNoThickness(const Math::Interval<double>& range) const
+{
+	Math::Interval<double> P1Q1_P1P2_Q1Q2_values = P1Q1_P1P2_Q1Q2().valuesOverInterval(range);
+	Math::Interval<double> P1P2_P1Q1_values = P1P2_P1Q1().valuesOverInterval(range);
+	Math::Interval<double> Q1Q2_P1Q1_values = Q1Q2_P1Q1().valuesOverInterval(range);
+	Math::Interval<double> P1P2_Q1Q2_values = P1P2_Q1Q2().valuesOverInterval(range);
+	Math::Interval<double> P1P2_sq_values = P1P2_sq().valuesOverInterval(range);
+	Math::Interval<double> Q1Q2_sq_values = Q1Q2_sq().valuesOverInterval(range);
+	Math::Interval<double> crossProductSquared_values = crossValueOnInterval(range);
+
+	P1Q1_P1P2_Q1Q2_values.addThickness(m_volumeEpsilonTimes6);
+
+	bool lineDistanceIsZero = P1Q1_P1P2_Q1Q2_values.containsZero();
+	if (! lineDistanceIsZero)
+	{
+		return 1;  // collision check #1 says no collision
+	}
+
+	// [The following eqns and their derivation are written up in
+	// Docs/Collision detection/Segment-segment CCD location check.pdf]
+	// 0 = \mu (P1P2 x Q1Q2)^2 - (P1P2 * P1Q1) Q1Q2^2 + (Q1Q2 * P1Q1) (P1P2 * Q1Q2)
+	// 0 = \nu (P1P2 x Q1Q2)^2 - (Q1Q2 * P1Q1) P1P2^2 + (P1P2 * P1Q1) (P1P2 * Q1Q2)
+
+	// Calculate the values of the \mu and \nu terms in the equations above; note that valid \mu and \nu are in [0,1].
+	// We make use of the fact that I*0 = [0,0] and I*1 = I, so I*[0,1] can be found by simply extending I to include 0.
+	Math::Interval<double> muNuTerm_values = crossProductSquared_values;
+	muNuTerm_values.extendToInclude(0);
+	// Add an epsilon to the result, to handle possible numerical noise.
+	muNuTerm_values.addThickness(m_muNuEpsilon);
+
+	Math::Interval<double> muExpression = muNuTerm_values - P1P2_P1Q1_values * Q1Q2_sq_values + Q1Q2_P1Q1_values *
+										  P1P2_Q1Q2_values;
+	Math::Interval<double> nuExpression = muNuTerm_values + Q1Q2_P1Q1_values * P1P2_sq_values - P1P2_P1Q1_values *
+										  P1P2_Q1Q2_values;
+
+	bool segmentLocationsMakeCollisionPossible = muExpression.containsZero() && nuExpression.containsZero();
+	if (! segmentLocationsMakeCollisionPossible)
+	{
+		return 2;  // collision check #2 says no collision
+	}
+
+	return 0;  // as far as we know, a collision is possible
+}
+
+int SegmentSegmentCcdCheck::possibleCollisionTestWithThickness(const Math::Interval<double>& range) const
+{
+	Math::Interval<double> P1Q1_P1P2_Q1Q2_values = P1Q1_P1P2_Q1Q2().valuesOverInterval(range);
+	Math::Interval<double> P1P2_P1Q1_values      = P1P2_P1Q1().valuesOverInterval(range);
+	Math::Interval<double> Q1Q2_P1Q1_values      = Q1Q2_P1Q1().valuesOverInterval(range);
+	Math::Interval<double> P1P2_Q1Q2_values      = P1P2_Q1Q2().valuesOverInterval(range);
+	Math::Interval<double> P1P2_sq_values        = P1P2_sq().valuesOverInterval(range);
+	Math::Interval<double> Q1Q2_sq_values        = Q1Q2_sq().valuesOverInterval(range);
+	Math::Interval<double> P1P2xQ1Q2_sq_values   = crossValueOnInterval(range);
+
+	// Now we need to account for thickness.  [Look at Docs/Collision detection/Segment-segment
+	// CCD with thickness.pdf for details!]
+	double maxLengthP = sqrt(P1P2_sq_values.getMax());
+	double maxLengthQ = sqrt(Q1Q2_sq_values.getMax());
+	// Figure out some upper bounds on the value of | P1P2 x Q1Q2 |.
+	// TO DO: figure out if both of the bounds are effective, i.e. if it makes sense to keep them both.
+	// (My guess would be that the first is more effective for perpendicular segments, second for near-parallel.)
+	double crossProductUpperBound1  = maxLengthP * maxLengthQ;             // ignores the sine of the angle
+	double crossProductUpperBound2  = sqrt(P1P2xQ1Q2_sq_values.getMax());  // uses Interval_nD -> NOT necessarily tight
+	double crossProductUpperBound = std::min(crossProductUpperBound1,
+									crossProductUpperBound2);  // pick the LOWEST of the upper bounds...
+	// Compute the upper bound on the volume that the tetrahedron P1P2Q1Q2 would have if the segments were
+	// just coming into contact. (What we actually calculate is the bound on the triple product, which is
+	// six times the tetrahedron volume.)
+	double touchingVolumeUpperBound = crossProductUpperBound * (thicknessP() + thicknessQ());
+
+	// Just to be clear here: The triple product P1Q1_P1P2_Q1Q2_values is the 6*volume of the
+	// tetrahedron defined by [p1, p2, q1, q2] over the interval. The value touchingVolumeUpperBound
+	// is an upper bound of 6*volume for the tetrahedron if the the two segments just reach contact.
+	// If the interval contains 0, then somewhere in the interval, the two volumes meet and we have
+	// contact at that point.
+	P1Q1_P1P2_Q1Q2_values.addThickness(touchingVolumeUpperBound);
+	bool lineDistanceIsSmallEnough = P1Q1_P1P2_Q1Q2_values.containsZero();
+	if (!lineDistanceIsSmallEnough)
+	{
+		return 1;  // collision check #1 says no collision
+	}
+
+	// If we detected that a collision is possible based on segment orientation, then we will make one more
+	// check to verify that the actual points of closest approach live on the segments.
+	double minLengthP = sqrt(P1P2_sq_values.getMin());
+	double minLengthQ = sqrt(Q1Q2_sq_values.getMin());
+
+	// This bit is somewhat screwed up.  A fully self-consistent check wouldn't use
+	// the overshoots, and would instead
+	// check the distances of each endpoint-endpoint pair (4 in all). --bert 2011-05-11
+	Math::Interval<double> weightCoefficient = P1P2xQ1Q2_sq_values;
+
+	// So, if we consider the endpoints, then the lambda and \mu overshoots extend the physical dimension
+	// to include the end caps of the capsules. The division of the radii by minLengthX scales the implicit
+	// overshoot proportionately to the length of the vector. E.g. so if the radii of the segments combine
+	// to be 10% of the length of segment P, then the lambda interval will be extended by 10% = 0.1 at
+	// the beginning and end, i.e. [-0.1, 1.1]
+	double lambdaOvershoot = (thicknessP() + thicknessQ()) / minLengthP;
+	Math::Interval<double> lambdaInterval = Math::Interval<double>(-lambdaOvershoot, 1. + lambdaOvershoot);
+	Math::Interval<double> lambdaExpression = lambdaInterval * weightCoefficient - P1P2_P1Q1_values * Q1Q2_sq_values +
+			Q1Q2_P1Q1_values * P1P2_Q1Q2_values;
+
+	double muOvershoot = (thicknessP() + thicknessQ()) / minLengthQ;
+	Math::Interval<double> muInterval = Math::Interval<double>(-muOvershoot, 1. + muOvershoot);
+	Math::Interval<double> muExpression = muInterval * weightCoefficient + Q1Q2_P1Q1_values * P1P2_sq_values -
+										  P1P2_P1Q1_values * P1P2_Q1Q2_values;
+
+	bool segmentLocationsMakeCollisionPossible = lambdaExpression.containsZero() && muExpression.containsZero();
+	if (!segmentLocationsMakeCollisionPossible)
+	{
+		return 2;  // collision check #2 says no collision
+	}
+
+	return 0;  // as far as we know, a collision is possible
+}
+
+}; // namespace Collision
+}; // namespace SurgSim
---
+++ SurgSim/Collision/SegmentSegmentCcdCheck.h
@@ -0,0 +1,184 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013-2015, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SURGSIM_COLLISION_SEGMENTSEGMENTCCDCHECK_H
+#define SURGSIM_COLLISION_SEGMENTSEGMENTCCDCHECK_H
+
+#include "SurgSim/Math/LinearMotionArithmetic.h"
+#include "SurgSim/Math/PolynomialValues.h"
+
+namespace SurgSim
+{
+namespace Collision
+{
+
+class SegmentSegmentCcdCheck
+{
+public:
+	/// Constructor
+	/// \param pT0 Starting and ending vertices for segment p at time 0
+	/// \param pT1 Starting and ending vertices for segment p at time 1
+	/// \param qT0 Starting and ending vertices for segment q at time 0
+	/// \param qT1 Starting and ending vertices for segment q at time 1
+	/// \param thicknessP Radius of segment P
+	/// \param thicknessQ Radius of segment Q
+	SegmentSegmentCcdCheck(const std::array<Math::Vector3d, 2>& pT0,
+						   const std::array<Math::Vector3d, 2>& pT1,
+						   const std::array<Math::Vector3d, 2>& qT0,
+						   const std::array<Math::Vector3d, 2>& qT1,
+						   double thicknessP, double thicknessQ);
+	/// @{
+	/// Motion accessors
+	/// \return the motion vector (value(t1) - value(t0)) for the segment endpoints p1, p2, q1, and q2, respectively.
+	const Math::LinearMotionND<double, 3>& motionP1() const;
+	const Math::LinearMotionND<double, 3>& motionP2() const;
+	const Math::LinearMotionND<double, 3>& motionQ1() const;
+	const Math::LinearMotionND<double, 3>& motionQ2() const;
+	/// @}
+
+	/// @{
+	/// Endpoint accessors
+	/// \return the motion vector [value(t0), value(t0)] for the segment endpoints p1, p2, q1, and q2, respectively.
+	Math::Vector3d p11() const;
+	Math::Vector3d p12() const;
+	Math::Vector3d p21() const;
+	Math::Vector3d p22() const;
+	Math::Vector3d q11() const;
+	Math::Vector3d q12() const;
+	Math::Vector3d q21() const;
+	Math::Vector3d q22() const;
+	/// @}
+
+	/// Triple product value
+	/// \return the triple product of (Q1 - P1) X (P2 - P1) X (Q2 - Q1) as a 3rd degree polynomial.
+	/// \note the triple product is equivalent to 6 x the volume of tetrahedron P1P2Q1Q2.
+	const Math::PolynomialValues<double, 3>& P1Q1_P1P2_Q1Q2() const;
+
+	/// @{
+	/// Dot product accessors
+	/// \return the dot product of the difference operators for the named endpoints
+	const Math::PolynomialValues<double, 2>& P1P2_P1Q1() const;
+	const Math::PolynomialValues<double, 2>& Q1Q2_P1Q1() const;
+	const Math::PolynomialValues<double, 2>& P1P2_Q1Q2() const;
+	/// @}
+
+	/// @{
+	/// Dot product accessors
+	/// \return the squared magnitude of the difference operators for the named endpoints.
+	const Math::PolynomialValues<double, 2>& P1P2_sq() const;
+	const Math::PolynomialValues<double, 2>& Q1Q2_sq() const;
+	/// @}
+
+	/// \param range the interval over which the cross product values are to be bounded.
+	/// \return the minimum and maximum of (P2 - P1) X (Q2 - Q1) restricted to the interval range.
+	Math::Interval<double> crossValueOnInterval(const Math::Interval<double>& range) const;
+
+	/// @{
+	/// Thickness accessors
+	/// \return the thickness parameters for P and Q, respectively.
+	double thicknessP() const;
+	double thicknessQ() const;
+	/// @}
+
+	/// @{
+	/// Algorithm epsilons
+	/// \return the algorithm epsilon parameters for no thickness detections.
+	double tripleProductEpsilon() const;
+	double muNuEpsilon() const;
+	/// @}
+
+	/// Check if a collision is possible within a specified time interval assuming ideal (0 thickness) segments
+	/// \param range the parametric [0, 1] time interval over which the collision is to be detected.
+	/// \return 0, 1, or 2 indicating if a collision is possible (returns 0); if tetrahedron (P1, P2, Q1, Q2) has too
+	/// great a volume for a collision (returns 1); or if the possibly valid collision is not contained
+	/// within segments (P1, P2) and (Q1, Q2) (returns 2).
+	int possibleCollisionTestNoThickness(const Math::Interval<double>& range) const;
+
+	/// Check if a collision is possible within a specified time interval assuming segments with fixed radius
+	/// \param range the parametric [0, 1] time interval over which the collision is to be detected.
+	/// \return 0, 1, or 2 indicating if a collision is possible (returns 0); if tetrahedron (P1, P2, Q1, Q2) has too
+	/// great a volume for a collision (returns 1); or if the possibly valid collision is not contained
+	/// within segments (P1, P2) and (Q1, Q2) (returns 2).
+	int possibleCollisionTestWithThickness(const Math::Interval<double>& range) const;
+
+private:
+	/// @{
+	/// Private constructor and assignment operators to prevent copying.
+	SegmentSegmentCcdCheck(const SegmentSegmentCcdCheck&);
+	SegmentSegmentCcdCheck& operator=(const SegmentSegmentCcdCheck&);
+	/// @}
+
+	// The order of members MUST match the necessary initialization order in the constructor --
+	// if member B is calculated from member A, then B must of course be initialized after A in
+	// the constructor, and B must be declared after A in here the class definition.
+
+	/// @{
+	/// Linear motion intervals for each of the segment endpoints from t(0) to t(1).
+	Math::LinearMotionND<double, 3>  m_motionP1;
+	Math::LinearMotionND<double, 3>  m_motionP2;
+	Math::LinearMotionND<double, 3>  m_motionQ1;
+	Math::LinearMotionND<double, 3>  m_motionQ2;
+	/// @}
+
+	/// @{
+	/// Linear motion intervals for relative endpoint differences (i.e. P1Q1 indicates that the
+	/// interval encodes Q1 - P1).
+	Math::LinearMotionND<double, 3>  m_relativeP1Q1;
+	Math::LinearMotionND<double, 3>  m_relativeQ1Q2;
+	Math::LinearMotionND<double, 3>  m_relativeP1P2;
+	/// @}
+
+	/// The triple product of (Q1 - P1) X (P2 - P1) X (Q2 - Q1) as a 3rd degree polynomial.
+	/// \note the triple product is equivalent to 6 x the volume of tetrahedron P1P2Q1Q2.
+	Math::PolynomialValues<double, 3> m_P1Q1_P1P2_Q1Q2;
+
+	/// @{
+	/// Dot product accessors
+	/// The dot product of the difference operators for the named endpoints
+	Math::PolynomialValues<double, 2> m_P1P2_P1Q1;
+	Math::PolynomialValues<double, 2> m_Q1Q2_P1Q1;
+	Math::PolynomialValues<double, 2> m_P1P2_Q1Q2;
+	/// @}
+
+	/// @{
+	/// The squared magnitude of the difference operators for the named endpoints.
+	Math::PolynomialValues<double, 2> m_P1P2_sq;
+	Math::PolynomialValues<double, 2> m_Q1Q2_sq;
+	/// @}
+
+	/// @{
+	/// The x, y and z components of (P2 - P1) X (Q2 - Q1).
+	Math::PolynomialValues<double, 2> m_P1P2xQ1Q2_x;
+	Math::PolynomialValues<double, 2> m_P1P2xQ1Q2_y;
+	Math::PolynomialValues<double, 2> m_P1P2xQ1Q2_z;
+	/// @}
+
+	/// @{
+	/// The thickness parameters for P and Q, respectively.
+	double m_thicknessP;
+	double m_thicknessQ;
+	/// @}
+
+	/// @{
+	/// The algorithm epsilon parameters for no thickness detections.
+	double m_volumeEpsilonTimes6;
+	double m_muNuEpsilon;
+	/// @}
+};
+
+}; // namespace Collision
+}; // namespace SurgSim
+
+#endif // SURGSIM_COLLISION_SEGMENTSEGMENTCCDCHECK_H
