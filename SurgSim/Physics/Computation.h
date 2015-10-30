4f3296a5cd134da8b34b2ed8a327455e6dcfa195
Ryan Beasley <rbeasley@simquest.com>
2015-10-30 10:24:58 -0700

    The PhysicsManager logs the timing of its Computations.
    This information can be useful for a first look at which Computations
    take the longest, particularly now that most of the computational
    complexity actually happens inside the evaluation of lambdas via
    ThreadPool. Unlike MSVC profiling, the simulation is running while
    these statistics are logged, so the user can try different states
    (e.g., collisions) and see how they effect the various Computations.

--- SurgSim/Physics/Computation.h
+++ SurgSim/Physics/Computation.h
@@ -19,6 +19,8 @@
 #include <vector>
 #include <memory>
 
+#include "SurgSim/Framework/Timer.h"
+
 namespace SurgSim
 {
 namespace Physics
@@ -59,6 +61,10 @@ public:
 	/// \return The fully namespace qualified name of this class.
 	virtual std::string getClassName() const = 0;
 
+	/// Provides access to the update timer.
+	/// \return The timer.
+	Framework::Timer& getTimer();
+
 protected:
 
 	/// Override this function to implement the computations specific behavior
@@ -71,6 +77,9 @@ private:
 
 	/// Copy the PhysicsManagerState object when isCopyingState() is true
 	std::shared_ptr<PhysicsManagerState> preparePhysicsState(const std::shared_ptr<PhysicsManagerState>& state);
+
+	/// The update timer.
+	Framework::Timer m_timer;
 };
 
 
