c2812c288fa214494428df5c3e882560659a1488
Julien Lenoir <jlenoir@simquest.com>
2015-12-03 10:06:35 -0300

    Revert "Add ccd collision to the OSS pipeline."
    This reverts commit c337f8e4d26d66859c11955d10fc6baa5f86481e.

--- SurgSim/Physics/PhysicsManager.cpp
+++ SurgSim/Physics/PhysicsManager.cpp
@@ -17,7 +17,6 @@
 
 #include "SurgSim/Framework/Component.h"
 #include "SurgSim/Physics/BuildMlcp.h"
-#include "SurgSim/Physics/CcdCollision.h"
 #include "SurgSim/Physics/ClearCollisions.h"
 #include "SurgSim/Physics/ConstraintComponent.h"
 #include "SurgSim/Physics/ContactConstraintGeneration.h"
@@ -61,7 +60,6 @@ bool PhysicsManager::doInitialize()
 	addComputation(std::make_shared<FreeMotion>(copyState));
 	addComputation(std::make_shared<UpdateCollisionRepresentations>(copyState));
 	addComputation(std::make_shared<DcdCollision>(copyState));
-	addComputation(std::make_shared<CcdCollision>(copyState));
 	addComputation(std::make_shared<ContactConstraintGeneration>(copyState));
 	addComputation(std::make_shared<BuildMlcp>(copyState));
 	addComputation(std::make_shared<SolveMlcp>(copyState));
