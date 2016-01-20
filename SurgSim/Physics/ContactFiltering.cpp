fd28fbbe607a044b173f584bc4be9e2913519039
Julien Lenoir <jlenoir@simquest.com>
2016-01-20 17:38:45 -0300

    Add phx Computation ContactFiltering
    Filters all contacts based on relative velocity. Any contact opposite or
    orthogonal to the motion is filtered.

---
+++ SurgSim/Physics/ContactFiltering.cpp
@@ -0,0 +1,110 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013-2015, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <vector>
+
+#include "SurgSim/Collision/CollisionPair.h"
+#include "SurgSim/Collision/Representation.h"
+#include "SurgSim/Physics/ContactFiltering.h"
+#include "SurgSim/Physics/PhysicsManagerState.h"
+
+using SurgSim::Collision::CollisionPair;
+
+namespace SurgSim
+{
+namespace Physics
+{
+
+ContactFiltering::ContactFiltering(bool doCopyState) : Computation(doCopyState)
+{
+	m_logger = SurgSim::Framework::Logger::getLogger("ContactFiltering");
+}
+
+ContactFiltering::~ContactFiltering()
+{
+}
+
+std::shared_ptr<PhysicsManagerState> ContactFiltering::doUpdate(
+	const double& dt,
+	const std::shared_ptr<PhysicsManagerState>& state)
+{
+	std::shared_ptr<PhysicsManagerState> result = state;
+
+	for (auto& pair : result->getCollisionPairs())
+	{
+		auto collisionRepresentations = pair->getRepresentations();
+
+		auto collisionToPhysicsMap = state->getCollisionToPhysicsMap();
+		auto foundFirst = collisionToPhysicsMap.find(collisionRepresentations.first);
+		auto foundSecond = collisionToPhysicsMap.find(collisionRepresentations.second);
+		if (foundFirst == collisionToPhysicsMap.end() || foundSecond == collisionToPhysicsMap.end())
+		{
+			continue;
+		}
+		std::pair<std::shared_ptr<Representation>, std::shared_ptr<Representation>> physicsRepresentations;
+		physicsRepresentations.first = foundFirst->second;
+		physicsRepresentations.second = foundSecond->second;
+
+		if (!(physicsRepresentations.first->isActive() && physicsRepresentations.second->isActive()))
+		{
+			continue;
+		}
+
+		for (auto& contact : pair->getContacts())
+		{
+			Math::Vector3d normal = contact->normal;
+			auto loc1 = physicsRepresentations.first->createLocalization(contact->penetrationPoints.first);
+			auto loc2 = physicsRepresentations.second->createLocalization(contact->penetrationPoints.second);
+			Math::Vector3d velocity1 = loc1->calculateVelocity(), velocity2 = loc2->calculateVelocity();
+			Math::Vector3d relativeVelocity = (velocity2 - velocity1);
+
+			// If the relative velocity is small, it means that the motion is not strong enough to drive the
+			// contact filtering...i.e. all contacts are potentially valid and can push the object in any direction.
+			double relativeVelocityNorm = relativeVelocity.norm();
+			if (relativeVelocityNorm < 1e-4)
+			{
+				continue;
+			}
+			relativeVelocity /= relativeVelocityNorm;
+
+			// Filter contacts that are opposite to the motion
+			double criteria = normal.dot(relativeVelocity);
+			if (criteria < 0.0)
+			{
+				contact->active = false;
+				SURGSIM_LOG(m_logger, DEBUG) << "Contact filtered [normal.relativeVelocity = " <<
+					criteria << "] < 0 (opposite to the motion)" << std::endl <<
+					" > normal = " << normal.transpose() << std::endl <<
+					" > relativeVelocity = " << relativeVelocity.transpose() << std::endl;
+			}
+
+			// Filter contact that are too orthogonal to the motion and would produce inconsitant forces wrt motion
+			criteria = std::abs(normal.dot(relativeVelocity));
+			if (criteria < std::cos(5 * M_PI / 12.0))
+			{
+				contact->active = false;
+				SURGSIM_LOG(m_logger, DEBUG) << "Contact filtered [|normal.relativeVelocity| = "<<
+					criteria << "] < cos(5*PI/12) = " << std::cos(5 * M_PI / 12.0) << std::endl <<
+					" > normal = " << normal.transpose() << std::endl <<
+					" > relativeVelocity = " << relativeVelocity.transpose() << std::endl;
+			}
+		}
+	}
+
+	return result;
+}
+
+}; // Physics
+}; // SurgSim
---
+++ SurgSim/Physics/ContactFiltering.h
@@ -0,0 +1,58 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SURGSIM_PHYSICS_CONTACTFILTERING_H
+#define SURGSIM_PHYSICS_CONTACTFILTERING_H
+
+#include <memory>
+
+#include "SurgSim/Framework/Macros.h"
+#include "SurgSim/Math/Shape.h"
+#include "SurgSim/Physics/Computation.h"
+
+namespace SurgSim
+{
+
+namespace Physics
+{
+class PhysicsManagerState;
+
+/// Computation to filter the contacts with the relative motion. Any contact orthogonal to the motion will be filtered.
+class ContactFiltering : public Computation
+{
+public:
+	/// Constructor
+	/// \param doCopyState Specify if the output state in Computation::Update() is a copy or not of the input state
+	explicit ContactFiltering(bool doCopyState = false);
+
+	SURGSIM_CLASSNAME(SurgSim::Physics::ContactFiltering);
+
+	/// Destructor
+	virtual ~ContactFiltering();
+
+protected:
+	std::shared_ptr<PhysicsManagerState> doUpdate(const double& dt, const std::shared_ptr<PhysicsManagerState>& state)
+	override;
+
+private:
+	/// The logger for this class
+	std::shared_ptr<SurgSim::Framework::Logger> m_logger;
+
+};
+
+}; // Physics
+}; // SurgSim
+
+#endif // SURGSIM_PHYSICS_CONTACTFILTERING_H
