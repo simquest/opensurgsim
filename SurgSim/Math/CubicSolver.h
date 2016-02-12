246bcabf36517b390178804080d62d880d2adf01
Julien Lenoir <jlenoir@simquest.com>
2016-02-12 15:26:59 -0300

    Update copyright year

--- SurgSim/Math/CubicSolver.h
+++ SurgSim/Math/CubicSolver.h
@@ -1,5 +1,5 @@
 // This file is a part of the OpenSurgSim project.
-// Copyright 2013, SimQuest Solutions Inc.
+// Copyright 2016, SimQuest Solutions Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
