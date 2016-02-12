571b5ea1f0c84cf276556dd391e9712d944df9e4
Julien Lenoir <jlenoir@simquest.com>
2016-02-12 15:26:29 -0300

    Remove unnecessary conditional test (given the fact that the intervals overlap)

--- SurgSim/Math/CubicSolver-inl.h
+++ SurgSim/Math/CubicSolver-inl.h
@@ -182,12 +182,12 @@ int findRootsInRange01(const Polynomial<T, 3>& p, std::array<T, 3>* roots)
 			std::vector<Interval<T>> intervalsPartitioning01;
 
 			T lastValue = static_cast<T>(0);
-			if (x0 > static_cast<T>(0) && x0 < static_cast<T>(1))
+			if (x0 > static_cast<T>(0))
 			{
 				intervalsPartitioning01.emplace_back(lastValue, x0);
 				lastValue = x0;
 			}
-			if (x1 > static_cast<T>(0) && x1 < static_cast<T>(1))
+			if (x1 < static_cast<T>(1))
 			{
 				intervalsPartitioning01.emplace_back(lastValue, x1);
 				lastValue = x1;
