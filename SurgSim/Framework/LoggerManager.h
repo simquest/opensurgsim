0d86e756b7f3636d1eed76e3a3ffd5b80d550d82
Paul Novotny <pnovotny@simquest.com>
2016-04-28 09:33:00 -0400

    Fix missing vector include in LoggerManager
    For some reason the build broke due to a missing include in the
    LoggerManager.

--- SurgSim/Framework/LoggerManager.h
+++ SurgSim/Framework/LoggerManager.h
@@ -1,5 +1,5 @@
 // This file is a part of the OpenSurgSim project.
-// Copyright 2013, SimQuest Solutions Inc.
+// Copyright 2013-2016, SimQuest Solutions Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,8 +16,9 @@
 #ifndef SURGSIM_FRAMEWORK_LOGGERMANAGER_H
 #define SURGSIM_FRAMEWORK_LOGGERMANAGER_H
 
-#include <unordered_map>
 #include <boost/thread/mutex.hpp>
+#include <unordered_map>
+#include <vector>
 
 namespace SurgSim
 {
