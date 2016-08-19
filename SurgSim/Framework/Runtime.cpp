2623998df901f12b8dc1b9fd73751cd0b6f55624
Harald Scheirich <hscheirich@simquest.com>
2016-08-19 13:38:57 -0400

    Add 'Include' functionality to scenelement serialization

--- SurgSim/Framework/Runtime.cpp
+++ SurgSim/Framework/Runtime.cpp
@@ -13,7 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 #include <boost/thread/thread.hpp>
 #include <boost/thread/locks.hpp>
 
