5a349e6ba9f03485a1b32a5f5ce9bcb2ce569aac
Gughan Sathyaseelan <gsathyaseelan@simquest.com>
2016-10-28 10:11:53 -0400

    Move the render test from Physics to Blocks + Add square and granny knots.

---
+++ SurgSim/Blocks/RenderTests/KnotIdentificationRenderTests.cpp
@@ -0,0 +1,231 @@
+// This file is a part of the OpenSurgSim project.
+// Copyright 2013-2016, SimQuest Solutions Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+///\file KnotIdentificationRenderTests.cpp render test for knot identification behavior
+
+#include <gtest/gtest.h>
+
+#include "SurgSim/Blocks/Blocks.h"
+#include "SurgSim/Framework/Framework.h"
+#include "SurgSim/Framework/Log.h"
+#include "SurgSim/Graphics/Graphics.h"
+#include "SurgSim/Math/Math.h"
+#include "SurgSim/Physics/Physics.h"
+
+using SurgSim::Blocks::KnotIdentificationBehavior;
+using SurgSim::Math::Vector3d;
+
+namespace
+{
+std::shared_ptr<SurgSim::Framework::SceneElement> makeSuture(const std::string& filename)
+{
+	auto element = std::make_shared<SurgSim::Framework::BasicSceneElement>("Suture");
+
+	// Physics
+	auto physics = std::make_shared<SurgSim::Physics::Fem1DRepresentation>("Physics");
+	physics->setFemElementType("SurgSim::Physics::Fem1DElementBeam");
+	physics->setLocalPose(SurgSim::Math::RigidTransform3d::Identity());
+	physics->loadFem(filename);
+	physics->setIntegrationScheme(SurgSim::Math::INTEGRATIONSCHEME_EULER_IMPLICIT);
+	physics->setLinearSolver(SurgSim::Math::LINEARSOLVER_LU);
+	physics->setRayleighDampingMass(5.0);
+	physics->setRayleighDampingStiffness(0.001);
+	physics->setIsGravityEnabled(true);
+	element->addComponent(physics);
+
+	// Graphics
+	auto gfx = std::make_shared<SurgSim::Graphics::OsgCurveRepresentation>("Graphics");
+	gfx->setColor(SurgSim::Math::Vector4d(0.0, 0.0, 1.0, 1.0));
+	gfx->setAntiAliasing(true);
+	gfx->setWidth(0.9);
+	element->addComponent(gfx);
+
+	auto collision = std::make_shared<SurgSim::Physics::DeformableCollisionRepresentation>("Collision");
+	auto shape = std::make_shared<SurgSim::Math::SegmentMeshShape>();
+	shape->load(filename);
+	shape->setRadius(0.001);
+	collision->setShape(shape);
+	collision->setCollisionDetectionType(SurgSim::Collision::COLLISION_DETECTION_TYPE_CONTINUOUS);
+	collision->setSelfCollisionDetectionType(SurgSim::Collision::COLLISION_DETECTION_TYPE_CONTINUOUS);
+	physics->setCollisionRepresentation(collision);
+	element->addComponent(collision);
+
+	auto copier = std::make_shared<SurgSim::Blocks::TransferPhysicsToVerticesBehavior>("Copier");
+	copier->setSource(physics);
+	copier->setTarget(gfx);
+	element->addComponent(copier);
+
+	return element;
+}
+
+class KnotNameTextBehavior : public SurgSim::Framework::Behavior
+{
+public:
+	KnotNameTextBehavior(const std::string& name)
+		: SurgSim::Framework::Behavior(name)
+	{
+
+	}
+
+	void setKnotIdBehavior(std::shared_ptr<SurgSim::Blocks::KnotIdentificationBehavior> knotId)
+	{
+		m_knotId = knotId;
+	}
+
+	bool doInitialize() override
+	{
+		if (m_knotId == nullptr)
+		{
+			return false;
+		}
+		m_text = std::make_shared<SurgSim::Graphics::OsgTextRepresentation>("KnotName");
+		m_text->setLocation(100.0, 100.0);
+		m_text->setText("Waiting...");
+		m_text->setColor(SurgSim::Math::Vector4d(1.0, 0.5, 0.5, 1.0));
+		m_text->setDrawBackground(true);
+		m_text->setBackgroundColor(SurgSim::Math::Vector4d(0.3, 0.3, 0.3, 1.0));
+		getSceneElement()->addComponent(m_text);
+		return true;
+	}
+
+	bool doWakeUp() override
+	{
+		return true;
+	}
+
+	void update(double dt) override
+	{
+		m_text->setText(m_knotId->getKnotName());
+	}
+
+private:
+	/// The osgText
+	std::shared_ptr<SurgSim::Graphics::OsgTextRepresentation> m_text;
+	/// The knot ID behavior
+	std::shared_ptr<SurgSim::Blocks::KnotIdentificationBehavior> m_knotId;
+};
+}
+
+class KnotIdentificationRenderTests : public ::testing::Test
+{
+public:
+	virtual void SetUp()
+	{
+		runtime = std::make_shared<SurgSim::Framework::Runtime>("config.txt");
+
+		graphicsManager = std::make_shared<SurgSim::Graphics::OsgManager>();
+		runtime->addManager(graphicsManager);
+
+		physicsManager = std::make_shared<SurgSim::Physics::PhysicsManager>();
+		runtime->addManager(physicsManager);
+
+		behaviorManager = std::make_shared<SurgSim::Framework::BehaviorManager>();
+		runtime->addManager(behaviorManager);
+
+		scene = runtime->getScene();
+
+		viewElement = std::make_shared<SurgSim::Graphics::OsgViewElement>("Physics Render Scene");
+		scene->addSceneElement(viewElement);
+	}
+
+	virtual void TearDown()
+	{
+		runtime->stop();
+	}
+
+	virtual void runTest(const SurgSim::Math::Vector3d& cameraPosition, const SurgSim::Math::Vector3d& cameraLookAt,
+		double miliseconds)
+	{
+		using SurgSim::Graphics::OsgAxesRepresentation;
+
+		viewElement->enableManipulator(true);
+		viewElement->setManipulatorParameters(cameraPosition, cameraLookAt);
+
+		auto element = std::make_shared<SurgSim::Framework::BasicSceneElement>("Other");
+		std::shared_ptr<OsgAxesRepresentation> axes = std::make_shared<OsgAxesRepresentation>("axes");
+		axes->setSize(1.0);
+		element->addComponent(axes);
+		scene->addSceneElement(element);
+
+		/// Run the thread
+		runtime->start();
+
+		boost::this_thread::sleep(boost::posix_time::milliseconds(miliseconds));
+	}
+	
+	std::shared_ptr<SurgSim::Framework::Runtime> runtime;
+	std::shared_ptr<SurgSim::Graphics::OsgManager> graphicsManager;
+	std::shared_ptr<SurgSim::Physics::PhysicsManager> physicsManager;
+	std::shared_ptr<SurgSim::Framework::BehaviorManager> behaviorManager;
+	std::shared_ptr<SurgSim::Framework::Scene> scene;
+	std::shared_ptr<SurgSim::Graphics::OsgViewElement> viewElement;
+};
+
+TEST_F(KnotIdentificationRenderTests, TrefoilKnot)
+{
+	auto suture = makeSuture("trefoil_knot.ply");
+	auto knotId = std::make_shared<SurgSim::Blocks::KnotIdentificationBehavior>("KnotId");
+	knotId->setFem1d(suture->getComponent("Physics"));
+	suture->addComponent(knotId);
+	auto knotText = std::make_shared<KnotNameTextBehavior>("KnotNameTextBehavior");
+	knotText->setKnotIdBehavior(knotId);
+	suture->addComponent(knotText);
+
+	scene->addSceneElement(suture);
+
+	SurgSim::Math::Vector3d cameraPosition(0.25, 0.0, 0.25);
+	SurgSim::Math::Vector3d cameraLookAt(0.0, 0.0, 0.0);
+	double miliseconds = 2000.0;
+
+	runTest(cameraPosition, cameraLookAt, miliseconds);
+}
+
+TEST_F(KnotIdentificationRenderTests, SquareKnot)
+{
+	auto suture = makeSuture("square_knot.ply");
+	auto knotId = std::make_shared<SurgSim::Blocks::KnotIdentificationBehavior>("KnotId");
+	knotId->setFem1d(suture->getComponent("Physics"));
+	suture->addComponent(knotId);
+	auto knotText = std::make_shared<KnotNameTextBehavior>("KnotNameTextBehavior");
+	knotText->setKnotIdBehavior(knotId);
+	suture->addComponent(knotText);
+
+	scene->addSceneElement(suture);
+
+	SurgSim::Math::Vector3d cameraPosition(0.25, 0.0, 0.25);
+	SurgSim::Math::Vector3d cameraLookAt(0.0, 0.0, 0.0);
+	double miliseconds = 2000.0;
+
+	runTest(cameraPosition, cameraLookAt, miliseconds);
+}
+
+TEST_F(KnotIdentificationRenderTests, GrannyKnot)
+{
+	auto suture = makeSuture("granny_knot.ply");
+	auto knotId = std::make_shared<SurgSim::Blocks::KnotIdentificationBehavior>("KnotId");
+	knotId->setFem1d(suture->getComponent("Physics"));
+	suture->addComponent(knotId);
+	auto knotText = std::make_shared<KnotNameTextBehavior>("KnotNameTextBehavior");
+	knotText->setKnotIdBehavior(knotId);
+	suture->addComponent(knotText);
+
+	scene->addSceneElement(suture);
+
+	SurgSim::Math::Vector3d cameraPosition(0.25, 0.0, 0.25);
+	SurgSim::Math::Vector3d cameraLookAt(0.0, 0.0, 0.0);
+	double miliseconds = 2000.0;
+
+	runTest(cameraPosition, cameraLookAt, miliseconds);
+}
