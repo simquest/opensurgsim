9b6b7a024b6d52d076f33602f089a32a025aaabc
Ryan Kornheisl <rkornheisl@simquest.com>
2016-11-04 11:43:15 -0400

    Fix FEM 3D exporting, cleaned up graphics export code, fix boundary condition export bug At the moment I removed UV coordinates in the export to get FEM 3D exporting with faces correctly working first.

--- Tools/Blender/oss_export.py
+++ Tools/Blender/oss_export.py
@@ -9,6 +9,7 @@ bl_info = {
     "category": "Import-Export"}
 
 import os
+from mathutils import Matrix
 import bpy
 from bpy.props import (CollectionProperty,
                        StringProperty,
@@ -87,6 +88,8 @@ def export_fem_save(operator,
     if use_graphics:
         mesh.calc_normals()
 
+    mesh.transform(Matrix() * object.matrix_world)
+
     ret = save_mesh(filepath, mesh, object.get('boundary_condition'),
                     fem_dimensions,
                     use_graphics=use_graphics,
@@ -107,63 +110,57 @@ def save_mesh(filepath,
     file = open(filepath, "w", encoding="utf8", newline="\n")
     fw = file.write
 
-    has_uv = bool(mesh.uv_textures) and mesh.uv_textures.active
-    if has_uv and use_graphics:
-        uv_data = mesh.uv_textures.active
-
-    uvcoord = normal = None
-
     verts = []
     faces = []
-    vert_count = 0
-    if use_graphics and has_uv:
-        for i, polygon in enumerate(mesh.polygons):
-
-            if not polygon.use_smooth:
-                normal = polygon.normal[:]
-
-            if has_uv:
-                uv = uv_data[i].uv1, uv_data[i].uv2, uv_data[i].uv3, uv_data[i].uv4
-
-            face = faces[i]
-            for j, vert in enumerate(polygon.vertices):
-                v = mesh.vertices[vert]
-
-                if polygon.use_smooth:
-                    normal = v.normal[:]
-
-                if has_uv:
-                    uvcoord = uv[j][0], uv[j][1]
-
-                verts.append((vert, normal, uvcoord))
-                face.append(vert_count);
-                vert_count += 1
-    else:
-        vert_count = len(mesh.vertices)
-
     elements = []
+
     if fem_dimensions == '3':
-        indices = []
+        from meshpy.tet import MeshInfo, build, Options
+
+        bpy.ops.object.mode_set(mode = 'OBJECT', toggle = False)
+        bpy.context.scene.update()
+
+        # get list of verts and faces direct from mesh
+        mesh_verts = []
+        for vert in mesh.vertices:
+            mesh_verts.append(vert.co[:])
+
+        mesh_faces = []
         for face in mesh.polygons:
-            print(face.vertices[:])
-            new_vert = 0
-            for vert in face.vertices:
-                if vert not in indices:
-                    new_vert += 1
+            mesh_faces.append(face.vertices[:])
 
-            if new_vert > 0:
-                if len(indices) + new_vert <= 4:
-                    for vert in face.vertices:
-                        if vert not in indices:
-                            indices.append(vert)
-                else:
-                    elements.append(indices)
-                    indices = []
-                    for vert in face.vertices:
-                        indices.append(vert)
+        args = "pq1.5a5MY";
+        mesh_info = MeshInfo()
+        mesh_info.set_points(mesh_verts)
+        mesh_info.set_facets(mesh_faces)
+        tets = build(mesh_info, Options(args),
+                     verbose = True,
+                     attributes = False,
+                     volume_constraints = False,
+                     max_volume = None,
+                     diagnose = False,
+                     insert_points = None)
 
-        if len(indices) == 4:
-            elements.append(indices)
+        # Ply prep by creating updated vert list and face list using tets.points and tets.elements
+        for point in tets.points:
+            verts.append((point[0], point[1], point[2]))
+
+        for tet in tets.elements:
+            elements.append((tet[0], tet[1], tet[2], tet[3]))
+
+        for face in tets.faces:
+            faces.append((face[0], face[1], face[2]))
+
+    else:
+        for vert in mesh.vertices:
+            verts.append((vert.co[0], vert.co[1], vert.co[2]))
+
+        for face in mesh.polygons:
+            if len(face.vertices) == 3:
+                faces.append((face.vertices[0], face.vertices[1], face.vertices[2]))
+            else:
+                faces.append((face.vertices[0], face.vertices[1], face.vertices[2]))
+                faces.append((face.vertices[0], face.vertices[2], face.vertices[3]))
 
     fw("ply\n")
     fw("format ascii 1.0\n")
@@ -171,97 +168,60 @@ def save_mesh(filepath,
        "www.blender.org, source file: %r\n" %
        (bpy.app.version_string, os.path.basename(bpy.data.filepath)))
 
-    if fem_dimensions != '1' and use_graphics:
-        fw("element vertex %d\n" % vert_count)
-
+    fw("element vertex %d\n" % len(verts))
     fw("property double x\n"
        "property double y\n"
        "property double z\n")
 
     if use_graphics:
-        fw("property double nx\n"
-           "property double ny\n"
-           "property double nz\n")
-        if has_uv:
-            fw("property double s\n"
-               "property double t\n")
-
         if fem_dimensions != '1':
-            fw("element face %d\n" % len(mesh.polygons))
+            fw("element face %d\n" % len(faces))
             fw("property list uint uint vertex_indices\n")
 
     if fem_dimensions == '1':
         fw("element 1d_element %d\n" % vert_count-1)
         fw("property list uint uint vertex_indices\n")
     elif fem_dimensions == '2':
-        fw("element 2d_element %d\n" % len(mesh.polygons))
+        fw("element 2d_element %d\n" % len(faces))
         fw("property list uint uint vertex_indices\n")
     elif fem_dimensions == '3':
         fw("element 3d_element %d\n" % len(elements))
         fw("property list uint uint vertex_indices\n")
 
-    # TODO Implement physics material property in other addon
-    # if passed in variables are empty
-    fw("element material 1\n")
-    fw("property double mass_density\n")
-    fw("property double poisson_ratio\n")
-    fw("property double young_modulus\n")
-
     if boundary_condition is not None:
         fw("element boundary_condition %d\n" % len(boundary_condition))
-        fw("property list uint uint vertex_index\n")
+        fw("property uint vertex_index\n")
     fw("end_header\n")
 
     if fem_dimensions != '1':
         if use_graphics:
-            if has_uv:
-                for vert in verts:
-                    fw("%.6f %.6f %.6f" % mesh.vertices[vert[0]].co[:])  # co
-                    fw(" %.6f %.6f %.6f" % vert[1])  # no
-                    if has_uv:
-                        fw(" %.6f %.6f" % vert[2])  # uv
-                    fw("\n")
-                for face in faces:
-                    if len(face) == 3:
-                        fw("3 %d %d %d\n" % face[:])
-                    else:
-                        fw("4 %d %d %d %d\n" % face[:])
-            else:
-                for vert in mesh.vertices:
-                    fw("%.6f %.6f %.6f\n" % vert.co[:])  # co
-                for face in mesh.polygons:
-                    if len(face.vertices) == 3:
-                        fw("3 %d %d %d\n" % face.vertices[:])
-                    else:
-                        fw("4 %d %d %d %d\n" % face.vertices[:])
+            for vert in verts:
+                fw("%.6f %.6f %.6f\n" % vert[:])
+            for face in faces:
+                if len(face) == 3:
+                    fw("3 %d %d %d\n" % face[:])
+                else:
+                    fw("4 %d %d %d %d\n" % face[:])
         else:
-            for vert in mesh.vertices:
-                fw("%.6f %.6f %.6f\n" % vert.co[:])  # co
+            for vert in verts:
+                fw("%.6f %.6f %.6f" % vert[:])
     else:
         for vert in mesh.vertices:
-            fw("%.6f %.6f %.6f\n" % vert.co[:])  # co
+            fw("%.6f %.6f %.6f\n" % vert.co[:])
 
     if fem_dimensions is '1':
         for i in (len(mesh.vertices)-1):
             fw("2 %d %d\n" % list(i, i+1))
     elif fem_dimensions is '2':
-        for face in mesh.polygons:
-            if len(face.vertices) == 3:
-                fw("3 %d %d %d\n" % face.vertices[:])
-            else:
-                fw("3 %d %d %d\n" % face.vertices[:3])
-                fw("3 %d %d %d\n" % face.vertices[1:4])
+        for face in faces:
+            fw("3 %d %d %d\n" % face[:])
     elif fem_dimensions is '3':
         for tet in elements:
-            fw("4 %s\n" % ' '.join(map(str, tet)))
-
-    # TODO Implement physics material property in other addon
-    # if passed in variables are empty
-    fw("900.0 0.45 1.75e9\n")
+            fw("4 %d %d %d %d\n" % tet[:])
 
     if boundary_condition is not None:
         for bc in boundary_condition:
-            fw("%d " % bc)
+            fw("%d\n" % bc)
 
     file.close()
     print("writing %r done" % filepath)
