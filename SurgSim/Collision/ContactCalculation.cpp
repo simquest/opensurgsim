506233b9753389cef1bf46630b26b267a9c1285d
Gughan Sathyaseelan <gsathyaseelan@simquest.com>
2016-11-21 23:42:02 +0530

    Move shape1, shape2 calculation inside the code block where it is used.

--- SurgSim/Collision/ContactCalculation.cpp
+++ SurgSim/Collision/ContactCalculation.cpp
@@ -163,13 +163,12 @@ void ContactCalculation::doCalculateContact(std::shared_ptr<CollisionPair> pair)
 				"Second Object, wrong type of object" << secondShapeType;
 	}
 
-	std::shared_ptr<Math::Shape> shape1 = pair->getFirst()->getPosedShape();
-
-	std::shared_ptr<Math::Shape> shape2 = pair->getSecond()->getPosedShape();
-
 	std::list<std::shared_ptr<Contact>> contacts;
 	if (pair->getType() == Collision::CollisionDetectionType::COLLISION_DETECTION_TYPE_DISCRETE)
 	{
+		std::shared_ptr<Math::Shape> shape1 = pair->getFirst()->getPosedShape();
+		std::shared_ptr<Math::Shape> shape2 = pair->getSecond()->getPosedShape();
+
 		Math::PosedShape<std::shared_ptr<Math::Shape>> posedShape1(shape1, pair->getFirst()->getPose());
 		Math::PosedShape<std::shared_ptr<Math::Shape>> posedShape2(shape2, pair->getSecond()->getPose());
 		contacts = doCalculateDcdContact(posedShape1, posedShape2);
