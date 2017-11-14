7d64ce35742b6e6f53c4d0546294f9dcc3d6d482
Ryan Beasley <rbeasley@simquest.com>
2017-11-13 21:20:59 -0600

    LinearSparseSolveAndInverse only does analyzePattern when needed.
    If the sparse matrix has the same sparsity pattern, we can
    just call factorize.

--- SurgSim/Math/LinearSparseSolveAndInverse.cpp
+++ SurgSim/Math/LinearSparseSolveAndInverse.cpp
@@ -31,10 +31,52 @@ Matrix LinearSparseSolveAndInverse::getInverse() const
 void LinearSparseSolveAndInverseLU::setMatrix(const SparseMatrix& matrix)
 {
 	SURGSIM_ASSERT(matrix.cols() == matrix.rows()) << "Cannot inverse a non square matrix";
-	auto m = matrix.pruned();
-	m_solver.compute(m);
+	bool sameMatrix = false;
+
+	const auto* innerIndices = matrix.innerIndexPtr();
+	const auto* outerIndices = matrix.outerIndexPtr();
+	const auto* storedInnerIndices = m_matrix.innerIndexPtr();
+	const auto* storedOuterIndices = m_matrix.outerIndexPtr();
+	if ((matrix.outerSize() == m_matrix.outerSize()) && (matrix.innerSize() == m_matrix.innerSize()) &&
+		(matrix.outerSize() > 1))
+	{
+		sameMatrix = true;
+		for (Eigen::Index outerLoop = 0; outerLoop < matrix.outerSize() - 1; ++outerLoop)
+		{
+			if ((outerIndices[outerLoop] != storedOuterIndices[outerLoop]) ||
+				(outerIndices[outerLoop + 1] != storedOuterIndices[outerLoop + 1]))
+			{
+				sameMatrix = false;
+				break;
+			}
+			for (auto innerLoop = outerIndices[outerLoop];
+				innerLoop < outerIndices[outerLoop + 1];
+				++innerLoop)
+			{
+				if (innerIndices[innerLoop] != storedInnerIndices[innerLoop])
+				{
+					sameMatrix = false;
+					break;
+				}
+			}
+			if (!sameMatrix)
+			{
+				break;
+			}
+		}
+	}
+	
+	if (sameMatrix)
+	{
+		m_solver.factorize(matrix);
+	}
+	else
+	{
+		m_solver.compute(matrix);
+		m_matrix = matrix;
+	}
+
 	SURGSIM_ASSERT(m_solver.info() == Eigen::Success) << m_solver.lastErrorMessage();
-	//m_matrix = m;
 
 	if (m_identity.cols() != matrix.cols() || m_identity.rows() != matrix.rows())
 	{
