1ba3e21d766cb558988f866bae61d37a30c4aa47
Ryan Beasley <rbeasley@simquest.com>
2020-06-04 12:10:59 -0500

    Fixes the expected penetration depth of the new test.
    This commit does not fix the bug that causes assertions.
    Instead, it makes the test provide the correct expected penetration
    depth for the variations that do not assert.
    
    However, the triangle point is not equal to the expected triangle
    point.  To make them equal, use something like
    Vector3d(1e-12, 1e-12, 0.0) for the expected triangle point.

--- SurgSim/Math/UnitTests/TriangleCapsuleContactCalculationTests.cpp
+++ SurgSim/Math/UnitTests/TriangleCapsuleContactCalculationTests.cpp
@@ -484,8 +484,9 @@ TEST_F(TriangleCapsuleContactCalculationTest, TestCase36)
 	auto penetration = Geometry::DistanceEpsilon * 0.5;
 	testTriangleCapsuleContactCalculation(
 		"Failing test 5 (Angled capsule axis penetrating triangle, less than DistanceEpsilon on positive side)",
-		Vector3d(0.0, 0.0, penetration), Vector3d(0.01, 0.01, -1.0), true, true, Vector3d::Zero(),
-		Vector3d(0, 0, -0.5 - penetration));
+		Vector3d(0.0, 0.0, penetration), Vector3d(0.01, 0.01, -1.0), true, true,
+		Vector3d(0.01, 0.01, 0.0),
+		Vector3d(0.01, 0.01, -1.5));
 }
 }
 }
