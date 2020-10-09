60dd6abf2b1657aef1d962bb1294e44b007c4d14
Ryan Beasley <rbeasley@simquest.com>
2020-10-09 11:25:13 -0500

    Fixes copyright dates for new files.

--- SurgSim/Physics/MassSpring.cpp
+++ SurgSim/Physics/MassSpring.cpp
@@ -1,5 +1,5 @@
 // This file is a part of the OpenSurgSim project.
-// Copyright 2015, SimQuest Solutions Inc.
+// Copyright 2020, SimQuest Solutions Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
